8ed85435f1b5fa1e17c6f95972d18fdaf1a3987d
adam malantonio <malantoa@lafayette.edu>
2017-12-20 14:10:46 -0500

    inline ActiveJob adapter during initial dev

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Spot
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # inline the ActiveJob adapter for the moment
+    # https://github.com/samvera/hyrax#start-background-workers
+    config.active_job.queue_adapter = :inline
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
