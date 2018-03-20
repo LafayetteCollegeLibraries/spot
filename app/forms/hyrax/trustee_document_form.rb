679c72d7167777b04507d9ffd000941353502272
adam malantonio <malantoa@lafayette.edu>
2018-03-20 11:12:16 -0400

    add tests for TrusteeDocumentForm

--- app/forms/hyrax/trustee_document_form.rb
+++ app/forms/hyrax/trustee_document_form.rb
@@ -3,10 +3,19 @@
 module Hyrax
   class TrusteeDocumentForm < Hyrax::Forms::WorkForm
     self.model_class = ::TrusteeDocument
+
+    self.required_fields = [
+      :title,
+      :date_created,
+      :classification
+    ]
+
     self.terms = [
       :title,
+      :date_created,
       :start_page,
       :end_page,
+      :classification
     ]
   end
 end
--- spec/forms/hyrax/trustee_document_form_spec.rb
+++ spec/forms/hyrax/trustee_document_form_spec.rb
@@ -1,9 +1,19 @@
 RSpec.describe Hyrax::TrusteeDocumentForm do
   describe '.required_fields' do
     subject { described_class.required_fields }
+
+    it { is_expected.to include :title }
+    it { is_expected.to include :date_created }
+    it { is_expected.to include :classification }
   end
 
   describe '.terms' do
     subject { described_class.terms }
+
+    it { is_expected.to include :title }
+    it { is_expected.to include :date_created }
+    it { is_expected.to include :start_page }
+    it { is_expected.to include :end_page }
+    it { is_expected.to include :classification }
   end
 end
