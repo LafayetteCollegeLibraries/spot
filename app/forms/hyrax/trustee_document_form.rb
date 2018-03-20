aba5b410726ada5701856ebaeea91ef1271b801f
adam malantonio <malantoa@lafayette.edu>
2018-03-20 11:30:42 -0400

    use form.model_attributes to coerce start/end_page into integers

--- app/forms/hyrax/trustee_document_form.rb
+++ app/forms/hyrax/trustee_document_form.rb
@@ -17,5 +17,12 @@ module Hyrax
       :end_page,
       :classification
     ]
+
+    def self.model_attributes(_)
+      super.tap do |attrs|
+        attrs[:start_page] = attrs[:start_page].to_i unless attrs[:start_page].blank?
+        attrs[:end_page] = attrs[:end_page].to_i unless attrs[:end_page].blank?
+      end
+    end
   end
 end
