f796d086d9835bd9490291e43879dfba421fbcf9
adam malantonio <malantoa@lafayette.edu>
2018-03-21 16:54:00 -0400

    🎉  add feature test for PDF.js viewer

--- spec/factories/document.rb
+++ spec/factories/document.rb
@@ -4,6 +4,22 @@ FactoryBot.define do
     title [FFaker::Book.title]
     visibility Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
 
+    transient do
+      user { nil }
+      pdf { nil }
+    end
+
+    after(:build) do |work, evaluator|
+      work.apply_depositor_metadata(evaluator.user.user_key)
+
+      if evaluator.pdf
+        actor = Hyrax::Actors::FileSetActor.new(FileSet.create, evaluator.user)
+        actor.create_metadata({})
+        actor.create_content(Hyrax::UploadedFile.create(file: evaluator.pdf))
+        actor.attach_to_work(work)
+      end
+    end
+
     admin_set do
       AdminSet.find(AdminSet.find_or_create_default_admin_set_id)
     end
--- spec/features/show_document_spec.rb
+++ spec/features/show_document_spec.rb
@@ -1,20 +1,27 @@
-RSpec.feature 'Show Document page', js: false do
+RSpec.feature 'Show Document page', js: true do
   let(:user) { create(:user) }
-  let(:doc) { create(:document, depositor: user.user_key) }
+  let(:pdf) { File.open("#{::Rails.root}/spec/fixtures/image-document.pdf") }
+  let(:doc) { create(:document, user: user, pdf: pdf) }
 
   before do
-    pdf_file = "#{::Rails.root}/spec/fixtures/image-document.pdf"
-    uploaded_files = [
-      Hyrax::UploadedFile.create(file: File.open(pdf_file))
-    ]
-
     allow(CharacterizeJob).to receive(:perform_later) # There is no fits installed on travis-ci
-
-    AttachFilesToWorkJob.perform_now(doc, uploaded_files)
   end
 
-  scenario 'Show expected fields' do
+  scenario 'the PDF viewer is displayed' do
+    allow_any_instance_of(Hyrax::FileSetHelper)
+      .to receive(:media_display_partial)
+      .and_return 'hyrax/file_sets/media_display/pdf'
+
+    allow_any_instance_of(Hyrax::DownloadsController)
+      .to receive(:show)
+      .and_return pdf
+
     visit "/concern/documents/#{doc.id}"
+
     expect(page).to have_content doc.title.first
+
+    iframe = page.find('iframe')
+    expect(iframe).to be_present
+    expect(iframe[:src]).to include "/web/viewer.html?file=/downloads/#{doc.file_sets.first.id}"
   end
 end
