cad66967a146096e1f3bc39d14941aedbb547fce
adam malantonio <malantoa@lafayette.edu>
2018-03-27 17:18:42 -0400

    begin TrusteeDocumentsController spec

--- spec/controllers/hyrax/trustee_documents_controller_spec.rb
+++ spec/controllers/hyrax/trustee_documents_controller_spec.rb
@@ -1,9 +1,33 @@
-# Generated via
-#  `rails generate hyrax:work TrusteeDocument`
-require 'rails_helper'
-
 RSpec.describe Hyrax::TrusteeDocumentsController do
-  it "has tests" do
-    skip "Add your tests here"
+  let(:user) { create(:trustee_user) }
+  let(:doc) { create(:trustee_document, user: user) }
+
+  context 'when visiting a known document' do
+    before do
+      sign_in user
+      get :show, params: { id: doc.id }
+    end
+
+    it { expect(response).to be_successful }
+  end
+
+  context 'when visiting a non-existant TrusteeDocument' do
+    # TODO: this shouldn't be raising an exception, but rather be rendering a 404
+
+    it 'raises a Blacklight::RecordNotFound error' do
+      expect { get :show, params: { id: 'not-here' } }
+        .to raise_error(Blacklight::Exceptions::RecordNotFound)
+    end
+  end
+
+  context 'when visiting a Trustee Document as a guest/non-trustee user' do
+    let(:user) { create(:user) }
+    let(:doc) { create(:trustee_document) }
+
+    # TODO: skipping until we figure out auth issues with TrusteeDocuments
+    skip 'returns Unauthorized' do
+      expect(response.status).to eq 401
+      expect(response.message).to eq 'Unauthorized'
+    end
   end
 end
