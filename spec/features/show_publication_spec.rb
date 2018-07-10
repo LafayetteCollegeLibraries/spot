e43c527f2bae92b93c26076246fff422e9a3b4bd
adam malantonio <malantoa@lafayette.edu>
2018-07-10 11:55:55 -0400

    add feature spec for universal viewer

--- spec/features/show_publication_spec.rb
+++ spec/features/show_publication_spec.rb
@@ -1,27 +1,44 @@
 RSpec.feature 'Show Publication page', :js do
   let(:user) { create(:user) }
-  let(:pdf) { File.open("#{::Rails.root}/spec/fixtures/document.pdf") }
-  let(:pub) { create(:publication, :public, user: user, file: pdf) }
+  let(:pub) { create(:publication, :public, user: user, file: file) }
 
   before do
     allow(CharacterizeJob).to receive(:perform_later) # There is no fits installed on travis-ci
-  end
-
-  scenario 'the PDF viewer is displayed' do
-    allow_any_instance_of(Hyrax::FileSetHelper)
-      .to receive(:media_display_partial)
-      .and_return 'hyrax/file_sets/media_display/pdf'
 
     allow_any_instance_of(Hyrax::DownloadsController)
       .to receive(:show)
-      .and_return pdf
+      .and_return file
+
+    allow_any_instance_of(Hyrax::FileSetPresenter)
+      .to receive(presenter_check_method)
+      .and_return true
 
     visit "/concern/publications/#{pub.id}"
+  end
 
-    expect(page).to have_content pub.title.first
+  context 'when Publication is a PDF' do
+    let(:presenter_check_method) { :pdf? }
+    let(:file) { File.open("#{::Rails.root}/spec/fixtures/document.pdf") }
 
-    iframe = page.find('iframe')
-    expect(iframe).to be_present
-    expect(iframe[:src]).to include "/web/viewer.html?file=/downloads/#{pub.file_sets.first.id}"
+    scenario 'the PDF viewer is displayed' do
+      expect(page).to have_content pub.title.first
+
+      iframe = page.find('iframe')
+      expect(iframe).to be_present
+      expect(iframe[:src]).to include "/web/viewer.html?file=/downloads/#{pub.file_sets.first.id}"
+    end
+  end
+
+  context 'when Publication is an image' do
+    let(:file) { File.open("#{::Rails.root}/spec/fixtures/image.png") }
+    let(:presenter_check_method) { :image? }
+
+    scenario 'the UniversalViewer is displayed' do
+      expect(page).to have_content pub.title.first
+
+      viewer = page.find('.uv.viewer')
+      expect(viewer).to be_present
+      expect(viewer[:'data-uri']).to eq "/concern/#{pub.class.to_s.downcase.pluralize}/#{pub.id}/manifest"
+    end
   end
 end
