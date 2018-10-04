fee92be8c6307e37a758ea332186e32da6ed675a
adam malantonio <malantoa@lafayette.edu>
2018-10-04 13:48:43 -0400

    clear up some issues with Hyrax::PublicationForm

--- app/forms/hyrax/publication_form.rb
+++ app/forms/hyrax/publication_form.rb
@@ -2,7 +2,8 @@
 
 module Hyrax
   class PublicationForm < Hyrax::Forms::WorkForm
-    self.model_class = Publication
+    self.model_class = ::Publication
+    self.required_fields = [:title]
     self.terms = [
       # titles
       :title,
@@ -35,49 +36,111 @@ module Hyrax
       :related_resource,
       :rights_statement,
 
-      # internal fields (from Hyrax::Forms::WorkForm)
+      # internal fields
+      # These are Hyrax-specific fields that deal with embargoes,
+      # parent/child relationships. These need to be present in
+      # this array so that they are included in the sanitized_params
+      # hash used in submission.
       :representative_id, :thumbnail_id, :rendering_ids, :files,
       :visibility_during_embargo, :embargo_release_date, :visibility_after_embargo,
       :visibility_during_lease, :lease_expiration_date, :visibility_after_lease,
       :visibility, :ordered_member_ids, :in_works_ids,
-      :member_of_collection_ids, :admin_set_id
+      :member_of_collection_ids, :admin_set_id,
     ]
 
-    def self.singular_fields
-      %i(
-        resource_type
-        abstract
-        issued
-        available
-        date_created
-      )
+    # samvera/hydra-editor uses both the class method (new form) and
+    # instance method (edit form) versions of this method, so we need
+    # to provide both (otherwise we're head-first down a rabbit hole
+    # to figure out why it's not working). This is just a wrapper around
+    # the class method. I'm not 100% certain that the +super+ call
+    # of the class method will mess up the instance method, but I
+    # _think_ they both end up at the same FieldMetadataService
+    # anyway, which is alright.
+    #
+    # @param [String,Symbol] term
+    # @return [TrueClass, FalseClass]
+    def multiple?(term)
+      self.class.multiple?(term)
     end
 
-    def self.multiple?(field)
-      if singular_fields.include?(field.to_sym)
-        false
-      else
+    # an array to iterate through when building our custom portion
+    # of the Publication form. Excluded are the
+    # @return [Array<Symbol>]
+    def primary_terms
+      terms - [
+        :representative_id, :thumbnail_id, :rendering_ids, :files,
+        :visibility_during_embargo, :embargo_release_date, :visibility_after_embargo,
+        :visibility_during_lease, :lease_expiration_date, :visibility_after_lease,
+        :visibility, :ordered_member_ids, :in_works_ids,
+        :member_of_collection_ids, :admin_set_id,
+      ]
+    end
+
+    # @return [String]
+    def abstract
+      self['abstract'].first
+    end
+
+    # @return [String]
+    def date_issued
+      self['date_issued'].first
+    end
+
+    # @return [String]
+    def date_available
+      self['date_available'].first
+    end
+
+    class << self
+
+      # samvera/hydra-editor uses both the class method (new form) and
+      # instance method (edit form) versions of this method, so we need
+      # to provide both (otherwise we're head-first down a rabbit hole
+      # to figure out why it's not working). This is just a wrapper around
+      # the class method. I'm not 100% certain that the +super+ call
+      # of the class method will mess up the instance method, but I
+      # _think_ they both end up at the same FieldMetadataService
+      # anyway, which is alright.
+      #
+      # @param [String,Symbol] term
+      # @return [TrueClass,FalseClass]
+      def multiple?(term)
+        return false if singular_terms.include?(term.to_sym)
         super
       end
-    end
 
-    def self.model_attributes(form_params)
-      prefixes = form_params.delete('identifier_prefix')
-      values = form_params.delete('identifier_value')
-
-      merged = prefixes.zip(values).map do |(key, value)|
-        Spot::Identifier.new(key, value).to_s
+      # @return [Array<Symbol>]
+      def singular_terms
+        %i(
+          resource_type
+          abstract
+          date_issued
+          date_available
+        )
       end
 
-      super.tap do |params|
-        params[:identifier] = merged
-      end
-    end
+      def model_attributes(form_params)
+        prefixes = form_params.delete('identifier_prefix')
+        values = form_params.delete('identifier_value')
 
-    def self.build_permitted_params
-      super.tap do |params|
-        params << { identifier_prefix: [] }
-        params << { identifier_value: [] }
+        merged = prefixes.zip(values).map do |(key, value)|
+          Spot::Identifier.new(key, value).to_s
+        end
+
+        super.tap do |params|
+          params[:identifier] = merged
+
+          singular_terms.each do |term|
+            params[term] = Array(params[term]) if params[term]
+          end
+        end
+      end
+
+      def build_permitted_params
+        super.tap do |params|
+          params << { identifier_prefix: [] }
+          params << { identifier_value: [] }
+        end
       end
     end
   end
