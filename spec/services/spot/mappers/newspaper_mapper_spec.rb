88501fb999539892cc7dc7bcaafbf1d9feb5f3f3
adam malantonio <malantoa@lafayette.edu>
2018-10-29 20:56:06 -0400

    use 'logs warning' shared example for magazine and newspapers

--- spec/services/spot/mappers/newspaper_mapper_spec.rb
+++ spec/services/spot/mappers/newspaper_mapper_spec.rb
@@ -22,38 +22,11 @@ RSpec.describe Spot::Mappers::NewspaperMapper do
     end
 
     context 'when location is not in our internal mapping' do
-      before do
-        @original_logger = Rails.logger
-
-        class FakeLogger < Logger
-          attr_reader :messages
-          def initialize(*args)
-            super(File::NULL)
-            @messages = []
-          end
-
-          def add(*args)
-            @messages << args
-            super(*args)
-          end
-        end
-
-        Rails.logger = logger
-      end
-
-      after do
-        Rails.logger = @original_logger
-        Object.send(:remove_const, :FakeLogger)
-      end
-
-      let(:logger) { FakeLogger.new(File::NULL)  }
       let(:metadata) { {'dc:coverage' => ['Coolsville, Daddy-O']} }
 
-      it 'writes a warning to the logger' do
-        expect(based_near_attributes).to be_empty
-        expect(logger.messages).not_to be_empty
-        expect(logger.messages.first.first).to eq Logger::WARN
-      end
+      it { is_expected.to be_empty }
+
+      it_behaves_like 'it logs a warning'
     end
   end
 
