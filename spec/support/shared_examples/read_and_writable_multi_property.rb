6ab8d4eebded56ca81abb36213e46a1f1e411497
adam malantonio <malantoa@lafayette.edu>
2018-11-27 09:42:17 -0500

    use hyrax-spec for Publication specs

--- spec/support/shared_examples/read_and_writable_multi_property.rb
+++
@@ -1,9 +0,0 @@
-shared_examples 'a read and writable multiple property' do
-  let(:value) { ['Example value'] }
-
-  it 'reads and writes values' do
-    expect(subject).to be_empty
-    subject = value
-    expect(subject).to eq value
-  end
-end
