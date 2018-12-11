f93a065a4ed613c43b0c27c1bbdbdff90f517069
adam malantonio <malantoa@lafayette.edu>
2018-12-11 16:11:07 -0500

    fix factory_bot deprecations

--- spec/factories/hyrax_uploaded_files.rb
+++ spec/factories/hyrax_uploaded_files.rb
@@ -6,11 +6,11 @@
 FactoryBot.define do
   factory :uploaded_pdf, class: Hyrax::UploadedFile do
     user
-    file File.open('spec/fixtures/document.pdf')
+    file { File.open('spec/fixtures/document.pdf') }
   end
 
   factory :uploaded_image, class: Hyrax::UploadedFile do
     user
-    file File.open('spec/fixtures/image.png')
+    file { File.open('spec/fixtures/image.png') }
   end
 end
