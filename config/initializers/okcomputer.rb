c0884ae4020eef3882af9970f30549c7937aaa40
adam malantonio <malantoa@lafayette.edu>
2019-01-08 15:16:48 -0500

    prevent load-order errors when initializing OkComputer

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -6,16 +6,22 @@
 # lets us mount it manually (and at a different endpoint) in config/routes.rb
 OkComputer.mount_at = false
 
-redis_config = YAML.safe_load(ERB.new(IO.read(Rails.root.join('config', 'redis.yml'))).result)
+env = Rails.env
 
-fcrepo_uri = URI.parse(ActiveFedora.config.credentials[:url]).tap do |uri|
-  cred = ActiveFedora.config.credentials
-  uri.userinfo = "#{cred[:user]}:#{cred[:password]}"
+solr_config =
+  YAML.safe_load(ERB.new(IO.read(Rails.root.join('config', 'solr.yml'))).result)[env]
+redis_config =
+  YAML.safe_load(ERB.new(IO.read(Rails.root.join('config', 'redis.yml'))).result)[env]
+fcrepo_config =
+  YAML.safe_load(ERB.new(IO.read(Rails.root.join('config', 'fedora.yml'))).result)[env]
+
+fcrepo_uri = URI.parse(fcrepo_config['url']).tap do |uri|
+  uri.userinfo = "#{fcrepo_config['user']}:#{fcrepo_config['password']}"
 end.to_s
 
 # out of the box, "application running?" and "active record working?" checks
 # are run. we need to register the others
-OkComputer::Registry.register 'solr', OkComputer::SolrCheck.new(ActiveFedora.solr.options[:url])
+OkComputer::Registry.register 'solr', OkComputer::SolrCheck.new(solr_config['url'])
 OkComputer::Registry.register 'redis', OkComputer::RedisCheck.new(redis_config)
 OkComputer::Registry.register 'fedora', OkComputer::HttpCheck.new(fcrepo_uri)
 OkComputer::Registry.register 'queue_default', OkComputer::SidekiqLatencyCheck.new(:default)
