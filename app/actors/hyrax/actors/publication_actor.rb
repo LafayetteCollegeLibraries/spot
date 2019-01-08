802535abd78e5285fb8165197fc2b05f9f6516fd
adam malantonio <malantoa@lafayette.edu>
2019-01-08 11:05:23 -0500

    allow PublicationActor to use date_uploaded values

--- app/actors/hyrax/actors/publication_actor.rb
+++ app/actors/hyrax/actors/publication_actor.rb
@@ -2,6 +2,39 @@
 module Hyrax
   module Actors
     class PublicationActor < Hyrax::Actors::BaseActor
+      private
+
+        # Overrides the BaseActor method to allow us to stuff in
+        # `date_uploaded` values where necessary.
+        #
+        # @return [void]
+        def apply_deposit_date(env)
+          return super unless date_uploaded_present?(env)
+          env.curation_concern.date_uploaded = get_date_uploaded_value(env)
+        end
+
+        # @param [Hyrax::Actors::Environment] env
+        # @return [true, false]
+        def date_uploaded_present?(env)
+          env.attributes[:date_uploaded].present? || env.curation_concern.date_uploaded.present?
+        end
+
+        # @param [Hyrax::Actors::Environment] env
+        # @return [DateTime]
+        def get_date_uploaded_value(env)
+          concern = env.curation_concern
+
+          if concern.date_uploaded.present?
+            # calling `#to_s` allows us to recycle a previous Time value
+            # or use a string attached to the model (not recommended!)
+            DateTime.parse(concern.date_uploaded.to_s).utc
+          elsif env.attributes[:date_uploaded].present?
+            DateTime.parse(env.attributes[:date_uploaded]).utc
+          else
+            # Probably unnecessary but I don't want to leave any mystery
+            TimeService.time_in_utc
+          end
+        end
     end
   end
 end
--- spec/actors/hyrax/actors/publication_actor_spec.rb
+++ spec/actors/hyrax/actors/publication_actor_spec.rb
@@ -1,6 +1,54 @@
 # frozen_string_literal: true
 RSpec.describe Hyrax::Actors::PublicationActor do
-  it "has tests" do
-    skip "Add your tests here"
+  let(:actor) { described_class.new(Hyrax::Actors::Terminator.new) }
+
+  # thumbs-down to testing a private method, but this is (currently) the only
+  # thing we've changed in PublicationActor
+  describe '#apply_deposit_date' do
+    subject(:apply_date) { actor.send(:apply_deposit_date, env) }
+
+    let(:work) { Publication.new }
+    let(:user) { build(:user) }
+    let(:ability) { Ability.new(user) }
+    let(:attributes) { {} }
+    let(:env) { Hyrax::Actors::Environment.new(work, ability, attributes) }
+    let(:date_uploaded) { '2018-01-08T00:00:00Z' }
+
+    context 'when no date_uploaded value is provided' do
+      before do
+        allow(Hyrax::TimeService).to receive(:time_in_utc).and_return(time_value)
+      end
+
+      let(:time_value) { DateTime.now.utc }
+
+      it 'sets the date to TimeService.time_in_utc' do
+        expect { apply_date }
+          .to change { work.date_uploaded }
+          .from(nil)
+          .to(time_value)
+      end
+    end
+
+    context 'when a date_uploaded is provided to the attributes' do
+      let(:attributes) { { date_uploaded: date_uploaded } }
+
+      it 'sets the date_uploaded of the work to a DateTime of the value' do
+        expect { apply_date }
+          .to change { work.date_uploaded }
+          .from(nil)
+          .to(DateTime.parse(date_uploaded).utc)
+      end
+    end
+
+    context 'when a date_uploaded value is present on the work' do
+      let(:work) { Publication.new(date_uploaded: date_uploaded) }
+
+      it 'ensures the value is a DateTime' do
+        expect { apply_date }
+          .to change { work.date_uploaded }
+          .from(date_uploaded)
+          .to(DateTime.parse(date_uploaded).utc)
+      end
+    end
   end
 end
