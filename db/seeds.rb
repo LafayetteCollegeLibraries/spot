a12ae816e2cbcfe3a5bfdee6e2049a3e692483a3
adam malantonio <malantoa@lafayette.edu>
2019-01-09 11:39:08 -0500

    simplify db seeds + remove 'trustee' role (for now)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,13 @@
 # seed the application w/ some essentials
 
-DEFAULT_USERS = %w[
-  malantoa@lafayette.edu
-  trustee_user@lafayette.edu
-  faculty_user@lafayette.edu
-]
-
-ADMIN_USERS = %w[
-  malantoa@lafayette.edu
-]
-
-TRUSTEE_USERS = %w[
-  malantoa@lafayette.edu
-  trustee_user@lafayette.edu
-]
-
 Rake::Task['spot:roles:default'].invoke
 Rake::Task['hyrax:default_admin_set:create'].invoke
 Rake::Task['hyrax:default_collection_types:create'].invoke
 
-DEFAULT_USERS.each { |email| User.create(email: email, password: 'letmein') }
 # create a depositor user
-deposibot = User.create!(email: 'dss@lafayette.edu', display_name: 'DeposiBot', password: 'beep-boop')
+deposibot = User.find_or_create_by(email: 'dss@lafayette.edu') do |bot|
+  require 'securerandom'
 
-admin = Role.find_by(name: 'admin')
-trustee = Role.find_by(name: 'trustee')
-
-ADMIN_USERS.each { |email| admin.users << User.find_by(email: email) }
-admin.users << deposibot
-admin.save!
-
-TRUSTEE_USERS.each { |email| trustee.users << User.find_by(email: email) }
-trustee.users << deposibot
-trustee.save!
+  bot.display_name = 'DeposiBot'
+  bot.password = SecureRandom.base64
+end
