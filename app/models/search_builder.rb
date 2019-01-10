c7ba02671a0017725bbdd01a4dda41a3ea3599a9
adam malantonio <malantoa@lafayette.edu>
2019-01-10 09:06:08 -0500

    add back search_builder.rb
    looks like this is called from Hyrax::WorksControllerBehavior? this should let our
    specs resume

---
+++ app/models/search_builder.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+#
+# Generated from +rails generate hyrax:install+
+class SearchBuilder < Blacklight::SearchBuilder
+  include Blacklight::Solr::SearchBuilderBehavior
+  include BlacklightAdvancedSearch::AdvancedSearchBuilder
+
+  # Add a filter query to restrict the search to documents the current user has access to
+  include Hydra::AccessControlsEnforcement
+  include Hyrax::SearchFilters
+
+  ##
+  # @example Adding a new step to the processor chain
+  #   self.default_processor_chain += [:add_custom_data_to_query]
+  #
+  #   def add_custom_data_to_query(solr_parameters)
+  #     solr_parameters[:custom] = blacklight_params[:user_value]
+  #   end
+end
