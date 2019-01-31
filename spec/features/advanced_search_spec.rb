5e6a621d0218f97cfcfe252c53f9c234ac0753ac
adam malantonio <malantoa@lafayette.edu>
2019-01-31 11:27:11 -0500

    add rudimentary advanced search feature spec

---
+++ spec/features/advanced_search_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+RSpec.feature 'Advanced search form', :clean, :js do
+  before do
+    # add some stuff to the index
+  end
+
+  scenario 'the advanced search form' do
+    visit '/advanced'
+
+    expect(page).to have_content 'More Search Options'
+
+    # search fields
+    expect(page).to have_css 'input[name="all_fields"]'
+    expect(page).to have_css 'input[name="title"]'
+    expect(page).to have_css 'input[name="author"]'
+    expect(page).to have_css 'input[name="subject"]'
+  end
+end
