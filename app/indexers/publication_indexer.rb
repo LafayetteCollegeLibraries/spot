e21202d11b246cf82f255e7d0691315814efcfbd
adam malantonio <malantoa@lafayette.edu>
2019-02-08 14:18:17 -0500

    add indexing of english-language dates

--- app/indexers/publication_indexer.rb
+++ app/indexers/publication_indexer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class PublicationIndexer < Hyrax::WorkIndexer
   include IndexesRightsStatements
+  include IndexesEnglishLanguageDates
 
   # Overriding the default +Hyrax::DeepIndexingService+ for our own, which
   # doesn't require +Hyrax::BasicMetadata+
