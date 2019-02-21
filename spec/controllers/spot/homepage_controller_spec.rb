1b6f892c1e51f329b827f467c81da796e6cd2c0f
adam malantonio <malantoa@lafayette.edu>
2019-02-21 15:28:04 -0500

    oops, need to specify a different layout for the homepage spec

--- spec/controllers/spot/homepage_controller_spec.rb
+++ spec/controllers/spot/homepage_controller_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Spot::HomepageController do
   describe '#index' do
     it 'renders the homepage layout' do
       get :index
-      expect(response).to render_template('layouts/1_column_no_navbar')
+      expect(response).to render_template('hyrax')
     end
   end
 end
