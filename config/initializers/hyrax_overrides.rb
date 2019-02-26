775519f774f68cced476c1448dcab087df2a3b2d
adam malantonio <malantoa@lafayette.edu>
2019-02-26 17:34:07 -0500

    override the layout of Hyrax::PagesController

--- config/initializers/hyrax_overrides.rb
+++ config/initializers/hyrax_overrides.rb
@@ -19,4 +19,18 @@ Rails.application.config.after_initialize do
 
   # Uses our own CollectionForm, which itself is a subclass of the Hyrax one
   Hyrax::Dashboard::CollectionsController.form_class = Spot::Forms::CollectionForm
+
+  # We've dropped the navbar + banner image that come with Hyrax, and the
+  # 'homepage' layout that the PagesController calls defines content for
+  # this block. By switching to the 'hyrax' layout (which we're using for
+  # the homepage + others), we can drop this component.
+  #
+  # @todo is there a better way to do this?
+  Hyrax::PagesController.class_eval do
+    private
+
+      def pages_layout
+        action_name == 'show' ? 'hyrax' : 'hyrax/dashboard'
+      end
+  end
 end
