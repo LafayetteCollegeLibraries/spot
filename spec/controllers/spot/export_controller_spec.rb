00789da7f4e4c3d0a7f64b086d4a94e49067ecb1
adam malantonio <malantoa@lafayette.edu>
2019-03-08 13:17:30 -0500

    we don't need to actually create exports for the controller spec

--- spec/controllers/spot/export_controller_spec.rb
+++ spec/controllers/spot/export_controller_spec.rb
@@ -4,29 +4,39 @@ require 'fileutils'
 RSpec.describe Spot::ExportController do
   routes { Rails.application.routes }
 
-  before { allow(controller).to receive(:render) }
+  before do
+    allow(controller).to receive(:render)
+    allow(Spot::Exporters::ZippedWorkExporter).to receive(:new).and_return(exporter)
+    allow(File).to receive(:open)
+    allow(ActiveFedora::Base).to receive(:find).with('fs000').and_return(file_set)
+    allow(ActiveFedora::Base).to receive(:find).with('pub000').and_return(work)
+    allow(file_set).to receive(:is_a?).with(FileSet).and_return(true)
+    allow(controller.hyrax)
+      .to receive(:download_path)
+      .with(file_set, locale: nil)
+      .and_return("/downloads/#{file_set.id}")
+  end
+
+  let(:exporter) { instance_double(Spot::Exporters::ZippedWorkExporter, export!: true) }
+  let(:work) { instance_double(Publication, id: 'pub000', etag: 'W/"xxxxxx"') }
+  let(:file_set) { instance_double(FileSet, id: 'fs000') }
 
   describe 'GET #show' do
-    subject(:get_export!) { get :show, params: { id: item_id, format: :zip } }
+    before do
+      allow(controller).to receive(:send_data)
+      get :show, params: { id: item_id, format: :zip }
+    end
 
     context 'when requesting a file_set' do
-      let(:item_id) { FileSet.create.id }
+      let(:item_id) { file_set.id }
 
       it { is_expected.to redirect_to Hyrax::Engine.routes.url_helpers.download_path(item_id) }
     end
 
     context 'when requesting a work' do
-      before { allow(controller).to receive(:send_data) }
-
-      after { FileUtils.rm(cached_file) }
-
-      let(:work) { create(:publication) }
       let(:item_id) { work.id }
-      let(:cached_file) { Rails.root.join('tmp', 'export', "#{item_id}-#{work.etag[3..-2]}.zip") }
 
       it 'calls :send_data' do
-        get_export!
-
         expect(controller).to have_received(:send_data)
       end
     end
