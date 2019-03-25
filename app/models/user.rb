52352bc86b56b2820cfd3fe86c6f389a1d3068d9
adam malantonio <adam.malantonio@gmail.com>
2019-03-25 11:40:19 -0400

    update cas attributes (#186)
    * fix cas attribute keys for surname + email
    * update user spec for cas attributes / add username callback
    * make rubo happy

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,8 @@ class User < ApplicationRecord
 
   devise :cas_authenticatable, :rememberable
 
+  before_save :ensure_username
+
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
   # the account.
@@ -27,18 +29,29 @@ class User < ApplicationRecord
     email
   end
 
-  # Sets up attributes returned from CAS
+  # Sets up attributes returned from CAS. No need to save the object: that's done
+  # via devise_cas_authenticatable.
   #
   # @param [Hash<String => String>] attributes
   # @return [void]
+  # @todo when/if released, capture: memberOf, affiliation, department
   def cas_extra_attributes=(attributes)
     self.username = attributes['uid']
-    self.email = attributes['mail']
-
-    self.display_name = "#{attributes['givenName']} #{attributes['sn']}".strip
-
-    # self.member_of = attributes['memberOf']
-    # self.affiliation = attributes['affiliation'] if attributes['affiliation']
-    # self.department = attributes['department'] if attributes['department']
+    self.email = attributes['email']
+    self.display_name = "#{attributes['givenName']} #{attributes['surname']}".strip
   end
+
+  private
+
+    # Callback to ensure that we store a username, as that's what's used for uniqueness.
+    # We occasionally provide a depositor in some ingest cases, but that relies on the
+    # email address and _not_ the username. We'll capture the username as anything
+    # before the +@+ symbol of the email.
+    #
+    # @return [void]
+    def ensure_username
+      return unless username.blank?
+
+      self.username = email.gsub(/@.*$/, '')
+    end
 end
