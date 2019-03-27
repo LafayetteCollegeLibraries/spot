d8716d3f93b617c78b0044c7d8c53f6f25960360
adam malantonio <malantoa@lafayette.edu>
2019-03-27 18:48:29 -0400

    depositor user needs to be an admin to add an item to a collection

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,9 @@ Rake::Task['hyrax:default_collection_types:create'].invoke
 # create a depositor user
 deposibot = User.find_or_create_by(email: 'dss@lafayette.edu') do |bot|
   bot.display_name = 'DeposiBot'
+
+  # give the depositor user admin access
+  admin_role = Role.find_by_name('admin')
+  bot.roles << admin_role if admin_role
 end
+
