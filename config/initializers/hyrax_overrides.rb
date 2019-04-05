29f34823ca7e181bf2e758e22a8e9def54d378d4
adam malantonio <malantoa@lafayette.edu>
2019-04-05 08:56:29 -0400

    load overrides using :to_prepare callback
    rather than :after_initialize

--- config/initializers/hyrax_overrides.rb
+++ config/initializers/hyrax_overrides.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 #
-# Where possible, we'll try to use the class_attributes provided by the
-# various Hyrax pieces to provide our own functionality. However, this
-# isn't always possible, and we need to either copy files locally to
-# modify or peak into the classes via +class_eval+. Where the latter
-# case is necessary, we'll put the file into +app/overrides+ and load
-# all of them here, after initialization.
-Rails.application.config.after_initialize do
-  # Load all of our overrides/class_evals
-  # (adapted from https://github.com/sciencehistory/chf-sufia/blob/d1c7d58/config/application.rb#L43-L48)
+# Load all of our overrides/class_evals
+# (adapted from https://github.com/sciencehistory/chf-sufia/blob/d1c7d58/config/application.rb#L43-L48)
+#
+# Trying the +:to_prepare+ callback, rather than +:after_initialization+
+# because occasionally the changes (namely class_attribute assignments)
+# are forgotten on a reload. This should only run once in production,
+# but before each request in development.
+Rails.application.config.to_prepare do
   Dir.glob(Rails.root.join('app', 'overrides', '**', '*.rb')) do |c|
     Rails.configuration.cache_classes ? require(c) : load(c)
   end
