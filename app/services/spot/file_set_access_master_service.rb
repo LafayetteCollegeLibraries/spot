8bf967f601b90f42bf102955a42eaea17c130ad9
adam malantonio <malantoa@lafayette.edu>
2019-06-06 12:47:23 -0400

    add comments to the access_master service

--- app/services/spot/file_set_access_master_service.rb
+++ app/services/spot/file_set_access_master_service.rb
@@ -1,4 +1,30 @@
 # frozen_string_literal: true
+#
+# +Hyrax::FileSetDerivativesService+ already handles creating
+# the thumbnail. We just want to create an access master for
+# an image server to be able to retrieve.
+#
+# Note: for now we're going with pyramidal TIFs as a uniform
+# format for our access masters. The community consensus seems
+# to point to JPEG2000s or pyramidal TIFs as access masters.
+# The former seems to require Kakadu (a paid software) to get
+# the best results, while the later seems to be fairly standard.
+#
+# From https://mw2016.museumsandtheweb.com/paper/iiif-unshackle-your-images/:
+#
+# > If you want a free performant option, we would suggest the use
+# > of Pyramidal TIFFs. When used in conjunction with a high-performance
+# > image server, this format will give you the performance required
+# > to implement IIIF.
+#
+# @example basic usage
+#   fs = FileSet.find('abc123def')
+#   working_dir = Hyrax::WorkingDirectory.new(fs.files.first.id, fs.id)
+#   service = Spot::FileSetAccessMasterService.new(fs)
+#   service.create_derivatives(working_dir)
+#   File.exist?(Rails.root.join('tmp/derivatives/ab/c1/23/de/f-access.tif'))
+#   # => true
+#
 module Spot
   class FileSetAccessMasterService
     attr_reader :file_set
@@ -16,15 +42,17 @@ module Spot
       end
     end
 
-    # +Hyrax::FileSetDerivativesService+ already handles creating
-    # the thumbnail. We just want to create an access_master for
-    # an image server to be able to retrieve.
+    # Since we want to pass some extended options to the creation process,
+    # we'll just use MiniMagick, rather than use
+    # Hydra::Derviatives::ImageDerivatives.
     #
     # @param [String,Pathname] filename the src path of the file
     # @return [void]
     def create_derivatives(filename)
       MiniMagick::Tool::Magick.new do |magick|
         magick << filename
+        # note: we need to use an array for each piece of this command;
+        # using a string will cause an error
         magick.merge! %w[-define tiff:tile-geometry=128x128]
         magick << "ptif:#{derivative_url('access.tif')}"
       end
@@ -47,16 +75,6 @@ module Spot
     end
 
     private
-
-      # @return [Array<Hash>]
-      def access_master_outputs
-        [{
-          label: :access,
-          format: 'tif',
-          url: derivative_url('access.tif'),
-          layer: 0
-        }]
-      end
 
       # @return [Class]
       def derivative_path_factory
