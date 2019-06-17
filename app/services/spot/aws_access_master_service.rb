f92990d9f1c8e72d7f296a7f875f8692265d05ea
adam malantonio <malantoa@lafayette.edu>
2019-06-17 15:03:39 -0400

    create aws_access_master_service

---
+++ app/services/spot/aws_access_master_service.rb
@@ -0,0 +1,100 @@
+# frozen_string_literal: true
+require 'tmpdir'
+require 'digest/md5'
+
+module Spot
+  class AwsAccessMasterService
+    attr_reader :file_set
+
+    # @param [FileSet] file_set
+    def initialize(file_set)
+      @file_set = file_set
+    end
+
+    def cleanup_derivatives
+      return nil unless aws_credentials_present?
+
+      client.delete_object({
+        bucket: bucket,
+        key: access_master_filename
+      })
+    end
+
+    # @param [String, Pathname] filename
+    def create_derivatives(filename)
+      return nil unless aws_credentials_present?
+
+      create_access_master(src: filename) { |path| upload_object_to_s3(path) }
+    end
+
+    private
+
+      # @return [String]
+      def access_master_filename
+        "#{file_set.id}-access_master.tif"
+      end
+
+      # Are all the credentials we need present?
+      #
+      # @return [true, false]
+      def aws_credentials_present?
+        %w[
+          AWS_ACCESS_KEY_ID
+          AWS_ACCESS_MASTER_BUCKET
+          AWS_REGION
+          AWS_SECRET_ACCESS_KEY
+        ].all? { |k| ENV[k].present? }
+      end
+
+      # @return [String]
+      def bucket
+        ENV['AWS_ACCESS_MASTER_BUCKET']
+      end
+
+      # Calculates a base64 digest to send with the PUT action
+      # to ensure that our bits make it to AWS intact.
+      #
+      # @param [IO] io
+      # @return [String]
+      def calculate_md5(io)
+        Digest::MD5.new.tap do |md5|
+          io.each { |chunk| md5.update(chunk) }
+          io.rewind
+        end.base64digest
+      end
+
+      # @return [Aws::S3::Client]
+      def client
+        @client ||= Aws::S3::Client.new
+      end
+
+      def create_access_master(src:)
+        Dir.mktmpdir do |tmpdir|
+          out_path = File.join(tmpdir, access_master_filename)
+
+          MiniMagick::Tool::Magick.new do |magick|
+            magick << src
+            # note: we need to use an array for each piece of this command;
+            # using a string will cause an error
+            magick.merge! %w[-define tiff:tile-geometry=128x128]
+            magick << "ptif:#{out_path}"
+          end
+
+          yield out_path if block_given?
+        end
+      end
+
+      # @param [String, Pathname] pathname
+      # @return [void]
+      def upload_object_to_s3(pathname)
+        io = File.open(pathname, 'rb')
+
+        client.put_object({
+          body: io,
+          bucket: bucket,
+          content_md5: calculate_md5(io),
+          key: access_master_filename
+        })
+      end
+  end
+end
