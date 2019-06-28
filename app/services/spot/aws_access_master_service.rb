0264e8d755fb2c3775a771d59e337fb16f001390
adam malantonio <malantoa@lafayette.edu>
2019-06-28 12:01:24 -0400

    add comments / #valid? method

--- app/services/spot/aws_access_master_service.rb
+++ app/services/spot/aws_access_master_service.rb
@@ -1,4 +1,39 @@
 # frozen_string_literal: true
+#
+# Replacement for FileSetAccessMasterService that generates
+# an access master in a similar fashion (maybe that needs to
+# be abstracted out?) but uploads the object to an S3 bucket,
+# where it will be grabbed by the IIIF server.
+#
+# +Hyrax::FileSetDerivativesService+ already handles creating
+# the thumbnail. We just want to create an access master for
+# an image server to be able to retrieve.
+#
+# Note: for now we're going with pyramidal TIFs as a uniform
+# format for our access masters. The community consensus seems
+# to point to JPEG2000s or pyramidal TIFs as access masters.
+# The former seems to require Kakadu (a paid software) to get
+# the best results, while the later seems to be fairly standard.
+#
+# From https://mw2016.museumsandtheweb.com/paper/iiif-unshackle-your-images/:
+#
+# > If you want a free performant option, we would suggest the use
+# > of Pyramidal TIFFs. When used in conjunction with a high-performance
+# > image server, this format will give you the performance required
+# > to implement IIIF.
+#
+# Note: this requires the following environment variables to be defined:
+#   - AWS_ACCESS_KEY_ID
+#   - AWS_ACCESS_MASTER_BUCKET
+#   - AWS_REGION
+#   - AWS_SECRET_ACCESS_KEY
+#
+# @example basic usage
+#   fs = FileSet.find('abc123def')
+#   working_dir = Hyrax::WorkingDirectory.new(fs.files.first.id, fs.id)
+#   service = Spot::AwsAccessMasterService.new(fs)
+#   service.create_derivatives(working_dir)
+
 require 'tmpdir'
 require 'digest/md5'
 
@@ -6,11 +41,14 @@ module Spot
   class AwsAccessMasterService
     attr_reader :file_set
 
+    delegate :uri, :mime_type, to: :file_set
+
     # @param [FileSet] file_set
     def initialize(file_set)
       @file_set = file_set
     end
 
+    # @return [void]
     def cleanup_derivatives
       return nil unless aws_credentials_present?
 
@@ -18,10 +56,16 @@ module Spot
     end
 
     # @param [String, Pathname] filename
+    # @return [void]
     def create_derivatives(filename)
       return nil unless aws_credentials_present?
 
       create_access_master(src: filename) { |path| upload_object_to_s3(path) }
+    end
+
+    # @return [true, false]
+    def valid?
+      image_mime_types.include? mime_type
     end
 
     private
@@ -79,6 +123,11 @@ module Spot
 
           yield out_path if block_given?
         end
+      end
+
+      # @return [Array<String>]
+      def image_mime_types
+        FileSet.image_mime_types
       end
 
       # @param [String, Pathname] pathname
