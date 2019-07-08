919c9afc114bcb6e77b697d5f1ad5f93c354cd70
adam malantonio <malantoa@lafayette.edu>
2019-07-08 17:41:19 -0400

    use FITS_SERVLET_URL for okcomputer check

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -20,8 +20,8 @@ fcrepo_uri = URI.parse(fcrepo_config['url']).tap do |uri|
   uri.userinfo = "#{fcrepo_config['user']}:#{fcrepo_config['password']}"
 end.to_s
 
-# out of the box, "application running?" and "active record working?" checks
-# are run. we need to register the others
+# out of the box, "application running?" and "active record working?"
+# checks are run. we need to register the others
 OkComputer::Registry.register 'solr', OkComputer::SolrCheck.new(solr_config['url'])
 OkComputer::Registry.register 'redis', OkComputer::RedisCheck.new(redis_config)
 OkComputer::Registry.register 'fedora', OkComputer::HttpCheck.new(fcrepo_uri)
@@ -30,7 +30,9 @@ sidekiq_config[:queues].each do |queue|
   OkComputer::Registry.register "sidekiq :#{queue}", OkComputer::SidekiqLatencyCheck.new(queue.to_sym)
 end
 
-if ENV['FITS_SERVLET_HOST'].present?
-  fits_url = "#{ENV['FITS_SERVLET_HOST']}/fits/version"
+# we need to use the /version endpoint for a healthcheck as the /examine one
+# will throw a 400 Bad Request if there's no file to examine
+if ENV['FITS_SERVLET_URL'].present?
+  fits_url = ENV['FITS_SERVLET_URL'].gsub(%r{/fits/examine$}, '/fits/version')
   OkComputer::Registry.register 'fits (servlet)', OkComputer::HttpCheck.new(fits_url)
 end
