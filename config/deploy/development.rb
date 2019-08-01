beabab263498758d6b3c7f69833bb4532651addb
adam malantonio <malantoa@lafayette.edu>
2019-08-01 13:31:43 -0400

    move sidekiq concurrency to capistrano configs
    see https://github.com/mperham/sidekiq/issues/3090. looks like
    dotenv values aren't loaded when sidekiq is started (especially
    through our capistrano incantation), so we've moved variable
    configuration (ex. concurrency) to the capistrano files rather
    than ENV values.

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -3,4 +3,7 @@
 # Our development server. Everything lives here as an attempt to
 # prove that all of the pieces work together more than trying to
 # host a lot of objects.
+
+set :sidekiq_concurrency, 4
+
 server 'nostromo0-0.dev.lafayette.edu', user: 'deploy', roles: %w[app db solr jobs web]
--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -4,6 +4,8 @@
 # the stage application will most likely be from our `develop` branch
 set :branch, ENV.fetch('BRANCH') { 'develop' }
 
+set :sidekiq_concurrency, 10
+
 server 'nostromo0-0.stage.lafayette.edu',
        user: 'deploy',
        roles: %w[app db web]
