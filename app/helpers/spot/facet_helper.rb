39066487ec2dc724b84264707dbac754914cddd6
adam malantonio <malantoa@lafayette.edu>
2019-08-20 08:40:00 -0400

    add an admin facet for visibility (#253)

--- app/helpers/spot/facet_helper.rb
+++ app/helpers/spot/facet_helper.rb
@@ -3,18 +3,25 @@
 # Helpers to split out facets useful to admins from the regular ("general") ones
 module Spot
   module FacetHelper
-    # render partials for the majority of our partials
-    #
-    # @return [String]
-    def render_general_facet_partials
-      render_facet_partials(general_facet_names)
-    end
-
     # render only the admin facet partials
     #
     # @return [String]
     def render_admin_facet_partials
       render_facet_partials(admin_facet_names)
+    end
+
+    # Wraps our visibility facets in the Hyrax::PermissionBadge HTML
+    #
+    # @return [String]
+    def render_catalog_visibility_facet(visibility)
+      Hyrax::PermissionBadge.new(visibility).render
+    end
+
+    # render partials for the majority of our partials
+    #
+    # @return [String]
+    def render_general_facet_partials
+      render_facet_partials(general_facet_names)
     end
 
     # Returns only the facets where the option +:admin+ is truthy
