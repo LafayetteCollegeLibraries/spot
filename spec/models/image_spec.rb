8a22afb64c935fc3123a7c9e0f4c4a09113ce147
adam malantonio <malantoa@lafayette.edu>
2019-09-09 20:35:22 -0400

    update image_spec

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,11 +1,39 @@
 # frozen_string_literal: true
-
-# Generated via
-#  `rails generate hyrax:work Image`
-require 'rails_helper'
-
 RSpec.describe Image do
-  it "has tests" do
-    skip "Add your tests here"
+  subject { described_class.new }
+
+  it_behaves_like 'a model with hyrax core metadata'
+
+  [
+    [:subtitle, 'http://purl.org/spar/doco/Subtitle'],
+    [:title_alternative, RDF::Vocab::DC.alternative],
+    [:publisher, RDF::Vocab::DC11.publisher],
+    [:repository, 'http://ns.nature.com/terms/#repository'],
+    [:resource_type, RDF::Vocab::DC.type],
+    [:physical_medium, RDF::Vocab::DC.PhysicalMedium],
+    [:original_item_extent, RDF::Vocab::DC.extent],
+    [:language, RDF::Vocab::DC11.language],
+    [:description, RDF::Vocab::DC11.description],
+    [:ethnic_group, 'http://sparql.cwrc.ca/ontologies/cwrc#hasEthnicity'],
+    [:inscription, 'http://dbpedia.org/ontology/inscription'],
+    [:date, RDF::Vocab::DC.date],
+    [:date_scope_note, RDF::Vocab::SKOS.scopeNote],
+    [:date_associated, 'https://d-nb.info/standards/elementset/gnd#associatedDate'],
+    [:creator, RDF::Vocab::DC11.creator],
+    [:contributor, RDF::Vocab::DC11.contributor],
+    [:related_resource, RDF::RDFS.seeAlso],
+    [:subject, RDF::Vocab::DC11.subject],
+    [:subject_ocm, 'https://hraf.yale.edu/resources/reference/outline-of-cultural-materials'],
+    [:keyword, RDF::Vocab::SCHEMA.keywords],
+    [:location, RDF::Vocab::DC.spatial],
+    [:rights_statement, RDF::Vocab::EDM.rights],
+    [:rights_holder, RDF::Vocab::DC.rightsHolder],
+    [:identifier, RDF::Vocab::DC.identifier],
+    [:requested_by, 'http://rdf.myexperiment.org/ontologies/base/has-requester'],
+    [:research_assistance, 'http://www.rdaregistry.info/Elements/a/#P50265'],
+    [:donor, RDF::Vocab::DC.provenance],
+    [:note, RDF::Vocab::SKOS.note]
+  ].each do |(prop, uri)|
+    it { is_expected.to have_editable_property(prop).with_predicate(uri) }
   end
 end
