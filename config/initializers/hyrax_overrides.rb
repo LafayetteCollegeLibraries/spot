df18709d88cbd8482338bb9851ec9bf07e41cd78
adam malantonio <malantoa@lafayette.edu>
2019-09-24 16:37:11 -0400

    we no longer rely on hyrax for rendering 'pages'

--- config/initializers/hyrax_overrides.rb
+++ config/initializers/hyrax_overrides.rb
@@ -13,21 +13,6 @@ Rails.application.config.to_prepare do
     ::Hyrax::FileSetDerivativesService
   ]
 
-  # We've dropped the navbar + banner image that come with Hyrax, and the
-  # 'homepage' layout that the PagesController calls defines content for
-  # this block. By switching to the 'hyrax' layout (which we're using for
-  # the homepage + others), we can drop this component.
-  #
-  # @todo is there a better way to do this?
-  Hyrax::PagesController.class_eval do
-    private
-
-      # @return [String]
-      def pages_layout
-        action_name == 'show' ? 'hyrax' : 'hyrax/dashboard'
-      end
-  end
-
   # see above
   Hyrax::ContactFormController.class_eval { layout 'hyrax' }
 end
