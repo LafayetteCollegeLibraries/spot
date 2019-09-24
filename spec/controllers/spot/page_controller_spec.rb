e443636a072a737019dd6598a2785c5d09ad5ed5
adam malantonio <malantoa@lafayette.edu>
2019-09-24 16:49:24 -0400

    add terms_of_use page to spec / use main_app for _footer links

--- spec/controllers/spot/page_controller_spec.rb
+++ spec/controllers/spot/page_controller_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Spot::PageController do
   # currently, our page controller only provides
   # methods to render the associated templates,
   # rails style.
-  %w[about help].each do |page|
+  %w[about help terms_of_use].each do |page|
     describe "##{page}" do
       it "renders the #{page}.html.erb page" do
         get page.to_sym
