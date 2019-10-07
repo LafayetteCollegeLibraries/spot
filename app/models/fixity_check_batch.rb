748c64757e8c9c86f35df8b286496ec461a4b37f
adam malantonio <malantoa@lafayette.edu>
2019-10-07 10:13:29 -0400

    rubo updates

--- app/models/fixity_check_batch.rb
+++ app/models/fixity_check_batch.rb
@@ -1,15 +1,29 @@
 # frozen_string_literal: true
 class FixityCheckBatch < ApplicationRecord
-  class Summary < Struct.new(:success, :failed, :failed_item_ids, :total_time)
+  class Summary
+    attr_reader :success, :failed, :failed_item_ids, :total_time
+
+    def initialize(success:, failed:, failed_item_ids:, total_time:)
+      @success = success
+      @failed = failed
+      @failed_item_ids = failed_item_ids
+      @total_time = total_time
+    end
+
+    def to_h
+      { success: success, failed: failed,
+        failed_item_ids: failed_item_ids, total_time: total_time }
+    end
+
     # Loads a Summary object from a JSON string
     #
     # @param [String] json
     # @return [FixityCheckBatch::Summary]
     def self.load(json)
       return nil if json.blank?
-      parsed = JSON.parse(json)
+      parsed = JSON.parse(json).with_indifferent_access
 
-      new(parsed['success'], parsed['failed'], parsed['failed_item_ids'], parsed['total_time'])
+      new(parsed)
     end
 
     # Turns a Summary into JSON.
@@ -22,22 +36,16 @@ class FixityCheckBatch < ApplicationRecord
 
       case obj
       when self
-        JSON.dump(success: obj.success,
-                  failed: obj.failed,
-                  failed_item_ids: obj.failed_item_ids,
-                  total_time: obj.total_time)
+        JSON.dump(obj.to_h)
       when Hash
-        JSON.dump(success: obj[:success],
-                  failed: obj[:failed],
-                  failed_item_ids: obj[:failed_item_ids],
-                  total_time: obj[:total_time])
+        JSON.dump(new(obj).to_h)
       else
         raise StandardException, "Expected #{self} or Hash, got #{obj.class}"
       end
     end
   end
 
-  has_and_belongs_to_many :checksum_audit_logs
+  has_and_belongs_to_many :checksum_audit_logs # rubocop:disable Rails/HasAndBelongsToMany
   serialize :summary, Summary
 
   delegate :success, :failed, :failed_item_ids, :total_time, to: :summary
