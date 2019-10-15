c296a9f0fd73349f852dd67ba40707cdc2b0b568
adam malantonio <malantoa@lafayette.edu>
2019-10-15 11:06:41 -0400

    provide a google_analytics_id via env variable (#302)
    * provide a google_analytics_id via env variable
    * add google analytics id to .env.sample

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -46,7 +46,7 @@ Hyrax.config do |config|
   # config.analytics = false
 
   # Google Analytics tracking ID to gather usage statistics
-  # config.google_analytics_id = 'UA-99999999-1'
+  config.google_analytics_id = ENV.fetch('GOOGLE_ANALYTICS_ID', nil)
 
   # Date you wish to start collecting Google Analytic statistics for
   # Leaving it blank will set the start date to when ever the file was uploaded by
