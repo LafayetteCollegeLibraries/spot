baac2590a487becc3f0891105f218f7c869e72db
adam malantonio <malantoa@lafayette.edu>
2019-10-16 13:55:03 -0400

    Update help page (#304)
    * add content to help page
    * compress help images + style them on the page

---
+++ app/helpers/spot/page_helper.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+module Spot
+  module PageHelper
+    def repository_librarian_mailto_link
+      link_to(repository_librarian_email, "mailto:#{repository_librarian_email}").html_safe
+    end
+
+    # @todo put this into a config file
+    def repository_librarian_email
+      'egloffn@lafayette.edu'
+    end
+  end
+end
