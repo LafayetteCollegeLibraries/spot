5f9ec197a2faf0a744261cf096ac67f63169cb11
adam malantonio <malantoa@lafayette.edu>
2019-11-22 13:02:50 -0500

    Publication#date_available modifications (#356)
    * apply date_available via actor_stack
    * remove date_available from the publication form

--- spec/actors/hyrax/actors/publication_actor_spec.rb
+++ spec/actors/hyrax/actors/publication_actor_spec.rb
@@ -9,9 +9,10 @@ RSpec.describe Hyrax::Actors::PublicationActor do
   let(:attributes) { {} }
   let(:env) { Hyrax::Actors::Environment.new(work, ability, attributes) }
 
+  before { allow(work).to receive(:save) }
+
   describe '#apply_deposit_date' do
     before do
-      allow(work).to receive(:save)
       allow(Hyrax::TimeService).to receive(:time_in_utc).and_return(time_value)
     end
 
@@ -46,6 +47,42 @@ RSpec.describe Hyrax::Actors::PublicationActor do
           .to change { work.date_uploaded }
           .from(date_uploaded)
           .to(DateTime.parse(date_uploaded).utc)
+      end
+    end
+  end
+
+  describe '#apply_date_available' do
+    context 'when a date_available value is present' do
+      let(:attributes) { { date_available: ['2019-11-22'] } }
+
+      it 'does not update the value' do
+        expect { actor }.not_to change { work.date_available }
+      end
+    end
+
+    context 'when no date_available value is present and the work is not under embargo' do
+      let(:todays_date) { Time.zone.now.strftime('%Y-%m-%d') }
+
+      # no idea why the +expect { actor }.to change .....+ syntax keeps
+      # failing for this, but explicitly checking works?
+      it 'updates the value to today\'s date' do
+        expect(work.date_available).to eq []
+        actor
+        expect(work.date_available).to eq [todays_date]
+      end
+    end
+
+    context 'when an embargo is set for the work' do
+      before { allow(work).to receive(:embargo).and_return embargo }
+
+      let(:embargo) { instance_double(Hydra::AccessControls::Embargo, embargo_release_date: tomorrow_time) }
+      let(:tomorrow_time) { Time.zone.tomorrow }
+      let(:tomorrow) { tomorrow_time.strftime('%Y-%m-%d') }
+
+      it 'updates the value to match the embargo' do
+        expect(work.date_available).to eq []
+        actor
+        expect(work.date_available).to eq [tomorrow]
       end
     end
   end
