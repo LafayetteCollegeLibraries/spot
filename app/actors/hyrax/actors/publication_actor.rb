0de5a4285f15acb38b354c6fc52499307b834dbe
adam malantonio <malantoa@lafayette.edu>
2019-11-22 12:28:38 -0500

    apply date_available via actor_stack

--- app/actors/hyrax/actors/publication_actor.rb
+++ app/actors/hyrax/actors/publication_actor.rb
@@ -9,9 +9,42 @@ module Hyrax
         # Overrides the BaseActor method to allow us to stuff in
         # `date_uploaded` values where necessary.
         #
+        # @param [Hyrax::Actors::Environment] env
         # @return [void]
         def apply_deposit_date(env)
           env.curation_concern.date_uploaded = get_date_uploaded_value(env)
+        end
+
+        # Overrides the BaseActor method so that we can apply a +date_available+
+        # value to the item.
+        #
+        # @param [Hyrax::Actors::Environment] env
+        # @return [void]
+        # @see {#apply_date_available}
+        def apply_save_data_to_curation_concern(env)
+          super
+
+          apply_date_available(env)
+        end
+
+        # Allows us to apply a +date_available+ value to a work. The property
+        # is similar to +date_uploaded+, but accounts for embargoes where present.
+        # As the method calling this ({#apply_save_data_to_curation_concern}) is
+        # run on both +#create+ and +#update+, we'll skip this if the value has
+        # already been provided.
+        #
+        # @param [Hyrax::Actors::Environment] env
+        # @return [void]
+        def apply_date_available(env)
+          return unless env.curation_concern.date_available.empty?
+
+          embargo = env.curation_concern.embargo
+          env.curation_concern.date_available =
+            if embargo.present?
+              [embargo.embargo_release_date.strftime('%Y-%m-%d')]
+            else
+              [Time.zone.now.strftime('%Y-%m-%d')]
+            end
         end
 
         # @param [Hyrax::Actors::Environment] env
