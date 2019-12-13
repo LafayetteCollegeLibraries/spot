2617d68d595b5ec763dd9a0cd93037b6fc9ae49f
adam malantonio <malantoa@lafayette.edu>
2019-12-13 10:11:10 -0500

    register + add form fields for remaining suggested properties

--- config/initializers/qa.rb
+++ config/initializers/qa.rb
@@ -3,6 +3,15 @@
 # QuestioningAuthority configuration
 
 # our Solr suggestion authorities
-Qa::Authorities::Local.register_subauthority('keyword', 'Qa::Authorities::SolrSuggest')
-Qa::Authorities::Local.register_subauthority('source', 'Qa::Authorities::SolrSuggest')
-Qa::Authorities::Local.register_subauthority('publisher', 'Qa::Authorities::SolrSuggest')
+solr_suggestion_authorities = %w[
+  keyword
+  name
+  organization
+  physical_medium
+  publisher
+  source
+]
+
+solr_suggestion_authorities.each do |subauth|
+  Qa::Authorities::Local.register_subauthority(subauth, 'Qa::Authorities::SolrSuggest')
+end
