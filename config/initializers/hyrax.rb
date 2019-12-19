2ad90e2f027492d1fcbdad0c808ad5eb1cbfa4e8
adam malantonio <malantoa@lafayette.edu>
2019-12-19 12:39:18 -0500

    use pcdm:memberOf predicate for admin-sets

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -21,7 +21,7 @@ Hyrax.config do |config|
   # Which RDF term should be used to relate objects to an admin set?
   # If this is a new repository, you may want to set a custom predicate term here to
   # avoid clashes if you plan to use the default (dct:isPartOf) for other relations.
-  # config.admin_set_predicate = ::RDF::DC.isPartOf
+  config.admin_set_predicate = ::RDF::URI.new('http://pcdm.org/models#memberOf')
 
   # Email recipient of messages sent via the contact form
   config.contact_email = "dss@lafayette.edu"
