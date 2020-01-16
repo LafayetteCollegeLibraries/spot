87357762a58ee5a1171de1a69f42a60c37155692
adam malantonio <malantoa@lafayette.edu>
2020-01-16 14:14:56 -0500

    start woodsworth-images mapper

---
+++ app/services/mappers/woodsworth_images_mapper.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+module Spot::Mappers
+  class WoodsworthImagesMapper < BaseEaicMapper
+    self.fields_map = {
+      date_scope_note: 'description.indicia',
+      description: 'description.critical',
+      language: 'language',
+      physical_medium: 'format.medium',
+      publisher: 'creator.company',
+      rights_statement: 'rights.digital',
+      subject_ocm: 'subject.ocm',
+    }
+
+    def fields
+      super + [
+        :date,
+        :date_associated,
+        :inscription,
+        :location,
+        :resource_type,
+        :title,
+        :title_alternative
+      ]
+    end
+
+    def date
+      edtf_range_for('date.artifact.lower', 'date.artifact.upper')
+    end
+
+    def date_associated
+      edtf_range_for('date.image.lower', 'date.image.upper')
+    end
+
+    def inscription
+      [['description.inscription.english', :en], ['description.inscription.japanese', :ja]]
+        .map { |(field, tag)| field_to_tagged_literals(field, tag) }
+        .flatten
+        .compact
+    end
+
+    def location
+      merge_fields('coverage.location', 'coverage.location.country')
+    end
+
+    def related_resource
+      merge_fields('description.citation', 'relation.seealso')
+    end
+
+    def resource_type
+      metadata.fetch('resource.type', ['Image'])
+    end
+  end
+end
--- app/services/spot/mappers/base_eaic_mapper.rb
+++ app/services/spot/mappers/base_eaic_mapper.rb
@@ -3,5 +3,25 @@
 module Spot::Mappers
   class BaseEaicMapper < BaseMapper
     include LanguageTaggedTitles
+
+    private
+
+      # @param [String] start_date_field
+      # @param [String] end_date_field
+      # @return [String] parsed EDTF range string
+      def etdf_range_for(start_date_field, end_date_field)
+        start_date = metadata.fetch(start_date_field, '')
+        end_date = metadata.fetch(end_date_field, '')
+
+        Date.edtf([start_date, end_date].reject(&:blank?).join('/')).to_s
+      end
+
+      # Helper method to group the values for multiple fields into one place.
+      #
+      # @param [Array<String>] *names field names to merge
+      # @return [Array<String>]
+      def merge_fields(*names)
+        names.map { |name| metadata[name] }.flatten.compact
+      end
   end
 end
