34d983e3adcb637ed31752abf576bf4884c24274
adam malantonio <malantoa@lafayette.edu>
2020-02-06 09:25:14 -0500

    add mapper for lewis-postcards

---
+++ app/services/spot/mappers/lewis_postcards_mapper.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+module Spot::Mappers
+  class LewisPostcardsMapper < BaseEaicMapper
+    self.fields_map = {
+      creator: 'creator.maker',
+      date_scope_note: 'description.indicia',
+      keyword: 'relation.ispartof',
+      original_item_extent: 'format.extant',
+      physical_medium: 'format.medium',
+      publisher: 'creator.company',
+      related_resource: 'relation.seealso',
+      research_assistance: 'contributor',
+      subject_ocm: 'subject.ocm'
+    }
+
+    def fields
+      super + [
+        :date_associated,
+        :inscription,
+
+        # inherited methods
+        :description,
+        :identifier,
+        :location,
+        :rights_statement,
+        :subject,
+        :title,
+        :title_alternative
+      ]
+    end
+
+    def date_associated
+      edtf_ranges_for('date.image.lower', 'date.image.upper')
+    end
+
+    def inscription
+      [
+        ['description.inscription.english', :en],
+        ['description.inscription.japanese', :ja],
+        ['description.text.english', :en],
+        ['description.text.japanese', :ja]
+      ].inject([]) { |pool, (field, lang)| pool + field_to_tagged_literals(field, lang) }
+    end
+  end
+end
