dc8b5e25700a8a8b2c7881dc9106ca0c0c7e4679
adam malantonio <malantoa@lafayette.edu>
2020-02-18 13:07:50 -0500

    add gc-iroha mapper

---
+++ app/services/spot/mappers/gc_iroha_mapper.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+module Spot::Mappers
+  class GcIrohaMapper < BaseEaicMapper
+    self.fields_map = {
+      keyword: 'relation.ispartof',
+      physical_medium: 'format.medium',
+      publisher: 'creator.company',
+      research_assistance: 'contributor',
+      subject_ocm: 'subject.ocm'
+    }
+
+    def fields
+      super + [
+        :inscription,
+
+        :date,
+        :description,
+        :identifier,
+        :resource_type,
+        :rights_statement,
+        :title,
+        :title_alternative
+      ]
+    end
+
+    def inscription
+      [
+        ['description.inscription.japanese', :ja],
+        ['description.text.japanese', :ja]
+      ].inject([]) { |pool, (field, lang)| pool + field_to_tagged_literals(field, lang) }
+    end
+
+    def resource_type
+      ['Image']
+    end
+  end
+end
