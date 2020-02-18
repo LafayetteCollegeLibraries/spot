67d92f1e38d93e9a8fa08432065c3c617ed29829
adam malantonio <malantoa@lafayette.edu>
2020-02-18 13:25:27 -0500

    add pa-koshitsu mapper

---
+++ app/services/spot/mappers/pa_koshitsu_mapper.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+module Spot::Mappers
+  class PaKoshitsuMapper < BaseEaicMapper
+    self.fields_map = {
+      creator: 'creator.maker',
+      date_scope_note: 'description.indicia',
+      keyword: 'relation.ispartof',
+      physical_medium: 'format.medium',
+      publisher: 'creator.company',
+      related_resource: 'description.citation',
+      research_assistance: 'contributor',
+      subject_ocm: 'subject.ocm'
+    }
+
+    def fields
+      super + [
+        :inscription,
+
+        :date,
+        :date_associated,
+        :description,
+        :identifier,
+        :location,
+        :rights_statement,
+        :title,
+        :title_alternative
+      ]
+    end
+
+    def inscription
+      field_to_tagged_literals('description.text.japanese', :ja)
+    end
+  end
+end
