7e76f5da5870b33c07bd8e2de0dbacb635b2cd46
adam malantonio <malantoa@lafayette.edu>
2020-06-18 12:32:13 -0400

    don't use const in the js yet, i guess

--- app/assets/javascripts/multi-auth-input.js
+++ app/assets/javascripts/multi-auth-input.js
@@ -1,5 +1,5 @@
 // instantiates the multi-auth controlled vocabulary input when we're on the work form.
-const MultiAuthControlledVocabulary = require('hyrax/editor/multi_auth_controlled_vocabulary');
+let MultiAuthControlledVocabulary = require('hyrax/editor/multi_auth_controlled_vocabulary');
 
 $(document).ready(function () {
   // don't bother setting up these additional inputs if there's not a form on the page
