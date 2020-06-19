c6ce886b23327b4059e647e71dba49dbd521fe41
adam malantonio <malantoa@lafayette.edu>
2020-06-19 11:22:31 -0400

    namespace our multi-auth js to 'spot'

--- app/assets/javascripts/multi-auth-input.js
+++ app/assets/javascripts/multi-auth-input.js
@@ -1,5 +1,5 @@
 // instantiates the multi-auth controlled vocabulary input when we're on the work form.
-var MultiAuthControlledVocabulary = require('hyrax/editor/multi_auth_controlled_vocabulary');
+var MultiAuthControlledVocabulary = require('spot/editor/multi_auth_controlled_vocabulary');
 
 $(document).ready(function () {
   // don't bother setting up these additional inputs if there's not a form on the page
