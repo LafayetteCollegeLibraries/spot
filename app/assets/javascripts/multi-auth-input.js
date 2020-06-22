8370808459f011fdec128cbef3e5acdf1959ab1f
adam malantonio <malantoa@lafayette.edu>
2020-06-22 10:16:49 -0400

    put multi-auth-input.js content into Blacklight.onLoad

--- app/assets/javascripts/multi-auth-input.js
+++ app/assets/javascripts/multi-auth-input.js
@@ -1,18 +1,20 @@
-// instantiates the multi-auth controlled vocabulary input when we're on the work form.
-var MultiAuthControlledVocabulary = require('spot/editor/multi_auth_controlled_vocabulary');
+(function ($) {
+  // instantiates the multi-auth controlled vocabulary input when we're on the work form.
+  var MultiAuthControlledVocabulary = require('spot/editor/multi_auth_controlled_vocabulary');
 
-$(document).ready(function () {
-  // don't bother setting up these additional inputs if there's not a form on the page
-  var $formElement = $('[data-behavior="work-form"]');
-  if ($formElement.length === 0) { return; }
+  Blacklight.onLoad(function () {
+    // don't bother setting up these additional inputs if there's not a form on the page
+    var $formElement = $('[data-behavior="work-form"]');
+    if ($formElement.length === 0) { return; }
 
-  // the work-type
-  var paramKey = $formElement.data('paramKey');
+    // the work-type
+    var paramKey = $formElement.data('paramKey');
 
-  $formElement
-    .find('.form-group.multi_auth_controlled_vocabulary')
-    .each((_idx, field) => {
-      console.log('adding multi-auth cv to our field', field)
-      return new MultiAuthControlledVocabulary(field, paramKey)
-    });
-});
+    $formElement
+      .find('.form-group.multi_auth_controlled_vocabulary')
+      .each((_idx, field) => {
+        console.log('adding multi-auth cv to our field', field)
+        return new MultiAuthControlledVocabulary(field, paramKey)
+      });
+  });
+})(jQuery);
