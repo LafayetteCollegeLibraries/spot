637c2e34ce56d760f68bafa644342141df1d6ae5
adam malantonio <malantoa@lafayette.edu>
2020-07-08 16:44:13 -0400

    first pass at parent collection membership

---
+++ app/actors/spot/actors/collections_membership_actor.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+module Spot
+  module Actors
+    class CollectionsMembershipActor < ::Hyrax::Actors::CollectionsMembershipActor
+      private
+
+        def add(env, id)
+          collection = ::Collection.find(id)
+          collection.reindex_extent = Hyrax::Adapters::NestingIndexAdapter::LIMITED_REINDEX
+
+          return unless env.current_ability.can?(:deposit, collection)
+
+          collection_ids = env.curation_concern.member_of_collections.map(&:id)
+          collection_stack = [collection]
+
+          while collection_stack.size > 0
+            col = collection_stack.shift
+
+            unless collection_ids.include?(col.id)
+              collection_ids << col.id
+              env.curation_concern.member_of_collections << col
+              collection_stack += col.member_of_collections
+            end
+          end
+        end
+    end
+  end
+end
