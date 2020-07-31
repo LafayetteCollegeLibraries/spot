2ddb11c9273adbf65e623932433b6652e0c00394
adam malantonio <malantoa@lafayette.edu>
2020-07-31 09:58:07 -0400

    add shared_example for image_derivatives

---
+++ spec/support/shared_examples/image_derivatives.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+RSpec.shared_examples 'it exports image derivatives' do
+  subject(:derivative_options) { presenter.image_derivative_options }
+
+  let(:presenter) { described_class.new(solr_doc, ability, nil) }
+  let(:solr_document) { SolrDocument.new(work.to_solr) }
+  let(:work) { build(work_klass.downcase.to_sym, id: 'work-id') }
+  let(:work_klass) { described_class.name.split('::').last.gsub(/Presenter$/, '') }
+  let(:url_builder) { ->(size, label) { "http://localhost/iiif/2/representative/full/#{size}/0/default.jpg?response-content-disposition=attachment%3B%20work-id-#{label}.jpg" } }
+
+  before do
+    allow(presenter).to receive(:representative_id).and_return('representative')
+    allow(presenter).to receive(:id).and_return(work.id)
+  end
+
+  it 'returns an array of labels and iiif urls' do
+    expect(derivative_options).to eq [
+      ['Full Size', url_builder.call('full', 'full')],
+      ['Large (1200px)', url_builder.call('!1200,1200', 'large')],
+      ['Medium (900px)', url_builder.call('!900,900', 'medium')],
+      ['Small (600px)', url_builder.call('!600,600', 'small')]
+    ]
+  end
+end
