5f5bbb7d251f144f1e185993f481186abc32f814
adam malantonio <malantoa@lafayette.edu>
2020-08-12 10:56:08 -0400

    use solr document fields for iiif manifest (#584)
    * use *_label fields for iiif manifests
    
    * add locales for more *_label fields

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -149,8 +149,8 @@ Hyrax.config do |config|
   # Fields to display in the IIIF metadata section
   config.iiif_metadata_fields = %i[
     title subtitle title_alternative creator contributor date date_issued
-    abstract description inscription subject subject_ocm keyword language_label
-    location standard_identifier rights_holder rights_statement
+    abstract description inscription subject_label subject_ocm keyword language_label
+    location_label standard_identifier rights_holder rights_statement_label
   ]
 
   # Should a button with "Share my work" show on the front page to all users (even those not logged in)?
