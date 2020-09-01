1e1ec0c2e61b9980e60b79b9234e669b51c5217f
adam malantonio <malantoa@lafayette.edu>
2020-09-01 16:16:55 -0400

    add resource_type to lewis-postcards mapper (#588)

--- spec/services/spot/mappers/lewis_postcards_mapper_spec.rb
+++ spec/services/spot/mappers/lewis_postcards_mapper_spec.rb
@@ -102,6 +102,14 @@ RSpec.describe Spot::Mappers::LewisPostcardsMapper do
     it_behaves_like 'a mapped field'
   end
 
+  describe '#resource_type' do
+    subject { mapper.resource_type }
+
+    let(:field) { 'resource.type' }
+
+    it_behaves_like 'a mapped field'
+  end
+
   describe '#subject_ocm' do
     subject { mapper.subject_ocm }
 
