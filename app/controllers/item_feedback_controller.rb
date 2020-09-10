884ec62b9ea6a80feeaba0264a7cd4863417cca3
adam malantonio <malantoa@lafayette.edu>
2020-09-10 13:44:39 -0400

    use correct User field for name

--- app/controllers/item_feedback_controller.rb
+++ app/controllers/item_feedback_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class ItemFeedbackController < ApplicationController
   def submit
-    user = User.find_or_initialize_by(email: feedback_params[:email], name: feedback_params[:name])
+    user = User.find_or_initialize_by(email: feedback_params[:email], display_name: feedback_params[:name])
     item = SolrDocument.find(feedback_params[:item_id])
 
     ItemFeedbackMailer.with(item: item, comment: feedback_params[:comment], user: user).deliver_later
