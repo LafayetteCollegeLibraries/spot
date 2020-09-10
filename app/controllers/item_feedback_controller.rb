c9514c4b404b4eba3d6de611cd6e18da0a548a46
adam malantonio <malantoa@lafayette.edu>
2020-09-10 13:18:48 -0400

    trying the mailer

--- app/controllers/item_feedback_controller.rb
+++ app/controllers/item_feedback_controller.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 class ItemFeedbackController < ApplicationController
   def submit
+    user = User.find_or_initialize_by(email: feedback_params[:email], name: feedback_params[:name])
+    item = SolrDocument.find(feedback_params[:item_id])
+
+    ItemFeedbackMailer.with(item: item, comment: feedback_params[:comment], user: user).deliver_later
+
     respond_to { |format| format.js }
   end
+
+  private
+
+    def feedback_params
+      params.require(:item_feedback).permit(:email, :comment, :item_id)
+    end
 end
---
+++ app/mailers/item_feedback_mailer.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+class ItemFeedbackMailer < ApplicationMailer
+  # @todo set as ENV value
+  default to: 'malantoa@lafayette.edu'
+
+  def feedback
+    @item = params[:item]
+    @user_email = params[:user].email
+    @user_name = params[:user].name
+    @comment = params[:comment]
+
+    mail(from: from_address, subject: "Item Feedback Submission for #{@item.id}")
+  end
+
+  private
+
+    # @return [String]
+    def from_address
+      return @user_email unless @user_name.present?
+
+      "#{@user_name} <#{@user_email}>"
+    end
+end
