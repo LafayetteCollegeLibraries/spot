9ec1b586c87acd28eeb842d6462bd1e722d38312
adam malantonio <malantoa@lafayette.edu>
2020-09-15 14:44:05 -0400

    cleanup our version display logic

--- config/initializers/spot_constants.rb
+++ config/initializers/spot_constants.rb
@@ -6,22 +6,10 @@
 #
 # copied from:
 #   https://curationexperts.github.io/playbook/every_project/git_sha.html
-
 revisions_logfile = '/var/www/spot/revisions.log'
 
-GIT_SHA =
-  if Rails.env.production? && File.exist?(revisions_logfile)
-    `tail -1 #{revisions_logfile}`.chomp.split(' ')[3].gsub(/\)$/, '')
-  elsif Rails.env.development? || Rails.env.test?
-    `git rev-parse HEAD`.chomp
-  end
-
-GIT_BRANCH =
-  if Rails.env.production? && File.exist?(revisions_logfile)
-    `tail -1 #{revisions_logfile}`.chomp.split(' ')[1]
-  elsif Rails.env.development? || Rails.env.test?
-    `git rev-parse --abbrev-ref HEAD`.chomp
-  end
+GIT_SHA = `tail -1 #{revisions_logfile}`.chomp.split(' ')[3].gsub(/\)$/, '') if Rails.env.production? && File.exist?(revisions_logfile)
+GIT_BRANCH = `tail -1 #{revisions_logfile}`.chomp.split(' ')[1] if Rails.env.production? && File.exist?(revisions_logfile)
 
 LAST_DEPLOYED =
   if Rails.env.production? && File.exist?(revisions_logfile)
@@ -43,10 +31,8 @@ SPOT_VERSION = begin
 
   if File.exist?(file_path)
     File.read(file_path).chomp
-  elsif (gittag = `git describe --tags 2> /dev/null`.chomp)
-    gittag
   else
-    require 'datetime'
+    require 'date'
     "#{DateTime.current.year}-dev"
   end
 end
