7e826e7cecab044f9d2b3725676bf651e5cea139
adam malantonio <malantoa@lafayette.edu>
2020-09-24 09:41:49 -0400

    init our test dbs/solr-cores on start-up

--- bin/migrate-and-seed-db.sh
+++ bin/migrate-and-seed-db.sh
@@ -6,6 +6,9 @@ $script_root/wait-for.sh db:5432
 $script_root/wait-for.sh fedora:8080
 $script_root/wait-for.sh solr:8983
 
-echo "migrating + seeding databases"
+echo "migrating test database"
+bundle exec rake db:migrate RAILS_ENV=test
+
+echo "migrating + seeding dev databases"
 bundle exec rake db:migrate
 bundle exec rake db:seed
