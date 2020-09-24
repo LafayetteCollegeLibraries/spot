54bddc2ca9cf13e96fe3f6602e5e154800e7a600
adam malantonio <malantoa@lafayette.edu>
2020-09-24 10:56:58 -0400

    ensure that fields are provided when calling OaiCollectionSolrSet.sets_for (#608)

--- app/models/spot/oai_collection_solr_set.rb
+++ app/models/spot/oai_collection_solr_set.rb
@@ -5,14 +5,18 @@ module Spot
   #   a) we'll be using +member_of_collections_ssim+ as our only hook for OAI-PMG ListSets
   #   b) collection titles will never contain underscores
   class OaiCollectionSolrSet < ::BlacklightOaiProvider::SolrSet
-    # @param [String] spec
-    # @return [String] solr filter for set
-    def self.from_spec(spec)
-      new(spec.tr('_', ' ')).solr_filter
+    def self.sets_for(record)
+      @fields = CatalogController.blacklight_config.oai[:document][:set_fields] if @fields.nil?
+
+      super
     end
 
     def initialize(spec)
       super(spec.tr('_', ' '))
+    end
+
+    def solr_filter
+      "#{@solr_field}:\"#{@value.tr('_', ' ')}\""
     end
 
     def spec
