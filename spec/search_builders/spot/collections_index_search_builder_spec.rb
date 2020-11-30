1ae8a1378a4775cc9fab6124cd729f0accbe4e98
adam malantonio <malantoa@lafayette.edu>
2020-11-30 09:33:42 -0500

    add specs for search_builder + controller

--- spec/search_builders/spot/collections_index_search_builder_spec.rb
+++ spec/search_builders/spot/collections_index_search_builder_spec.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 RSpec.describe Spot::CollectionsIndexSearchBuilder do
+  let(:scope) do
+    OpenStruct.new(blacklight_config: CatalogController.blacklight_config, current_ability: ability)
+  end
+
+  let(:user) { create(:public_user) }
+  let(:ability) { ::Ability.new(user) }
+
   describe '.default_processor_chain' do
     subject { described_class.default_processor_chain }
 
@@ -7,10 +14,11 @@ RSpec.describe Spot::CollectionsIndexSearchBuilder do
   end
 
   describe '#only_include_top_level_collections' do
-    subject(:solr_params) { described_class.new([]).with({}) }
+    let(:solr_params) { builder.to_h }
+    let(:builder) { described_class.new(scope).with({}) }
 
     it 'excludes subcollections' do
-      expect(solr_params['fq']).to include '-member_of_collection_ids_ssim:[* TO *]'
+      expect(solr_params['fq']).to include('-member_of_collection_ids_ssim:[* TO *]')
     end
   end
 end
