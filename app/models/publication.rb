e44d20b89710d47ac38cee7fcdaca06b306b5750
adam malantonio <malantoa@lafayette.edu>
2021-01-14 16:44:56 -0500

    extract common work behavior into a mixin (#676)
    * split out common work behavior into a mixin
    
    * use ClassMethods wrapper
    
    * woops. missing Publication validation
    
    * alphabetize model properties for readability

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,77 +1,26 @@
 # frozen_string_literal: true
 class Publication < ActiveFedora::Base
-  include ::Hyrax::WorkBehavior
-  include ::Spot::NoidIdentifier
+  include Spot::WorkBehavior
 
-  # The `controlled_properties` attribute is used by the Hyrax::DeepIndexingService,
-  # which is used to fetch RDF labels for indexing. This is used out-of-the-box
-  # for :place (which, I believe, uses GeoNames), but could be used for,
-  # say, LCSH headings in other models, if not this one. This is implemented in
-  # Hyrax::BasicMetadata, but since we're implementing our basic metadata fields
-  # outside of that mixin, we'll need to define this manually.
+  # if adding controlled fields (other than :location and :subject), uncomment this
+  # and add the fields to the +controlled_properties+ array
   #
-  # (You'll probably also need to switch on `accepts_nested_attributes` below)
-
-  class_attribute :controlled_properties
-  self.controlled_properties = [:location, :subject]
+  # self.controlled_properties += []
 
   self.indexer = PublicationIndexer
 
   # Change this to restrict which works can be added as a child.
   # self.valid_child_concerns = []
 
-  validates :title, presence: { message: 'Your work must include a Title.' }
-  validates :resource_type, presence: { message: 'Your work must include a Resource Type.' }
-  validates :rights_statement, presence: { message: 'Your work must include a Rights Statement.' }
-
-  validates_with ::Spot::DateIssuedValidator
-  validates_with ::Spot::RequiredLocalAuthorityValidator,
-                 field: :resource_type, authority: 'resource_types'
-  validates_with ::Spot::RequiredLocalAuthorityValidator,
-                 field: :rights_statement, authority: 'rights_statements'
-
-  # title is included with ::ActiveFedora::Base
-  property :subtitle, predicate: ::RDF::URI.new('http://purl.org/spar/doco/Subtitle') do |index|
-    index.as :stored_searchable
-  end
-
-  property :title_alternative, predicate: ::RDF::Vocab::DC.alternative do |index|
-    index.as :stored_searchable
-  end
-
-  property :publisher, predicate: ::RDF::Vocab::DC11.publisher do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :source, predicate: ::RDF::Vocab::DC.source do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :resource_type, predicate: ::RDF::Vocab::DC.type do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :physical_medium, predicate: ::RDF::Vocab::DC.PhysicalMedium do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  # see {IndexesLanguageAndLabel} mixin for indexing
-  property :language, predicate: ::RDF::Vocab::DC11.language
+  # additional validation
+  validates_with Spot::DateIssuedValidator
 
   property :abstract, predicate: ::RDF::Vocab::DC.abstract do |index|
     index.as :stored_searchable
   end
 
-  property :description, predicate: ::RDF::Vocab::DC11.description do |index|
-    index.as :stored_searchable
-  end
-
-  property :note, predicate: ::RDF::Vocab::SKOS.note do |index|
-    index.as :stored_searchable
-  end
-
-  property :identifier, predicate: ::RDF::Vocab::DC.identifier do |index|
-    index.as :symbol
+  property :academic_department, predicate: ::RDF::URI.new('http://vivoweb.org/ontology/core#AcademicDepartment') do |index|
+    index.as :stored_searchable, :facetable
   end
 
   property :bibliographic_citation, predicate: ::RDF::Vocab::DC.bibliographicCitation do |index|
@@ -86,65 +35,19 @@ class Publication < ActiveFedora::Base
     index.as :symbol
   end
 
-  property :creator, predicate: ::RDF::Vocab::DC11.creator do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :contributor, predicate: ::RDF::Vocab::DC11.contributor do |index|
+  property :division, predicate: ::RDF::URI.new('http://vivoweb.org/ontology/core#Division') do |index|
     index.as :stored_searchable, :facetable
   end
 
   property :editor, predicate: ::RDF::Vocab::BIBO.editor do |index|
     index.as :stored_searchable, :facetable
   end
-
-  property :academic_department, predicate: ::RDF::URI.new('http://vivoweb.org/ontology/core#AcademicDepartment') do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :division, predicate: ::RDF::URI.new('http://vivoweb.org/ontology/core#Division') do |index|
-    index.as :stored_searchable, :facetable
-  end
+  property :license, predicate: ::RDF::Vocab::DC.license
 
   property :organization, predicate: ::RDF::URI.new('http://vivoweb.org/ontology/core#Organization') do |index|
     index.as :stored_searchable, :facetable
   end
 
-  property :related_resource, predicate: ::RDF::RDFS.seeAlso do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :subject, predicate: ::RDF::Vocab::DC11.subject,
-                     class_name: Spot::ControlledVocabularies::Base do |index|
-    index.as :symbol
-  end
-
-  property :keyword, predicate: ::RDF::Vocab::SCHEMA.keywords do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :location, predicate: ::RDF::Vocab::DC.spatial,
-                      class_name: Spot::ControlledVocabularies::Location do |index|
-    index.as :symbol
-  end
-
-  property :license, predicate: ::RDF::Vocab::DC.license
-
-  # rights_statements are stored as URIs. indexing is done via {IndexesRightsStatements} mixin
-  property :rights_statement, predicate: ::RDF::Vocab::EDM.rights
-
-  property :rights_holder, predicate: ::RDF::Vocab::DC.rightsHolder do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  # accepts_nested_attributes_for needs to be defined at the end of the model.
-  # see note from Hyrax::BasicMetadata mixin:
-  #
-  #   This must be mixed after all other properties are defined because no other
-  #   properties will be defined once accepts_nested_attributes_for is called
-  id_blank = proc { |attributes| attributes[:id].blank? }
-
-  controlled_properties.each do |prop|
-    accepts_nested_attributes_for prop, reject_if: id_blank, allow_destroy: true
-  end
+  # see {Spot::WorkBehavior.setup_nested_attributes!}
+  setup_nested_attributes!
 end
