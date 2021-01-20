4b6e5dc6eb9491d3284d1e5bc6b81c3fea9ef171
adam malantonio <malantoa@lafayette.edu>
2021-01-20 15:27:06 -0500

    abstract Spot::WorkBehavior specs into a shared_example

---
+++ spec/support/shared_examples/spot_work_behavior.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+RSpec.shared_examples 'it includes Spot::WorkBehavior' do
+  subject { described_class.new }
+
+  # mixins
+  it_behaves_like 'a model with hyrax core metadata'
+  it_behaves_like 'it ensures the existence of a NOID identifier'
+  it_behaves_like 'it includes Spot::CoreMetadata'
+
+  # validations
+  it_behaves_like 'it validates field presence', field: :title
+  it_behaves_like 'it validates field presence', field: :resource_type, value: ['Image']
+  it_behaves_like 'it validates field presence', field: :rights_statement
+  it_behaves_like 'it validates local authorities', field: :resource_type, authority: 'resource_types'
+  it_behaves_like 'it validates local authorities', field: :rights_statement, authority: 'rights_statements'
+
+  # unique behaviors
+  describe '.controlled_properties (class_attribute)' do
+    subject { described_class }
+
+    it { is_expected.to respond_to(:controlled_properties) }
+    it { is_expected.to respond_to(:controlled_properties=) }
+  end
+end
