46e2b3f61ee6831b2327997651b765029f1a0e9f
adam malantonio <malantoa@lafayette.edu>
2021-02-03 11:24:40 -0500

    update Spot::ISO6391 to work with local file

--- app/services/spot/iso6391.rb
+++ app/services/spot/iso6391.rb
@@ -26,13 +26,15 @@ module Spot
 
       private
 
+        def language_hash
+          YAML.load_file(Rails.root.join('config', 'authorities', 'languages.yml'))
+        end
+
         # iso639-1 entries mapped to a key/val hash
         #
         # @return [Hash<String => String>]
         def mapped_639_1_entries
-          ISO_639::ISO_639_1.select { |e| e.alpha2.present? }
-                            .map { |e| [e.alpha2, e.english_name] }
-                            .to_h
+          language_hash['terms'].map { |entry| [entry['id'], entry['term']] }.to_h
         end
     end
   end
