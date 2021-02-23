4ac21cbb5eae7d07c965190677061539229cfb6d
adam malantonio <malantoa@lafayette.edu>
2021-02-23 15:12:19 -0500

    oof typos

--- app/helpers/spot/catalog_helper.rb
+++ app/helpers/spot/catalog_helper.rb
@@ -5,12 +5,14 @@ module Spot
     # Falls back to the original value.
     #
     # @return [String]
+    # rubocop:disable Style/RescueModifier
     def humanize_edtf_values(args)
-      Array.wrap(args[:value]).map { |val| Date.edtf(value).humanize rescue value }.to_sentence
+      Array.wrap(args[:value]).map { |value| Date.edtf(value).humanize rescue value }.to_sentence
     end
+    # rubocop:enable Style/RescueModifier
 
     def display_info_alert?(document)
-      return true if document.embargo_release_date.present? || document.lease_expiration_date.present? || document.authenticated?
+      return true if document.embargo_release_date.present? || document.lease_expiration_date.present? || document.registered?
       false
     end
   end
