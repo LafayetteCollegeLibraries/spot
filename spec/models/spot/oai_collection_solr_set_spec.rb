0e199a71bdb123cca2bc18749ffb96cedbb86633
adam malantonio <malantoa@lafayette.edu>
2021-03-25 09:44:24 -0400

    escape setSpec identifiers (#713)
    * try escaping setSpec values
    
    * use unescaped value for spec name
    
    * remove unused private methods
    
    * update spec text
    
    * update oai feature spec

--- spec/models/spot/oai_collection_solr_set_spec.rb
+++ spec/models/spot/oai_collection_solr_set_spec.rb
@@ -1,27 +1,17 @@
 # frozen_string_literal: true
 RSpec.describe Spot::OaiCollectionSolrSet do
-  before do
-    described_class.fields = fields
-  end
+  before { described_class.fields = fields }
 
   let(:fields) { [{ label: 'Collection', solr_field: 'member_of_collections_ssim' }] }
 
   describe '.from_spec' do
     subject(:solr_filter) { described_class.from_spec(spec) }
 
-    context 'when a spec has underscores in it' do
-      let(:spec) { 'Collection:A_Test_Collection' }
+    context 'when a spec has escaped characters in it' do
+      let(:spec) { 'Collection:Uniform%2C+White+Neck-band+%26+Collar%2C+Without+Bow' }
 
       it 'replaces them with spaces' do
-        expect(solr_filter).to eq 'member_of_collections_ssim:"A Test Collection"'
-      end
-    end
-
-    context 'when a spec has no spaces' do
-      let(:spec) { 'Collection:Photographs' }
-
-      it 'does nothing' do
-        expect(solr_filter).to eq 'member_of_collections_ssim:"Photographs"'
+        expect(solr_filter).to eq 'member_of_collections_ssim:"Uniform, White Neck-band & Collar, Without Bow"'
       end
     end
   end
@@ -29,14 +19,24 @@ RSpec.describe Spot::OaiCollectionSolrSet do
   describe '#initialize' do
     subject(:solr_set) { described_class.new(spec) }
 
-    let(:spec) { 'Collection:A_Test_Collection' }
+    let(:spec) { 'Collection:A+Test+Collection' }
 
-    it 'replaces underscores with spaces for the @value property' do
+    it 'escapes the setspec' do
       expect(solr_set.value).to eq 'A Test Collection'
     end
 
     it 'puts the underscores back when calling #spec' do
       expect(solr_set.spec).to eq spec
+    end
+  end
+
+  describe '#name' do
+    subject(:solr_set) { described_class.new(spec) }
+
+    let(:spec) { 'Collection:A+Test+Collection' }
+
+    it 'uses the unescaped value' do
+      expect(solr_set.name).to eq 'Collection: A Test Collection'
     end
   end
 end
