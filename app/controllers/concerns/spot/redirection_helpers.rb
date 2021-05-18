06ee1f8523097d202751acafb3d1ede8ea0b7cc1
adam malantonio <malantoa@lafayette.edu>
2021-05-18 13:04:58 -0400

    fix redirection to collections (#726)
    * move our object redirection logic to a mixin
    
    * first pass @ spec
    
    * mock all of the data for our redirect specs
    
    * update handle_spec
    
    * add note

---
+++ app/controllers/concerns/spot/redirection_helpers.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+module Spot
+  # Helper methods for common redirection tasks.
+  module RedirectionHelpers
+    # Solves a problem where passing a params hash including +{ controller: 'hyrax/collections', action: 'show' }+ would
+    # result in a No Route Matches error. Passing these params to +Hyrax::Engine.routes.url_for+ will work for
+    # collections but not other work types. There's probably something I'm missing, but this will at least
+    # allow us to resolve Handles and legacy URLs for collections.
+    def redirect_params_for(solr_document:)
+      controller = solr_document['has_model_ssim'].first.downcase.pluralize
+      params = { controller: "hyrax/#{controller}", action: 'show', id: solr_document['id'] }
+
+      return Hyrax::Engine.routes.url_for(**params, only_path: true) if controller == 'collections'
+
+      params
+    end
+  end
+end
--- spec/controllers/spot/redirect_controller_spec.rb
+++ spec/controllers/spot/redirect_controller_spec.rb
@@ -5,28 +5,68 @@ RSpec.describe Spot::RedirectController do
   describe '#show' do
     subject { get :show, params: { url: url } }
 
+    let(:solr_service) { ActiveFedora::SolrService }
+
+    before do
+      solr_service.add(solr_data)
+      solr_service.commit
+    end
+
+    after do
+      solr_service.delete(id: solr_data[:id])
+      solr_service.commit
+    end
+
     context 'when a matching URL exists' do
       let(:url) { 'http://cool.example.com/path/to/item' }
-      let(:obj) { build(:publication, identifier: ["url:#{url}"]) }
+      let(:solr_data) do
+        {
+          id: 'matching-test',
+          has_model_ssim: ['Publication'],
+          identifier_ssim: ["url:#{url}"]
+        }
+      end
 
-      before { obj.save! }
-
-      it { is_expected.to redirect_to hyrax_publication_path(obj.id) }
+      it { is_expected.to redirect_to hyrax_publication_path(solr_data[:id]) }
     end
 
     context 'when an https URL is passed' do
       let(:url) { 'https://digital.lafayette.edu' }
-      let(:obj) { build(:publication, identifier: ['url:http://digital.lafayette.edu']) }
+      let(:solr_data) do
+        {
+          id: 'https-test',
+          has_model_ssim: ['Image'],
+          identifier_ssim: ['url:http://digital.lafayette.edu']
+        }
+      end
 
-      before { obj.save! }
-
-      it { is_expected.to redirect_to hyrax_publication_path(obj.id) }
+      it { is_expected.to redirect_to hyrax_image_path(solr_data[:id]) }
     end
 
     context 'when a matching url does not exist' do
       let(:url) { 'http://nope.example.com/this/doesnt/exist' }
+      let(:solr_data) do
+        {
+          id: 'not-exist-test',
+          has_model_ssim: ['Image'],
+          title_tesim: ['This is not the thing we are looking for']
+        }
+      end
 
       it { is_expected.to have_http_status :not_found }
+    end
+
+    context 'when the item is a collection' do
+      let(:url) { 'http://digital.lafayette.edu/collections/legacy-collection' }
+      let(:solr_data) do
+        {
+          id: 'collection-test',
+          has_model_ssim: ['Collection'],
+          identifier_ssim: ["url:#{url}"]
+        }
+      end
+
+      it { is_expected.to redirect_to Hyrax::Engine.routes.url_helpers.collection_path(solr_data[:id]) }
     end
   end
 end
