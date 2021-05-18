e11ab41611e45da1a61bfb10a16d8e6072e18b0b
adam malantonio <malantoa@lafayette.edu>
2021-05-18 09:37:37 -0400

    first pass @ spec

--- spec/controllers/spot/redirect_controller_spec.rb
+++ spec/controllers/spot/redirect_controller_spec.rb
@@ -28,5 +28,30 @@ RSpec.describe Spot::RedirectController do
 
       it { is_expected.to have_http_status :not_found }
     end
+
+    context 'when the item is a collection' do
+      let(:collection_id) { 'colabc123' }
+      let(:collection_solr_data) do
+        {
+          id: collection_id,
+          has_model_ssim: ['Collection'],
+          identifier_ssim: ["url:#{url}"]
+        }
+      end
+
+      let(:url) { 'http://cool.website.org' }
+
+      # collections require a lot more setup, so we'll just fake it by putting the data in solr
+      before do
+        ActiveFedora::SolrService.add(collection_solr_data)
+        ActiveFedora::SolrService.commit
+      end
+
+      after do
+        ActiveFedora::SolrService.delete_by_id(collection_id)
+      end
+
+      it { is_expected.to redirect_to hyrax_collection_path(collection_id)}
+    end
   end
 end
