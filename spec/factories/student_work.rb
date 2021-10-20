5133ae34d32d0cf0961019fe8b59eadd321aabb5
adam malantonio <malantoa@lafayette.edu>
2021-10-20 17:16:54 -0400

    refactories

--- spec/factories/student_work.rb
+++ spec/factories/student_work.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 #
 FactoryBot.define do
-  factory :student_work do
-    # TODO fill this out
+  factory :student_work, traits: [:work_behavior] do
+    abstract { ['A short description of the thing'] }
+    access_note { ['Here is how to access this thing'] }
+    advisor { ['Prof. Smartfellow'] }
+    bibliographic_citation { ['A. Book, somewhere'] }
+    date { ['2021-10-20'] }
+    date_available { ['2021-10-20'] }
+
+    # to help with debugging maybe
+    creator { ['Student, A.'] }
+    title { ['Student Work, Submitted'] }
+
+    factory :student_work_with_file_set, traits: [:has_file_set]
   end
 end
