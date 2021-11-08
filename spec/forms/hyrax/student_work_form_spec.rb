0f1e5eb110da1da1b648e43d7d7b7f87c3d68809
adam malantonio <malantoa@lafayette.edu>
2021-11-08 11:51:34 -0500

    spec student_work_form terms + permitted params

--- spec/forms/hyrax/student_work_form_spec.rb
+++ spec/forms/hyrax/student_work_form_spec.rb
@@ -3,4 +3,71 @@ RSpec.describe Hyrax::StudentWorkForm do
   it_behaves_like 'a Spot work form'
 
   it_behaves_like 'it handles required fields', :title, :resource_type, :rights_statement
+
+  describe '.terms' do
+    subject { described_class.terms }
+
+    describe 'includes optional fields' do
+      it { is_expected.to include :title_alternative }
+      it { is_expected.to include :creator }
+      it { is_expected.to include :contributor }
+      it { is_expected.to include :abstract }
+      it { is_expected.to include :description }
+      it { is_expected.to include :date }
+      it { is_expected.to include :date_available }
+      it { is_expected.to include :language }
+      it { is_expected.to include :location }
+      it { is_expected.to include :physical_medium }
+      it { is_expected.to include :publisher }
+      it { is_expected.to include :identifier }
+      it { is_expected.to include :keyword }
+      it { is_expected.to include :related_resource }
+      it { is_expected.to include :resource_type }
+      it { is_expected.to include :source }
+      it { is_expected.to include :subject }
+      it { is_expected.to include :subtitle }
+      it { is_expected.to include :advisor }
+      it { is_expected.to include :academic_department }
+      it { is_expected.to include :division }
+      it { is_expected.to include :organization }
+      it { is_expected.to include :rights_holder }
+      it { is_expected.to include :rights_statement }
+      it { is_expected.to include :bibliographic_citation }
+      it { is_expected.to include :access_note }
+      it { is_expected.to include :note }
+    end
+  end
+
+  describe '.build_permitted_params' do
+    subject { described_class.build_permitted_params }
+
+    it { is_expected.to include(:title) }
+    it { is_expected.to include(title_alternative: []) }
+    it { is_expected.to include(creator: []) }
+    it { is_expected.to include(contributor: []) }
+    it { is_expected.to include(abstract: []) }
+    it { is_expected.to include(description: []) }
+    it { is_expected.to include(date: []) }
+    it { is_expected.to include(date_available: []) }
+    it { is_expected.to include(language: []) }
+    it { is_expected.to include(location: []) }
+    it { is_expected.to include(physical_medium: []) }
+    it { is_expected.to include(publisher: []) }
+    it { is_expected.to include(identifier: []) }
+    it { is_expected.to include(keyword: []) }
+    it { is_expected.to include(related_resource: []) }
+    it { is_expected.to include(resource_type: []) }
+    it { is_expected.to include(source: []) }
+    it { is_expected.to include(subject: []) }
+    it { is_expected.to include(subtitle: []) }
+    it { is_expected.to include(advisor: []) }
+    it { is_expected.to include(academic_department: []) }
+    it { is_expected.to include(division: []) }
+    it { is_expected.to include(organization: []) }
+    it { is_expected.to include(rights_holder: []) }
+    it { is_expected.to include(:rights_statement) }
+    it { is_expected.to include(bibliographic_citation: []) }
+    it { is_expected.to include(access_note: []) }
+    it { is_expected.to include(note: []) }
+  end
 end
