7134fc10f51c0149c1cc4c70dcb757f7b12335dd
Anna Malantonio <malantoa@lafayette.edu>
2021-11-16 22:06:07 -0500

    student_work#show

--- app/presenters/hyrax/student_work_presenter.rb
+++ app/presenters/hyrax/student_work_presenter.rb
@@ -3,6 +3,8 @@ module Hyrax
   class StudentWorkPresenter < Spot::BasePresenter
     humanize_date_fields :date, :date_available
 
-    delegate :abstract, :access_note, :advisor, :bibliographic_citation, to: :solr_document
+    delegate :abstract, :access_note, :advisor, :bibliographic_citation,
+             :date, :date_available, :academic_department, :division,
+             :organization, to: :solr_document
   end
 end
