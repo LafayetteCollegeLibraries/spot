2f2778a74f0e118c9a19b6446054ae74cd63297c
Anna Malantonio <malantoa@lafayette.edu>
2021-11-23 12:50:32 -0500

    add spec for load job

--- app/jobs/spot/load_lafayette_instructors_authority_job.rb
+++ app/jobs/spot/load_lafayette_instructors_authority_job.rb
@@ -4,7 +4,7 @@ module Spot
     # @todo We should probably query the current academic term and cache it (for a month?)
     #       before loading the instructors. For now, we'll stuff it with Winter 2021.
     def perform
-      Spot::LafayetteInstructorsAuthorityService.load(term: '202103')
+      Spot::LafayetteInstructorsAuthorityService.load(term: '202110')
     end
   end
 end
