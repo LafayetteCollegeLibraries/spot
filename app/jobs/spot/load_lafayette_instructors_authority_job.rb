683bb37328d4ae2d8e668a7938bcfc1af2000cfd
Anna Malantonio <malantoa@lafayette.edu>
2022-01-11 13:16:00 -0500

    don't generate a term for the default (#811)

--- app/jobs/spot/load_lafayette_instructors_authority_job.rb
+++ app/jobs/spot/load_lafayette_instructors_authority_job.rb
@@ -3,7 +3,7 @@ module Spot
   class LoadLafayetteInstructorsAuthorityJob < ::ApplicationJob
     # @todo We should probably query the current academic term and cache it (for a month?)
     #       before loading the instructors. For now, we'll stuff it with Winter 2021.
-    def perform(term: Time.zone.today.strftime('%Y10'))
+    def perform(term: '202110')
       Spot::LafayetteInstructorsAuthorityService.load(term: term)
     end
   end
