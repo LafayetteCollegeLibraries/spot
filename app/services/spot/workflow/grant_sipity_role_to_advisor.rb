da40712e17e59f9bd721d9e4404243c200c5dcba
Anna Malantonio <malantoa@lafayette.edu>
2022-01-13 15:06:46 -0500

    style fixes

--- app/services/spot/workflow/grant_sipity_role_to_advisor.rb
+++ app/services/spot/workflow/grant_sipity_role_to_advisor.rb
@@ -16,26 +16,26 @@ module Spot
 
       private
 
-        def advisor_from_target
-          lnumber = @target.advisor.first
-          User.find_by(lnumber: lnumber)
-        end
+      def advisor_from_target
+        lnumber = @target.advisor.first
+        User.find_by(lnumber: lnumber)
+      end
 
-        def agent
-          advisor_from_target.to_sipity_agent
-        end
+      def agent
+        advisor_from_target.to_sipity_agent
+      end
 
-        def entity
-          Sipity::Entity.find_or_create_by!(proxy_for_global_id: target_gid)
-        end
+      def entity
+        Sipity::Entity.find_or_create_by!(proxy_for_global_id: target_gid)
+      end
 
-        def target_gid
-          @target.to_global_id.to_s
-        end
+      def target_gid
+        @target.to_global_id.to_s
+      end
 
-        def workflow_role
-          Sipity::Role[:advising]
-        end
+      def workflow_role
+        Sipity::Role[:advising]
+      end
     end
   end
 end
--- app/services/spot/workflow/pending_library_review_notification.rb
+++ app/services/spot/workflow/pending_library_review_notification.rb
@@ -11,7 +11,7 @@ module Spot
         def message
           msg = [
             "#{title} (#{link_to work_id, document_path}) was deposited by #{user.user_key}.",
-            "It's been approved by its advisor and is awaiting approval.",
+            "It's been approved by its advisor and is awaiting approval."
           ]
 
           msg << "\n\n<blockquote>#{comment}</blockquote>" if comment
