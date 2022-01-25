f42aad63175b60e0a9523c09ea938b230e1903a3
Anna Malantonio <malantoa@lafayette.edu>
2022-01-25 16:46:50 -0500

    add more processing in Spot::CsvEntry

--- app/models/spot/csv_entry.rb
+++ app/models/spot/csv_entry.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 module Spot
   class CsvEntry < ::Bulkrax::CsvEntry
+    JOIN_CHARACTER = '|'
+
     def build_export_metadata
       super
 
-      self.parsed_metadata['location_label'] = hyrax.record.location.map do |location|
+      parsed_metadata['depositor'] = parse_user_string(hyrax_record.depositor)
 
-      end
+      build_controlled_properties
+      build_language_labels
+      build_advisors
+      build_rights_statement_labels
     end
 
     # Join our filenames with a pipe ('|') rather than ennumerating each with its own heading
@@ -14,7 +19,7 @@ module Spot
     #
     # @return [void]
     def build_files
-      self.parsed_metadata['file'] = hyrax_record.file_sets.map { |fs| filename(fs).to_s if filename(fs).present? }.compact.join('|')
+      parsed_metadata['file'] = hyrax_record.file_sets.map { |fs| filename(fs).to_s if filename(fs).present? }.compact.join(JOIN_CHARACTER)
     end
 
     # Join a field's value with a pipe ('|') rather than ennumerating each with its own heading
@@ -29,11 +34,52 @@ module Spot
       data = hyrax_record.send(key.to_s)
 
       if data.is_a?(ActiveTriples::Relation)
-        join_char = (config['join'] && config['join'].is_a?(String)) ? config['join'] : '|'
-        self.parsed_metadata[key_for_export(key)] = data.map { |d| prepare_export_data(d) }.join(join_char).to_s
+        join_char = config['join'] && config['join'].is_a?(String) ? config['join'] : JOIN_CHARACTER
+        parsed_metadata[key_for_export(key)] = data.map { |d| prepare_export_data(d) }.join(join_char).to_s
       else
-        self.parsed_metadata[key_for_export(key)] = prepare_export_data(data)
+        parsed_metadata[key_for_export(key)] = prepare_export_data(data)
       end
+    end
+
+    private
+
+    def build_advisors
+      parsed_metadata['advisor'] = hyrax_record.advisor.map { |advisor_email| parse_user_string(advisor_email) } if hyrax_record.respond_to?(:advisor)
+    end
+
+    def build_controlled_properties
+      props = hyrax_record.class&.controlled_properties || []
+
+      props.each do |field|
+        parsed_metadata["#{field}_label"] = map_uri_labels(hyrax_record.send(field)) if hyrax_record.respond_to?(field)
+      end
+    end
+
+    def build_language_labels
+      parsed_metadata['language_label'] = hyrax_record.language.map do |shortcode|
+        Spot::ISO6391.label_for(shortcode)
+      end
+    end
+
+    def build_rights_statement_labels
+      parsed_metadata['rights_statement_label'] = hyrax_record.rights_statement.map do |uri|
+        rights_statement_service.label(uri) { uri }
+      end
+    end
+
+    def map_uri_labels(values)
+      values.map(&:rdf_label).flatten.join(JOIN_CHARACTER)
+    end
+
+    def parse_user_string(value)
+      user = ::User.find_by(email: value)
+      return value if user.nil? || user.display_name.blank?
+
+      %("#{user.display_name}" <#{user.email}>)
+    end
+
+    def rights_statement_service
+      @rights_statement_service ||= Hyrax.config.rights_statement_service_class.new
     end
   end
 end
