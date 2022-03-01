408939a60ababcd9ba86e3e7940d32a39db8d7e5
Anna Malantonio <malantoa@lafayette.edu>
2022-03-01 14:28:06 -0500

    force StudentWorks into mediated_student_work_deposit admin_set for all users but admins (#843)
    * stuff mediated_student_work_deposit admin_set_id into StudentWorkForm (when present)
    
    * hide 'relationships' tab on StudentWorkForm for all but admins
    
    * style

--- spec/forms/hyrax/student_work_form_spec.rb
+++ spec/forms/hyrax/student_work_form_spec.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 RSpec.describe Hyrax::StudentWorkForm do
+  before do
+    allow(AdminSet).to receive(:find_or_create_default_admin_set_id).and_return(AdminSet::DEFAULT_ID)
+    allow(Sipity::Workflow)
+      .to receive(:where)
+      .with(name: anything)
+      .and_return(workflow_relation)
+    allow(workflow_relation)
+      .to receive(:order)
+      .with(anything)
+      .and_return(workflow_results)
+  end
+
+  let(:workflow_relation) { instance_double('Sipity::Workflow::ActiveRecord_Relation') }
+  let(:workflow_results) { [] }
+
   it_behaves_like 'a Spot work form'
 
   it_behaves_like 'it handles required fields',
@@ -60,8 +75,9 @@ RSpec.describe Hyrax::StudentWorkForm do
     subject(:attributes) { described_class.model_attributes(raw_params) }
 
     let(:raw_params) { ActionController::Parameters.new(params) }
+    let(:params) { {} }
 
-    context 'handles nested attributes' do
+    describe 'handles nested attributes' do
       describe 'language' do
         let(:field) { 'language' }
 
@@ -78,6 +94,52 @@ RSpec.describe Hyrax::StudentWorkForm do
         let(:field) { 'division' }
 
         it_behaves_like 'it transforms a local vocabulary attribute'
+      end
+    end
+
+    describe 'admin_set_id' do
+      before do
+        allow(Hyrax::PermissionTemplate)
+          .to receive(:find_by)
+          .with(source_id: admin_set_id)
+          .and_return(permission_template)
+        allow(permission_template)
+          .to receive(:active_workflow)
+          .and_return(workflow)
+      end
+
+      let(:workflow_results) { [workflow] }
+      let(:workflow) { instance_double('Sipity::Workflow', permission_template: permission_template, allows_access_grant?: false) }
+      let(:permission_template) { instance_double('Hyrax::PermissionTemplate', admin_set: admin_set) }
+      let(:admin_set) { instance_double('AdminSet', id: admin_set_id) }
+      let(:admin_set_id) { 'abc123def' }
+
+      context 'when a value is present' do
+        let(:params) { { 'admin_set_id' => admin_set_id } }
+        let(:admin_set_id) { 'admin_set_id_value' }
+
+        it 'retains the value' do
+          # { 'admin_set_id' => 'admin_set_id_value' }
+          expect(attributes[:admin_set_id]).to eq admin_set_id
+        end
+      end
+
+      context 'when a value is not present' do
+        context 'when the expected workflow exists' do
+          it 'uses the admin_set assigned to the workflow' do
+            # { 'admin_set_id' => 'abc123def' }
+            expect(attributes[:admin_set_id]).to eq admin_set.id
+          end
+        end
+
+        context 'when the workflow does not exist' do
+          let(:workflow_results) { [] }
+
+          it 'uses the default admin_set id' do
+            # { 'admin_set_id' => 'admin_set/default' }
+            expect(attributes[:admin_set_id]).to eq AdminSet::DEFAULT_ID
+          end
+        end
       end
     end
   end
