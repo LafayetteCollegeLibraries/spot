8222c1d8ac930536186f62dad32cf3e7ff34fee0
Anna Malantonio <malantoa@lafayette.edu>
2022-04-12 11:32:22 -0400

    add toggleable student honors thesis block to homepage (#886)
    * add toggleable student honors thesis block to homepage
    
    * wow get me a new keyboard please
    
    * add appropriate 'Learn more' url
    
    * add text suggestions from nora

--- config/features.rb
+++ config/features.rb
@@ -1,3 +1,8 @@
 # frozen_string_literal: true
 #
 # Add features that can be toggled within +/admin/features+
+Flipflop.configure do
+  feature :show_senior_honors_thesis_block,
+          default: false,
+          description: 'Display block on homepage for students to easily access the StudentWork form'
+end
---
+++ spec/presenters/spot/homepage_presenter_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+RSpec.describe Spot::HomepagePresenter do
+  let(:presenter) { described_class.new(recent_items, featured_collections) }
+  let(:recent_items) { [] }
+  let(:featured_collections) { [] }
+
+  describe '#show_senior_honors_thesis_block?' do
+    subject { presenter.show_senior_honors_thesis_block? }
+
+    before do
+      allow(Flipflop).to receive(:enabled?).with(:show_senior_honors_thesis_block).and_return(enabled)
+    end
+
+    context 'when the feature is enabled' do
+      let(:enabled) { true }
+
+      it { is_expected.to be true }
+    end
+
+    context 'when the feature is disabled' do
+      let(:enabled) { false }
+
+      it { is_expected.to be false }
+    end
+  end
+end
