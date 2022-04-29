c192ef09ee1d43d4ab4110e6adbfcd0303f58b4f
Anna Malantonio <malantoa@lafayette.edu>
2022-04-29 11:45:52 -0400

    expand comments

--- app/controllers/hyrax/student_works_controller.rb
+++ app/controllers/hyrax/student_works_controller.rb
@@ -5,6 +5,32 @@ module Hyrax
 
     self.curation_concern_type = ::StudentWork
     self.show_presenter = Hyrax::StudentWorkPresenter
+
+    # Modifying the search_builder_class to allow users whose user_keys
+    # are stored as a StudentWork#advisor value to view works currently
+    # in a workflow, that would normally be suppressed. We were finding
+    # that when an advisor submitted a request for changes to a work,
+    # their ability to view the work was recinded, as their "advising"
+    # workflow role wasn't eligible at the next stage. This would (for
+    # some reason) kick the user to CAS to re-authenticate, redirect them
+    # back to the :unauthorized work page, and then spiral into a redirect loop.
+    #
+    # This comes into play at Hyrax::WorksControllerBehavior#search_result_document,
+    # which uses Blacklight results to find the first valid document.
+    # With the default search_builder (Hyrax::WorkSearchBuilder), the
+    # work would not be returned after the advisor fell out of scope
+    # in the workflow. Our custom StudentWorkSearchBuilder checks a work's
+    # "advisor_ssim" field for the current_user to determine whether the
+    # "suppressed?" flag should be enabled or disabled.
+    #
+    # I don't fully understand the reasoning behind raising a WorkflowAuthorizationException
+    # if a doc is suppressed but the current_user has :read access to it
+    # (see Hyrax::WorksControllerBehavior#document_not_found!), but in order to
+    # avoid that being thrown, we need to have search_result_document return the
+    # document for valid users.
+    #
+    # @see https://github.com/samvera/hyrax/blob/v2.9.6/app/controllers/concerns/hyrax/works_controller_behavior.rb#L216-L221
+    # @see https://github.com/samvera/hyrax/blob/v2.9.6/app/controllers/concerns/hyrax/works_controller_behavior.rb#L223-L227
     self.search_builder_class = ::Spot::StudentWorkSearchBuilder
   end
 end
--- app/search_builders/spot/student_work_search_builder.rb
+++ app/search_builders/spot/student_work_search_builder.rb
@@ -8,6 +8,16 @@ module Spot
     # Also moves `#user_has_active_workflow_role?` method to after checking `#depositor?`
     # and `#advisor?`, as those methods don't require database lookups.
     #
+    # Fixes an issue where advisors would submit a request for changes on a work in progress
+    # using the `mediated_student_work_deposit` workflow, which would advance the work to
+    # the next workflow step, removing their ability view the work (checked via Hyrax::Workflow::PermissionQuery,
+    # via Hyrax::FilteredSuppressedWithRoles search_builder mixin). This would send the user
+    # into a redirect spiral, as the work controller would register the user as :unauthorized
+    # to view the work, send them to CAS to authorize, and then redirect back to the :unauthorized
+    # item.
+    #
+    # @see https://github.com/samvera/hyrax/blob/v2.9.6/app/services/hyrax/workflow/permission_query.rb#L35-L58
+    # @see https://github.com/samvera/hyrax/blob/v2.9.6/app/search_builders/hyrax/filter_suppressed_with_roles.rb#L26-L31
     # @see https://github.com/samvera/hyrax/blob/v2.9.6/app/search_builders/hyrax/filter_suppressed.rb#L10-L13
     def only_active_works(solr_parameters)
       return if depositor? || advisor? || user_has_active_workflow_role?
