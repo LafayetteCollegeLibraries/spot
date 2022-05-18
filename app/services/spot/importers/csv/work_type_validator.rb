beb2df2093f6857dd5fbb535de7ba064e83dd918
Anna Malantonio <malantoa@lafayette.edu>
2022-05-18 15:39:15 -0400

    attempt at cutting the cognitive complexity

--- app/services/spot/importers/csv/work_type_validator.rb
+++ app/services/spot/importers/csv/work_type_validator.rb
@@ -10,22 +10,18 @@ module Spot::Importers::CSV
     end
 
     def validate(parser:)
-      errors = []
       csv_field_name = parser.work_type_field_name.to_s
 
-      parser.parsed_csv.each_with_index do |row, idx|
+      parser.parsed_csv.each_with_index.each_with_object([]) do |(row, idx), errors|
         # +1 for header row
         # +1 for zero-based counting
         line_number = idx + 2
-        value = row[csv_field_name]
-        value = value.first if value.is_a?(Array)
+        value = Array.wrap(row[csv_field_name]).first
 
-        next if value.blank?
+        next if value.blank? || self.class.valid?(value)
 
-        errors << invalid_work_type_error(type: value, line_number: line_number) unless self.class.valid?(value)
+        errors << invalid_work_type_error(type: value, line_number: line_number)
       end
-
-      errors
     end
 
     private
