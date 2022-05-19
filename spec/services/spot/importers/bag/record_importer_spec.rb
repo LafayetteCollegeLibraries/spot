e2d0fb268069d43704e03328c5852394efa4d939
Anna Malantonio <malantoa@lafayette.edu>
2022-05-19 15:23:36 -0400

    remove darlingtonia infrastructure for initial migration

--- spec/services/spot/importers/bag/record_importer_spec.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-RSpec.describe Spot::Importers::Bag::RecordImporter do
-  it_behaves_like 'a RecordImporter',
-                  empty_file_warning: '[WARN] no files found for this bag\n'
-end
