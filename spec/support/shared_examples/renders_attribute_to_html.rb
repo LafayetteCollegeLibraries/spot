045815dc5f245db846f21063677fc023d8141fe4
Anna Malantonio <malantoa@lafayette.edu>
2022-05-24 13:25:40 -0400

    test that presenters can deal with unknown fields

--- spec/support/shared_examples/renders_attribute_to_html.rb
+++ spec/support/shared_examples/renders_attribute_to_html.rb
@@ -6,19 +6,18 @@ RSpec.shared_examples 'it renders an attribute to HTML' do
   let(:ability) { Ability.new(build(:user)) }
   let(:field) { :keyword }
   let(:options) { {} }
+  let(:klass) { Spot::Renderers::AttributeRenderer }
+
+  before do
+    allow(klass).to receive(:new).and_return(attribute_double)
+  end
 
   describe '#attribute_to_html' do
     subject(:render_attribute!) { presenter.attribute_to_html(field, options) }
 
     let(:attribute_double) { instance_double(klass.to_s, render: true) }
 
-    before do
-      allow(klass).to receive(:new).and_return(attribute_double)
-    end
-
     context 'default mode' do
-      let(:klass) { Spot::Renderers::AttributeRenderer }
-
       it 'calls the AttributeRenderer by default' do
         render_attribute!
 
@@ -56,6 +55,19 @@ RSpec.shared_examples 'it renders an attribute to HTML' do
         render_attribute!
 
         expect(attribute_double).to have_received(:render)
+      end
+    end
+
+    context 'when a field does not exist' do
+      let(:field) { :nope }
+
+      before do
+        allow(Rails.logger).to receive(:warn)
+      end
+
+      it 'returns nothing and logs a warning' do
+        expect(render_attribute!).to be nil
+        expect(Rails.logger).to have_received(:warn).exactly(1).time
       end
     end
   end
