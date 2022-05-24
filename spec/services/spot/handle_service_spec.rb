88fb230db9b625751aee0983fb7b4b3d2db17611
Anna Malantonio <malantoa@lafayette.edu>
2022-05-24 12:28:15 -0400

    update handle_service specs

--- spec/services/spot/handle_service_spec.rb
+++ spec/services/spot/handle_service_spec.rb
@@ -2,7 +2,7 @@
 RSpec.describe Spot::HandleService do
   subject(:service) { described_class.new(work) }
 
-  let(:work) { instance_double(Publication, id: 'abc123def', identifier: identifiers) }
+  let(:work) { build(:publication, id: 'abc123def', identifier: identifiers) }
   let(:identifiers) { [] }
   let(:handle_server_url) { 'http://handle-service:8000' }
   let(:handle_prefix) { '10385' }
@@ -53,10 +53,10 @@ RSpec.describe Spot::HandleService do
       stub_env('HANDLE_CLIENT_CERT', cert_path)
       stub_env('HANDLE_CLIENT_KEY', key_path)
 
-      allow(service).to receive(:cert_exist?).and_return(true)
-      allow(service).to receive(:cert_contents).and_return(:cert_data)
-      allow(service).to receive(:key_exist?).and_return(true)
-      allow(service).to receive(:key_contents).and_return(:key_data)
+      allow(File).to receive(:exist?).with(cert_path).and_return true
+      allow(File).to receive(:read).with(cert_path).and_return(:cert_data)
+      allow(File).to receive(:exist?).with(key_path).and_return true
+      allow(File).to receive(:read).with(key_path).and_return(:key_data)
 
       allow(OpenSSL::X509::Certificate).to receive(:new).with(:cert_data).and_return(cert_double)
       allow(OpenSSL::PKey).to receive(:read).with(:key_data).and_return(key_double)
@@ -78,6 +78,17 @@ RSpec.describe Spot::HandleService do
 
       expect(work).to have_received(:identifier=).with(["hdl:#{handle_value}"])
       expect(work).to have_received(:save!)
+    end
+
+    context 'when a work already has an identifier' do
+      let(:identifiers) { ['hdl:10385/abc123def'] }
+
+      it 'does not save the identifier to the record' do
+        service.mint
+
+        expect(work).not_to have_received(:identifier=)
+        expect(work).not_to have_received(:save!)
+      end
     end
 
     context 'when a responseCode != 1 is returned' do
