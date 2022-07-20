ed751aeeea2af0d908ca4fb4cda3b07176f194ce
Anna Malantonio <malantoa@lafayette.edu>
2022-07-20 13:27:34 -0400

    index Image#date_associated as symbol and stored_searchable

--- spec/indexers/image_indexer_spec.rb
+++ spec/indexers/image_indexer_spec.rb
@@ -22,6 +22,7 @@ RSpec.describe ImageIndexer do
     contributor: %w[tesim sim],
     related_resource: %w[tesim sim],
     subject_ocm: %w[ssim]
+    date_associated: %w[ssim tesim]
   }.each_pair do |method, suffixes|
     let(:work_method) { method }
     let(:solr_fields) { suffixes.map { |suffix| "#{method}_#{suffix}" } }
