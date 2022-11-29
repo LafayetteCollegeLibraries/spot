25f64d804ad03831d5b6222f90e1b15e0d7f4cd2
Anna Malantonio <malantoa@lafayette.edu>
2022-11-29 14:15:05 -0500

    add source_identifier property to work models

--- app/models/concerns/spot/core_metadata.rb
+++ app/models/concerns/spot/core_metadata.rb
@@ -12,6 +12,8 @@ module Spot
     extend ActiveSupport::Concern
 
     included do
+      after_create :ensure_source_identifier!
+
       # A bibliographic reference for the resource.
       property :bibliographic_citation, predicate: ::RDF::Vocab::DC.bibliographicCitation do |index|
         index.as :stored_searchable
@@ -107,6 +109,15 @@ module Spot
         index.as :stored_searchable, :facetable
       end
 
+      # An identifier that links this record to a Bulkrax import/export record.
+      # For objects created within the Hyrax UI (_not_ Bulkrax), this will be
+      # filled with a default value of ["ldr:#{work.id}"]
+      #
+      # @todo find a better predicate for this field
+      property :source_identifier, predicate: ::RDF::URI('http://ldr.lafayette.edu/ns#source_identifier') do |index|
+        index.as :symbol
+      end
+
       # Stored as OCLC FAST URI, displayed as human-readable prefLabel.
       # @see {Spot::DeepIndexingService} for label indexing details
       property :subject, predicate: ::RDF::Vocab::DC11.subject,
@@ -123,6 +134,15 @@ module Spot
       property :title_alternative, predicate: ::RDF::Vocab::DC.alternative do |index|
         index.as :stored_searchable
       end
+    end
+
+    private
+
+    def ensure_source_identifier!
+      return unless source_identifier.empty?
+
+      self.source_identifier = ["ldr:#{id}"]
+      save!
     end
   end
 end
--- spec/support/shared_examples/spot_core_metadata.rb
+++ spec/support/shared_examples/spot_core_metadata.rb
@@ -19,10 +19,46 @@ RSpec.shared_examples 'it includes Spot::CoreMetadata' do
     [:rights_holder,          RDF::Vocab::DC.rightsHolder],
     [:rights_statement,       RDF::Vocab::EDM.rights],
     [:source,                 RDF::Vocab::DC.source],
+    [:source_identifier,      RDF::URI('http://ldr.lafayette.edu/ns#source_identifier')],
     [:subject,                RDF::Vocab::DC11.subject],
     [:subtitle,               RDF::URI.new('http://purl.org/spar/doco/Subtitle')],
     [:title_alternative,      RDF::Vocab::DC.alternative]
   ].each do |(property, predicate)|
     it { is_expected.to have_editable_property(property).with_predicate(predicate) }
+  end
+
+  describe 'runs ensure_source_identifier! after :create' do
+    subject(:created_work) { described_class.create!(metadata) }
+
+    let(:base_metadata) do
+      {
+        title: ["Sourced Work #{described_class.name}"],
+        rights_statement: ['http://creativecommons.org/publicdomain/mark/1.0/'],
+        resource_type: ['Other']
+      }
+    end
+
+
+    let(:publication_metadata) { base_metadata.merge(date_issued: ['2022-11-28']) }
+    let(:image_metadata) { base_metadata.dup }
+    let(:student_work_metadata) { base_metadata.dup }
+
+    let(:metadata) { send(metadata_variable) }
+    let(:metadata_variable) { described_class.name.underscore + "_metadata" }
+
+    context 'when a source_identifier is provided' do
+      let(:source_identifier) { ['ldr:ingest:abc123'] }
+      let(:metadata) { send(metadata_variable).merge(source_identifier: source_identifier) }
+
+      it 'uses the provided identifier' do
+        expect(created_work.source_identifier).to eq source_identifier
+      end
+    end
+
+    context 'when a source_identifier is not provided' do
+      it 'creates one with the minted work id' do
+        expect(created_work.source_identifier).to eq ["ldr:#{created_work.id}"]
+      end
+    end
   end
 end
