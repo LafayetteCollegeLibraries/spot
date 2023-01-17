e3232ed2b03ba28fda0cb34ca77a9ce842eda568
Anna Malantonio <malantoa@lafayette.edu>
2023-01-17 08:42:47 -0500

    use minio instead of localstack

--- bin/migrate-and-seed-db.sh
+++ bin/migrate-and-seed-db.sh
@@ -2,7 +2,7 @@
 
 if [[ ! -z "$AWS_IIIF_ASSET_BUCKET" ]]; then
   echo "creating s3 bucket"
-  aws --endpoint-url="${AWS_ENDPOINT_URL:-"http://localhost:4566"}" s3 mb "s3://${AWS_IIIF_ASSET_BUCKET}"
+  aws --endpoint-url="${AWS_ENDPOINT_URL:-"http://localhost:9000"}" s3 mb "s3://${AWS_IIIF_ASSET_BUCKET}"
 fi
 
 script_root="$(dirname $0)"
