65f228a5f13ac1781015fc7c8084e656a6055b71
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2023-03-13 15:59:04 -0400

    checks for garbage data

--- app/indexers/concerns/indexes_citation_metadata.rb
+++ app/indexers/concerns/indexes_citation_metadata.rb
@@ -7,11 +7,11 @@ module IndexesCitationMetadata
 
             @citation = AnyStyle.parse(object.bibliographic_citation.first)
 
-            doc['citation_journal_title_ss'] = @citation.first[:"container-title"].first.to_s
-            doc['citation_volume_ss'] = @citation.first[:"volume"].first.to_s
-            doc['citation_issue_ss'] = @citation.first[:"issue"].first.to_s
-            doc['citation_firstpage_ss'] = @citation.first[:"pages"].first.to_s.split('–', 2).first
-            doc['citation_lastpage_ss'] = @citation.first[:"pages"].first.to_s.split('–', 2).last
+            doc['citation_journal_title_ss'] = puts @citation.first[:"container-title"].first.to_s if @citation.respond_to?(:"container-title")
+            doc['citation_volume_ss'] = puts @citation.first[:"volume"].first.to_s if @citation.respond_to?(:"volume")
+            doc['citation_issue_ss'] = puts @citation.first[:"issue"].first.to_s if @citation.respond_to?(:"issue")
+            doc['citation_firstpage_ss'] = puts @citation.first[:"pages"].first.to_s.split('–', 2).first if @citation.respond_to?(:"pages")
+            doc['citation_lastpage_ss'] = puts @citation.first[:"pages"].first.to_s.split('–', 2).last if @citation.respond_to?(:"pages")
         end
     end
 end
