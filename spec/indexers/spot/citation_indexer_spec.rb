92859cd6b3679287c1e23c0b79474c36ff59c4dd
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2023-03-16 15:20:53 -0400

    test structure?

---
+++ spec/indexers/spot/citation_indexer_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+RSpec.describe Spot::IndexesCitationMetadata do
+    subject(:solr_doc) { indexer.generate_solr_document }
+
+    describe 'extracting citation metadata' do
+      #let statement to set the biblographic citation field in the solr document
+      #trigger the indexing process? Does this happen automatically?
+        
+      #expect statment for each field
+      expect true
+    end
+
+    describe 'incomplete citation metadata' do
+      #let statement to set the biblographic citation field in the solr document
+      #trigger the indexing process? Does this happen automatically?
+        
+      #expect statment for each field
+      expect true
+    end
+
+    describe 'no citation metadata' do
+      #let statement to set the biblographic citation field in the solr document
+      #trigger the indexing process? Does this happen automatically?
+        
+      #expect statment for each field
+      expect true
+    end
+  end
