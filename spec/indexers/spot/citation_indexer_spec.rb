6a1b0e567d25768b396b309952483e21da1b5151
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2023-03-16 15:33:53 -0400

    test format check

--- spec/indexers/spot/citation_indexer_spec.rb
+++ spec/indexers/spot/citation_indexer_spec.rb
@@ -3,11 +3,15 @@ RSpec.describe Spot::IndexesCitationMetadata do
     subject(:solr_doc) { indexer.generate_solr_document }
 
     describe 'extracting citation metadata' do
-      #let statement to set the biblographic citation field in the solr document
-      #trigger the indexing process? Does this happen automatically?
-        
-      #expect statment for each field
-      expect true
+        let(:work) { build(:publication, bibliographic_citation: ['Last, First. "Title." Journal 1.2 (2000): 1-2.']) }
+
+        it 'extracts metadata' do
+          expect(solr_doc['citation_journal_title_ss']).to eq 'Title'
+          expect(solr_doc['citation_volume_ss']).to eq '1'
+          expect(solr_doc['citation_issue_ss']).to eq '2'
+          expect(solr_doc['citation_firstpage_ss']).to eq '1'
+          expect(solr_doc['citation_lastpage_ss']).to eq '2'
+        end
     end
 
     describe 'incomplete citation metadata' do
@@ -25,4 +29,4 @@ RSpec.describe Spot::IndexesCitationMetadata do
       #expect statment for each field
       expect true
     end
-  end
+end
