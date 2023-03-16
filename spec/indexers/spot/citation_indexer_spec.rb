e83f1d3a6f419f1b201ea07516d2e1fa7ec78c75
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2023-03-16 15:40:43 -0400

    test style changes

--- spec/indexers/spot/citation_indexer_spec.rb
+++ spec/indexers/spot/citation_indexer_spec.rb
@@ -1,32 +1,40 @@
 # frozen_string_literal: true
 RSpec.describe Spot::IndexesCitationMetadata do
-    subject(:solr_doc) { indexer.generate_solr_document }
+  subject(:solr_doc) { indexer.generate_solr_document }
 
-    describe 'extracting citation metadata' do
-        let(:work) { build(:publication, bibliographic_citation: ['Last, First. "Title." Journal 1.2 (2000): 1-2.']) }
+  describe 'extracting citation metadata' do
+    let(:work) { build(:publication, bibliographic_citation: ['Last, First. "Title." Journal 1.2 (2000): 1-2.']) }
 
-        it 'extracts metadata' do
-          expect(solr_doc['citation_journal_title_ss']).to eq 'Title'
-          expect(solr_doc['citation_volume_ss']).to eq '1'
-          expect(solr_doc['citation_issue_ss']).to eq '2'
-          expect(solr_doc['citation_firstpage_ss']).to eq '1'
-          expect(solr_doc['citation_lastpage_ss']).to eq '2'
-        end
+    it 'extracts metadata' do
+        expect(solr_doc['citation_journal_title_ss']).to eq 'Title'
+        expect(solr_doc['citation_volume_ss']).to eq '1'
+        expect(solr_doc['citation_issue_ss']).to eq '2'
+        expect(solr_doc['citation_firstpage_ss']).to eq '1'
+        expect(solr_doc['citation_lastpage_ss']).to eq '2'
     end
+  end
 
-    describe 'incomplete citation metadata' do
-      #let statement to set the biblographic citation field in the solr document
-      #trigger the indexing process? Does this happen automatically?
-        
-      #expect statment for each field
-      expect true
-    end
+  describe 'incomplete citation metadata' do
+    let(:work) { build(:publication, bibliographic_citation: ['Last, First. "Title." Journal 1.2 (2000)']) }
 
-    describe 'no citation metadata' do
-      #let statement to set the biblographic citation field in the solr document
-      #trigger the indexing process? Does this happen automatically?
-        
-      #expect statment for each field
-      expect true
+    it 'extracts metadata' do
+        expect(solr_doc['citation_journal_title_ss']).to eq 'Title'
+        expect(solr_doc['citation_volume_ss']).to eq '1'
+        expect(solr_doc['citation_issue_ss']).to eq '2'
+        expect(solr_doc['citation_firstpage_ss']).to eq ''
+        expect(solr_doc['citation_lastpage_ss']).to eq ''
     end
+  end
+
+  describe 'no citation metadata' do
+    let(:work) { build(:publication, bibliographic_citation: ['']) }
+
+    it 'extracts metadata' do
+        expect(solr_doc['citation_journal_title_ss']).to eq ''
+        expect(solr_doc['citation_volume_ss']).to eq ''
+        expect(solr_doc['citation_issue_ss']).to eq ''
+        expect(solr_doc['citation_firstpage_ss']).to eq ''
+        expect(solr_doc['citation_lastpage_ss']).to eq ''
+    end
+  end
 end
