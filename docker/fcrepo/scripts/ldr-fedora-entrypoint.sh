aa977244aedba311ee5cd6dcb384d5829e0c310c
Anna Malantonio <malantoa@lafayette.edu>
2023-04-04 09:18:55 -0400

    [fcrepo] remove memory from entrypoint + use envs

--- docker/fcrepo/scripts/ldr-fedora-entrypoint.sh
+++ docker/fcrepo/scripts/ldr-fedora-entrypoint.sh
@@ -14,19 +14,32 @@ done
 # configure fcrepo to use postgres as the database for metadata
 # NOTE: fcrepo uses the hard-coded "fcrepo" database, so you'll
 #       need to ensure that the postgres service has one created.
-export MODESHAPE_CONFIG="classpath:/config/jdbc-postgresql/repository.json"
 export JAVA_OPTIONS="${JAVA_OPTIONS} \
+  -Dfcrepo.modeshape.configuration=classpath:/config/jdbc-postgresql/repository.json \
   -Dfcrepo.postgresql.username=${FCREPO_POSTGRES_USER} \
   -Dfcrepo.postgresql.password=${FCREPO_POSTGRES_PASSWORD} \
   -Dfcrepo.postgresql.host=${db_host} \
   -Dfcrepo.postgresql.port=${db_port}"
 
-# JAVA_OPTIONS from our on-campus deploy, as recommended by FCRepo wiki
+# This block + the next (jetty-overrides) are copied from the NULib source.
+# see: https://github.com/nulib/docker-fcrepo/blob/master/assets/fedora-entrypoint.sh#L3-L4
+echo "Changing ownership of /data as $(whoami)"
+chown jetty:jetty /data
+
+# see: https://github.com/nulib/docker-fcrepo/blob/master/assets/fedora-entrypoint.sh#L20-L24
+if [[ -d /jetty-overrides ]]; then
+  cd /jetty-overrides
+  for file in $(find . -type f); do cp $file ${JETTY_BASE}/$file; done
+  cd -
+fi
+
+# JAVA_OPTIONS from our on-campus deploy, as recommended by FCRepo wiki,
+# with pathways in for changing the min/max memory allotted to fcrepo
 export JAVA_OPTIONS="${JAVA_OPTIONS} \
   -server \
   -Dfile.encoding=UTF-8 \
   -Xms${FCREPO_MIN_MEMORY:-512m} \
-  -Xmx${FCREPO_MAX_MEMORY:-2048m} \
+  -Xmx${FCREPO_MAX_MEMORY:-1024m} \
   -XX:NewSize=256m \
   -XX:MaxNewSize=256m \
   -XX:MetaspaceSize=64m \
@@ -34,4 +47,5 @@ export JAVA_OPTIONS="${JAVA_OPTIONS} \
   -XX:+UseG1GC \
   -XX:+DisableExplicitGC"
 
-exec /fedora-entrypoint.sh "$@"
+# see: https://github.com/nulib/docker-fcrepo/blob/master/assets/fedora-entrypoint.sh#L40
+su -s /bin/bash -c "exec /docker-entrypoint.sh $@" jetty
