12ece1be0dd441e2ff9aad80366a227e6c8ed4a1
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2023-05-15 14:41:04 -0400

    import folder and overrides for parser

--- config/initializers/spot_overrides.rb
+++ config/initializers/spot_overrides.rb
@@ -112,6 +112,41 @@ Rails.application.config.to_prepare do
     const_set(:VISIBILITY_LABEL_CLASS, old_visibility_label_class.tap { |h| h[:metadata] = 'label-info' }.freeze)
   end
 
+  # Adding support for cloud files in importers
+  Bulkrax::ImportersController.class_eval do
+    private
+
+    def files_for_import(file, cloud_files)
+      return if file.blank? && cloud_files.blank?
+      @importer[:parser_fields]['import_file_path'] = @importer.parser.write_import_file(file)
+      if cloud_files.present?
+        # For BagIt, there will only be one bag, so we get the file_path back and set import_file_path
+        # For CSV, we expect only file uploads, so we won't get the file_path back
+        # and we expect the import_file_path to be set already
+        target = @importer.parser.retrieve_cloud_files(cloud_files)
+        @importer[:parser_fields]['import_file_path'] = target if target.present?
+      end
+      @importer.save
+    end
+  end
+
+  Bulkrax::CsvParser.class_eval do
+    def records(_opts = {})
+      return @records if @records.present?
+
+      # client = Aws::S3::Client.new()
+      file_for_import = only_updates ? parser_fields['partial_import_file_path'] : import_file_path
+      # resp = client.get_object(response_target: '/spot/tmp/import/'+file_for_import, bucket: 'bulkrax-imports', key: file_for_import)
+      # data for entry does not need source_identifier for csv, because csvs are read sequentially and mapped after raw data is read.
+      # +file_for_import
+      csv_data = entry_class.read_data('/spot/tmp/import/test.csv')
+      importer.parser_fields['total'] = csv_data.count
+      importer.save
+
+      @records = csv_data.map { |record_data| entry_class.data_for_entry(record_data, nil, self) }
+    end
+  end
+
   # Define this constant, intended to be similar to AdminSet::DEFAULT_ID
   AdminSet::STUDENT_WORK_ID = Spot::StudentWorkAdminSetCreateService::ADMIN_SET_ID
 
