cd28c2db049c76dcaa575d2d140cdd19f72505b3
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2023-05-30 11:19:31 -0400

    downloads files

--- config/initializers/spot_overrides.rb
+++ config/initializers/spot_overrides.rb
@@ -113,27 +113,38 @@ Rails.application.config.to_prepare do
   end
 
   Bulkrax::CsvParser.class_eval do
+    private 
+    
     def records(_opts = {})
       return @records if @records.present?
 
       client = Aws::S3::Client.new()
       file_for_import = only_updates ? parser_fields['partial_import_file_path'] : import_file_path
-      # file_directory = File.dirname(file_for_import)
-      # FileUtils.mkdir_p('/spot/tmp/import/'+file_directory+'/files')
+      file_directory = File.dirname(file_for_import)
+      FileUtils.mkdir_p('/spot/tmp/import/'+file_directory+'/files')
       resp = client.get_object(response_target: '/spot/tmp/import/'+file_for_import, bucket: 'bulkrax-imports', key: file_for_import)
       # data for entry does not need source_identifier for csv, because csvs are read sequentially and mapped after raw data is read.
       csv_data = entry_class.read_data('/spot/tmp/import/'+file_for_import)
-      # resp2 = client.list_objects(bucket: "bulkrax-imports", prefix: file_directory)
+      resp2 = client.list_objects(bucket: "bulkrax-imports", prefix: file_directory)
       # resp2 = client.list_objects(bucket: "bulkrax-imports")
-      # resp2.contents.each do |entry|
-      #   puts '/spot/tmp/import/'+entry.key
-      #   client.get_object(response_target: '/spot/tmp/import/'+entry.key, bucket: 'bulkrax-imports', key: entry.key)
-      # end
+      resp2.contents.each do |entry|
+        puts '/spot/tmp/import/'+entry.key
+        client.get_object(response_target: '/spot/tmp/import/'+entry.key, bucket: 'bulkrax-imports', key: entry.key)
+      end
       importer.parser_fields['total'] = csv_data.count
       importer.save
 
       @records = csv_data.map { |record_data| entry_class.data_for_entry(record_data, nil, self) }
     end
+
+    # def path_to_files(**args)
+    #   filename = args.fetch(:filename, '')
+
+    #   return @path_to_files if @path_to_files.present? && filename.blank?
+    #   @path_to_files = File.join(
+    #       zip? ? importer_unzip_path : File.dirname(import_file_path), 'file', filename
+    #     )
+    # end
   end
 
   # Define this constant, intended to be similar to AdminSet::DEFAULT_ID
