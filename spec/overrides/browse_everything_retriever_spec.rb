3bf8e2115530a7862e132f4674d237d67747c268
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2023-06-28 15:27:56 -0400

    spoof requests

--- spec/overrides/browse_everything_retriever_spec.rb
+++ spec/overrides/browse_everything_retriever_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe BrowseEverything::Retriever do
 
       before do
         stub_request(
-          :head, "http://bulkrax-imports.s3.amazonaws.com/"
+          :head, "https://s3.amazonaws.com/bulkrax-imports//test.csv"
         ).and_return(
           headers: {
             'Content-Length' => '1234'
@@ -38,7 +38,7 @@ RSpec.describe BrowseEverything::Retriever do
         )
 
         stub_request(
-          :get, "http://bulkrax-imports.s3.amazonaws.com/"
+          :get, "https://s3.amazonaws.com/bulkrax-imports//test.csv"
         ).and_return(
           headers: {
             'Content-Length' => '1234'
@@ -60,7 +60,7 @@ RSpec.describe BrowseEverything::Retriever do
       let(:url) { 'http://bulkrax-imports.s3.amazonaws.com/' }
       before do
         stub_request(
-          :get, "http://bulkrax-imports.s3.amazonaws.com/"
+          :get, "https://s3.amazonaws.com/bulkrax-imports//test.csv"
         ).to_return(
           status: 206,
           body: '%'
