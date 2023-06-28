254e7146911596d52c5a406a5bafbe09cbfa0ed2
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2023-06-28 11:27:09 -0400

    test file size

--- spec/overrides/browse_everything_retriever_spec.rb
+++ spec/overrides/browse_everything_retriever_spec.rb
@@ -4,11 +4,46 @@
 RSpec.describe BrowseEverything::Retriever do
   subject(:service) { described_class.new }
 
-  describe '.can_retrieve?' do
-    let(:uri) { 's3://bulkrax-imports/test.csv' }
+  describe '#get_file_size' do
+    subject(:computed_file_size) { retriever.file_size(options) }
 
-    it 'evaluates a url' do
-      expect(service.can_retrieve?(:uri, _headers = {})).to be true
+    let(:url) { URI.parse("s3://bulkrax-imports/test.csv") }
+    let(:headers) { {} }
+    let(:file_size) { 0 }
+    let(:options) do
+      {
+        url: url,
+        headers: headers,
+        file_size: file_size
+      }.with_indifferent_access
+    end
+
+    context 'when retrieving a resource from Amazon s3' do
+      let(:url) { URI.parse('s3://bulkrax-imports/test.csv') }
+
+      before do
+        stub_request(
+          :head, "http://bulkrax-imports.s3.amazonaws.com/"
+        ).and_return(
+          headers: {
+            'Content-Length' => '1234'
+          }
+        )
+
+        stub_request(
+          :get, "http://bulkrax-imports.s3.amazonaws.com/"
+        ).and_return(
+          headers: {
+            'Content-Length' => '1234'
+          },
+          body: 'content'
+        )
+      end
+
+      it 'calculates or retrieves the size of a file' do
+        retriever.retrieve(options) do |_chunk, _retrieved, total|
+          expect(total).to eq 1234
+        end
+      end
     end
   end
-end
