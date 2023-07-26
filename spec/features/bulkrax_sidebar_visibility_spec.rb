19356718acaf60ac66bae1930a6e9d0c3c5146e0
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2023-07-26 10:22:57 -0400

    permissions test

--- spec/features/bulkrax_sidebar_visibility_spec.rb
+++ spec/features/bulkrax_sidebar_visibility_spec.rb
@@ -25,4 +25,15 @@ RSpec.feature 'the bulkrax importers and exporters dashboard page' do
       expect(page).to have_content 'No exporters have been created.'
     end
   end
+
+  context 'when when the user is a student' do
+    let(:user) { create(:student_user) }
+
+    scenario do
+      visit '/dashboard'
+
+      expect(page).not_to have_content 'Importers'
+      expect(page).not_to have_content 'Exporters'
+    end
+  end
 end
