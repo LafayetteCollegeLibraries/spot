f856ae7561fd80ebb853f515fe65d1d260c119cf
Anna Malantonio <malantoa@lafayette.edu>
2023-08-17 14:51:47 -0400

    add mail config via ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,13 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:   ENV['RAILS_SMTP_URL'],
+    user_name: ENV['RAILS_SMTP_USER'],
+    password:  ENV['RAILS_SMTP_PASSWORD']
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
