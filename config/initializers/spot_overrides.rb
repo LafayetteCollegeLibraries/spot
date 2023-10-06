7855f6dd3e889553bc342e879d3fa04d38d9012a
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2023-10-06 11:11:09 -0400

    Auto stash before checking out "HEAD"

--- config/initializers/spot_overrides.rb
+++ config/initializers/spot_overrides.rb
@@ -141,18 +141,18 @@ Rails.application.config.to_prepare do
     end
   end
 
-  Bulkrax::CsvEntry.class_eval do
-    def build_files_metadata
-      # attaching files to the FileSet row only so we don't have duplicates when importing to a new tenant
-      build_thumbnail_files if hyrax_record.work?
+  # Bulkrax::CsvEntry.class_eval do
+  #   def build_files_metadata
+  #     # attaching files to the FileSet row only so we don't have duplicates when importing to a new tenant
+  #     build_thumbnail_files if hyrax_record.work?
 
-      file_mapping = key_for_export('file')
-      file_sets = hyrax_record.file_set? ? Array.wrap(hyrax_record) : hyrax_record.file_sets
-      filenames = map_file_sets(file_sets)
+  #     file_mapping = key_for_export('file')
+  #     file_sets = hyrax_record.file_set? ? Array.wrap(hyrax_record) : hyrax_record.file_sets
+  #     filenames = map_file_sets(file_sets)
 
-      handle_join_on_export(file_mapping, filenames, mapping['file']&.[]('join')&.present?)
-    end
-  end
+  #     handle_join_on_export(file_mapping, filenames, mapping['file']&.[]('join')&.present?)
+  #   end
+  # end
 
   Bulkrax::ParserExportRecordSet::All.class_eval do
     # Uncomment if you don't want to include collections metadata in the 'All' exports
