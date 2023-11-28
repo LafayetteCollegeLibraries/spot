0dfa140104880491015435897a479a0e4b990cfd
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2023-11-28 10:22:52 -0500

    fileset fix

--- app/models/file_set.rb
+++ app/models/file_set.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 class FileSet < ActiveFedora::Base
-  include ::Hyrax::FileSetBehavior
-  include ::Spot::MetadataOnlyVisibility
-
-  # using our own FileSetIndexer that doesn't index full-text content
-  self.indexer = Spot::FileSetIndexer
-
   # An identifier that links this record to a Bulkrax import/export record.
   # For objects created within the Hyrax UI (_not_ Bulkrax), this will be
   # filled with a default value of ["ldr:#{work.id}"]
@@ -14,4 +8,12 @@ class FileSet < ActiveFedora::Base
   property :source_identifier, predicate: ::RDF::URI('http://ldr.lafayette.edu/ns#source_identifier') do |index|
     index.as :symbol, :stored_searchable
   end
+
+  include ::Hyrax::FileSetBehavior
+  include ::Spot::MetadataOnlyVisibility
+
+  # using our own FileSetIndexer that doesn't index full-text content
+  self.indexer = Spot::FileSetIndexer
+
+  
 end
