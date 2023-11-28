49c2556586aaba53309f3ee69a61e1e55f44cfbd
Anna Malantonio <malantoa@lafayette.edu>
2023-11-28 14:20:19 -0500

    ga4 analytics setup

--- bin/spot-entrypoint.sh
+++ bin/spot-entrypoint.sh
@@ -16,28 +16,34 @@ mkdir -p "$HYRAX_UPLOAD_PATH"
 # clear out any previous PIDs
 rm -f "$app_root/tmp/pids/server.pid"
 
-# Hyrax 3 uses the HYRAX_DERIVATIVES_PATH environment variable
-# to establish where derivatives are stored, but Hyrax 2 uses
-# the singular HYRAX_DERIVATIVE_PATH, so we'll need to set that
-# for backwards compat.
-#
-# @todo remove after upgrading Hyrax
-export HYRAX_DERIVATIVE_PATH=$HYRAX_DERIVATIVES_PATH
-
 # Generate a local SSL certificate so that we can run Rails on 443,
 # but only if does not exist (prevents constantly creating a new cert)
 ssl_key="$app_root/tmp/ssl/application.key"
 ssl_cert="$app_root/tmp/ssl/application.crt"
 
 if [[ ! -f "$ssl_key" && ! -f "$ssl_cert" ]]; then
-    echo "generating ssl certificate"
+  echo "generating ssl certificate"
 
-    # @todo should these be ENV variables so we can reference them in the CMD?
-    openssl req -x509 -nodes -newkey rsa:4096 \
-        -keyout "$ssl_key" \
-        -out "$ssl_cert" \
-        -subj "/C=US/ST=Pennsylvania/L=Easton/O=Lafayette College/OU=ITS/CN=${APPLICATION_FQDN}"
+  # @todo should these be ENV variables so we can reference them in the CMD?
+  openssl req -x509 -nodes -newkey rsa:4096 \
+    -keyout "$ssl_key" \
+    -out "$ssl_cert" \
+    -subj "/C=US/ST=Pennsylvania/L=Easton/O=Lafayette College/OU=ITS/CN=${APPLICATION_FQDN}"
+fi
 
+# The Google OAuth client used in Hyrax (for Analytics viewing) requires the private key
+# to be in an actual file, so we'll decode the value from ENV and create the file.
+if [[ ! -z "$HYRAX_ANALYTICS" && ! -z "$GOOGLE_OAUTH_PRIVATE_KEY_BASE64" ]]; then
+  credentials_dir="$app_root/config/credentials"
+  mkdir -p "$credentials_dir"
+
+  credentials_path="$credentials_dir/google_oauth_private.key"
+  echo -e "$GOOGLE_OAUTH_PRIVATE_KEY_BASE64" | base64 -d > "$credentials_path"
+
+  export GOOGLE_OAUTH_PRIVATE_KEY_PATH="$credentials_path"
+
+  unset credentials_dir
+  unset credentials_path
 fi
 
 unset ssl_key
