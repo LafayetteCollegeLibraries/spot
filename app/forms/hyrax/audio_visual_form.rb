0690a98adf2a91534c20219c5924472486d80cb6
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2024-02-28 14:52:42 -0500

    stripped AV, no oembed

---
+++ app/forms/hyrax/audio_visual_form.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+module Hyrax
+  class AudioVisualForm < ::Spot::Forms::WorkForm
+    singular_form_fields :title, :embed_url
+    transforms_language_tags_for :title
+
+    self.model_class = ::AudioVisual
+    self.required_fields = [:title, :rights_statement]
+    self.terms = [
+      :title,
+      :date,
+      :rights_statement
+    ].concat(hyrax_form_fields)
+  end
+end
---
+++ app/indexers/audio_visual_indexer.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+class AudioVisualIndexer < BaseIndexer
+  self.sortable_date_property = :date
+end
