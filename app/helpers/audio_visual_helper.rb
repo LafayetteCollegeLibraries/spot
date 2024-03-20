61ff24ffa7a92fa4c7c95b366369e9371a8ccff8
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2024-03-20 16:06:35 -0400

    getting presigned url

---
+++ app/helpers/audio_visual_helper.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+#
+# Helper methods to generate the s3 presigned URL for audio/video files
+module AudioVisualHelper
+    # @param [String] s3 key
+    # @return [String] presigned URL 
+    def s3_url(key)
+      obj = Aws::S3::Object.new(bucket_name: 'av-derivatives', key: key)
+      url = obj.presigned_url(:get, expires_in: 3600)
+      return url
+    end
+  end
