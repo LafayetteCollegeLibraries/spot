3540841731980a573f598b5baedf11662c3c5816
Anna Malantonio <malantoa@lafayette.edu>
2024-05-06 18:04:08 -0400

    make sure RAILS_LOG_LEVEL is a symbol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,9 +50,8 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # Use the lowest log level to ensure availability of diagnostic information
-  # when problems arise.
-  config.log_level = ENV.fetch('RAILS_LOG_LEVEL') { :error }
+  # Configure log levels via environment
+  config.log_level = ENV.fetch('RAILS_LOG_LEVEL') { :error }.to_sym
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
