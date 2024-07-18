3123879ce58c84fac47f82d8a8c77340bb4303fd
Anna Malantonio <malantoa@lafayette.edu>
2024-07-18 14:37:11 -0400

    first pass @ resources and change_sets

---
+++ app/models/publication_resource.rb
@@ -0,0 +1,5 @@
+class PublicationResource < ::Hyrax::Work
+  include Hyrax::Schema(:base_metadata)
+  include Hyrax::Schema(:institutional_metadata)
+  include Hyrax::Schema(:publication_metadata)
+end
