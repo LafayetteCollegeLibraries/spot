1aadf259cb7bfe05e480b021afbacc4540ad47d9
Anna Malantonio <malantoa@lafayette.edu>
2024-08-02 11:57:49 -0400

    remove first-pass change_sets

--- app/models/spot/application_resource_change_set.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationResourceChangeSet < ::Hyrax::ChangeSet
-  property :title, multiple: false, required: true
-  property :resource_type, required: true
-  property :rights_statement, multiple: false, required: true
-end
