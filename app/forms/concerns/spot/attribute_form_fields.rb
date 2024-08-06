d1fb87b49666780ecabe6e6e62ad963411b44460
Anna Malantonio <malantoa@lafayette.edu>
2024-08-06 12:52:33 -0400

    more tests

---
+++ app/forms/concerns/spot/attribute_form_fields.rb
@@ -0,0 +1,43 @@
+module Spot
+  def self.AttributeFormFields(*fields)
+    AttributeFormFields.new(fields)
+  end
+
+  class AttributeFormFields < Module
+    module HelperMethods
+      def parse_attribute_values(field:, value_key: 'id')
+        adds = []
+        deletes = []
+
+        Array.wrap(self.send(:"#{field}_attributes")).each do |_, attrs|
+          if attrs['_destroy'] == 'true'
+            deletes << attrs[value_key]
+          else
+            adds << attrs[value_key]
+          end
+        end
+
+        ((Array.wrap(self.send(field.to_sym)) + adds) - deletes).uniq
+      end
+    end
+
+    def initialize(fields)
+      @fields = fields
+    end
+
+    private
+
+    def included(descendant)
+      super
+
+      descendant.include(HelperMethods)
+
+      @fields.map(&:to_sym).each do |field|
+        descendant.property(:"#{field}_attributes",
+                            virtual: true,
+                            prepopulator: ->(_opts) { self.send(:"#{field}") },
+                            populator: ->(_opts) { self.send(:"#{field}=", parse_attribute_values(field: field)) })
+      end
+    end
+  end
+end
