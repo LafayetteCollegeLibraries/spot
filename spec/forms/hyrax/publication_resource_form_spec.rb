dcfaa0635af88d5cb409bfbad31040ffc30bb94c
Anna Malantonio <malantoa@lafayette.edu>
2024-08-06 12:52:33 -0400

    more tests

--- spec/forms/hyrax/publication_resource_form_spec.rb
+++ spec/forms/hyrax/publication_resource_form_spec.rb
@@ -1,52 +1,12 @@
 # frozen_string_literal: true
 RSpec.describe Hyrax::PublicationResourceForm do
   subject(:form) { described_class.new(resource) }
-
-  let(:form_definitions) { form.class.definitions }
   let(:resource) { build(:publication_resource) }
 
-  describe '#title' do
-    let(:field) { :title }
-
-    describe 'a language-tagged form field' do
-      it 'adds a virtual #title_language property' do
-        expect(form_definitions.keys).to include('title_language')
-        expect(form_definitions['title_language'][:writeable]).to be false
-        expect(form_definitions['title_language'][:readable]).to be false
-      end
-
-      it 'adds a virtual #title_value property' do
-        expect(form_definitions.keys).to include('title_value')
-        expect(form_definitions['title_value'][:writeable]).to be false
-        expect(form_definitions['title_value'][:readable]).to be false
-      end
-
-      describe 'prepopulation' do
-        let(:resource) { build(:publication_resource, title: [RDF::Literal('Resource Title', language: :eng)]) }
-
-        it 'populates #title_value' do
-          expect { form.prepopulate! }.to change { form.title_value }.from([]).to(['Resource Title'])
-        end
-
-        it 'populates #title_language' do
-          expect { form.prepopulate! }.to change { form.title_language }.from([]).to(['eng'])
-        end
-      end
-
-      describe 'RDF Literal builder validation' do
-        let(:resource) { build(:publication_resource, title: []) }
-        let(:incoming_metadata) { { 'title_value' => ['the 400 Blows', 'Les quatres-cents coups'], 'title_language' => ['eng', 'fra'] } }
-        let(:expected_literals) do
-          [RDF::Literal('the 400 Blows', language: :eng), RDF::Literal('Les quatres-cents coups', language: :fra)]
-        end
-
-        it 'converts field _values and _languages into language-tagged RDF literals' do
-          expect { form.validate(incoming_metadata) }
-            .to change { form.send(field) }
-            .from([])
-            .to(expected_literals)
-        end
-      end
+  [:abstract, :description, :subtitle, :title, :title_alternative].each do |language_tagged_field|
+    describe "##{language_tagged_field}" do
+      let(:field) { language_tagged_field }
+      it_behaves_like 'a language-tagged resource field'
     end
   end
 end
