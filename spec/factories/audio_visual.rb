a4b74e64c0da52694ce29e053c169993b11d9fac
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2024-08-16 15:16:37 -0400

    add back resource type

--- spec/factories/audio_visual.rb
+++ spec/factories/audio_visual.rb
@@ -2,6 +2,7 @@
 FactoryBot.define do
   factory :audio_visual, traits: [:work_behavior] do
     date { ['2019-12'] }
+    resource_type { ['Video'] }
   end
 
   factory :audio_visual_with_file_set, traits: [:has_file_set]
