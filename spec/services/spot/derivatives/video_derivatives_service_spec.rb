30c3ae7fb32d24814f875b870535e996b8feafb5
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2024-08-27 10:04:02 -0400

    valid test for base derivatives

--- spec/services/spot/derivatives/video_derivatives_service_spec.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-RSpec.describe Spot::Derivatives::VideoDerivativeService, derivatives: true do
-  let(:valid_file_set) { FileSet.new }
-
-  it_behaves_like 'a Hyrax::DerivativeService'
-end
