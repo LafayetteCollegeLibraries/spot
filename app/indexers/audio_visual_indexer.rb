ca1487143b095c8cd3df04ca482517c072b36f26
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2024-09-16 16:27:57 -0400

    problem identified, indexer

--- app/indexers/audio_visual_indexer.rb
+++ app/indexers/audio_visual_indexer.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 class AudioVisualIndexer < BaseIndexer
   self.sortable_date_property = :date
+
+  # @return [Hash<String => *>]
+  def generate_solr_document
+    super.tap do |solr_doc|
+      solr_doc['stored_derivatives_ssim'] = []
+    end
+  end
 end
