6238bca6abd31c9a3aa9d97a166b35f211dbd62a
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2024-10-24 13:56:33 -0400

    lint 2

--- spec/features/create_audio_visual_spec.rb
+++ spec/features/create_audio_visual_spec.rb
@@ -5,9 +5,9 @@ RSpec.feature 'Create a Audio Visual', :clean, :js do
     stub_request(:get, "http://www.geonames.org/getJSON?geonameId=5188140&username=lafayette_dss")
       .with(
         headers: {
-        'Accept' => 'application/json',
-        'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'User-Agent' => 'Faraday v0.17.6'
+          'Accept' => 'application/json',
+          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent' => 'Faraday v0.17.6'
         })
       .to_return(status: 200, body: "", headers: {})
     # Only enqueue the ingest job, not charactarization.
