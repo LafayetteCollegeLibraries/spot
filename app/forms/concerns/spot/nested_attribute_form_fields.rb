dda12b1667ae060d3054464b555675ed951409d3
Anna Malantonio <malantoa@lafayette.edu>
2024-11-06 13:15:27 -0500

    add comments

--- app/forms/concerns/spot/nested_attribute_form_fields.rb
+++ app/forms/concerns/spot/nested_attribute_form_fields.rb
@@ -1,18 +1,12 @@
 # frozen_string_literal: true
 module Spot
-  def self.NestedAttributeFormFields(*fields)
-    NestedAttributeFormFields.new(fields)
-  end
-
   # Adds support for nested attribute fields in Hyrax forms. These fields use Select2 in the UI
   # which sends data via a hash with numbered keys pointing at URI values.
   #
   # @usage
-  #   module Hyrax
-  #     class CoolResourceForm < Hyrax::ResourceForm(CoolResource)
-  #       include Hyrax::Schema(:core_metadata)
-  #       include Spot::NestedAttributeFormFields(:subject, :location)
-  #     end
+  #   class CoolResourceForm < Hyrax::ResourceForm(CoolResource)
+  #     include Hyrax::Schema(:core_metadata)
+  #     include Spot::NestedAttributeFormFields(:subject, :location)
   #   end
   #
   # @example incoming *_attributes data
@@ -36,42 +30,54 @@ module Spot
   #      }
   #   }
   #
-  #
+  def self.NestedAttributeFormFields(*fields)
+    NestedAttributeFormFields.new(fields)
+  end
+
   class NestedAttributeFormFields < Module
     module HelperMethods
-      # Converts incoming attributes hash into an array of values and removes
-      # entries that include `{ '_destroy' => 'true' }` values.
+      # Called from within the _attributes :populator method to parse through
+      # an incoming hash and return the intended field's values — original
+      # form values concated with incoming _attributes additions (see @note below)
       #
-      # @note I believe the Resource model is responsible for casting incoming values
-      #       to different classes, iirc, so we shouldn't have to worry about that here.
-      #
-      # @param [Hash] options
-      # @option [#to_s] field
-      # @option [String] value_key (used by incoming attributes, defaults to 'id')
+      # @params [Hash] options
+      # @option [Hash] fragment (see above for incoming hash format)
+      # @option [String] field
       # @return [Array<String>]
-      def parse_attribute_values(field:, value_key: 'id')
-        attribute_values = send(:"#{field}_attributes")
-        return if attribute_values.blank?
-
+      #
+      # @note I don't believe we need to cast the values that are returned, I
+      #       think the Resource model will take care of that?
+      #
+      # @note in theory we're setting the _attributes field with the original
+      #       form values on prepopulation, so we shouldn't need to including
+      #       them in the output, but this is following Hyrax conventions in
+      #       Hyrax::Forms::PcdmObjectForm.
+      #
+      # @see https://github.com/samvera/hyrax/blob/hyrax-v3.6.0/app/forms/hyrax/forms/pcdm_object_form.rb#L31-L43
+      def parse_attribute_fragment(fragment:, field:, value_key: 'id')
         adds = []
         deletes = []
 
-        attribute_values.each do |(_idx, attrs)|
+        fragment.each do |_idx, attrs|
+          value = attrs[value_key]
           if attrs['_destroy'] == 'true'
-            deletes << attrs[value_key]
+            deletes << value
           else
-            adds << attrs[value_key]
+            adds << value
           end
         end
 
-        (adds - deletes).uniq
+        ((Array.wrap(send(field)).map(&:to_s) + adds) - deletes).uniq
       end
 
-      def update_attributes_for!(field)
-        attributes = wrap_attribute_values(field: field)
-        send(:"#{field}_attributes=", attributes)
-      end
-
+      # Called from the _attributes :prepopulator method to wrap values
+      # in the hash format used by the Select2 widget for controlled
+      # vocabulary fields.
+      #
+      # @param [Hash] options
+      # @option [Symbol,String] field
+      # @option [String] value_key
+      # @return [Hash<String => Hash<String => String>>]
       def wrap_attribute_values(field:, value_key: 'id')
         Array.wrap(send(field)).each_with_index.each_with_object({}) do |(val, idx), out|
           out[idx.to_s] = { value_key => val.to_s }
@@ -91,7 +97,7 @@ module Spot
     end
 
     def initialize(fields)
-      @fields = fields
+      @fields = fields.map(&:to_sym)
     end
 
     private
@@ -101,27 +107,19 @@ module Spot
 
       descendant.include(HelperMethods)
 
-      @fields.map(&:to_sym).each do |field|
-        descendant.define_method(:"#{field}_populator") do |fragment:, **|
-          adds = []
-          deletes = []
+      @fields.each do |field|
+        descendant.define_method(:"#{field}_attributes_prepopulator") do |_opts|
+          send(:"#{field}_attributes=", wrap_attribute_values(field: field))
+        end
 
-          fragment.each do |_idx, attrs|
-            if attrs['_destroy'] == 'true'
-              deletes << attrs['id']
-            else
-              adds << attrs['id']
-            end
-          end
-
-          combined_values = ((Array.wrap(send(field)).map(&:to_s) + adds) - deletes).uniq
-          send(:"#{field}=", combined_values)
+        descendant.define_method(:"#{field}_attributes_populator") do |fragment:, **|
+          send(:"#{field}=", parse_attribute_fragment(fragment: fragment, field: field))
         end
 
         descendant.property(:"#{field}_attributes",
                             virtual: true,
-                            prepopulator: ->(_opts) { update_attributes_for!(field) },
-                            populator: :"#{field}_populator")
+                            prepopulator: :"#{field}_attributes_prepopulator",
+                            populator: :"#{field}_attributes_populator")
       end
     end
   end
