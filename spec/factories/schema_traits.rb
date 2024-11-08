f7df59902fb381c7fe59d83fd5c9ac389855d6cf
Anna Malantonio <malantoa@lafayette.edu>
2024-11-08 17:28:31 -0500

    get indexer specs working

--- spec/factories/schema_traits.rb
+++ spec/factories/schema_traits.rb
@@ -5,26 +5,27 @@
 # in `config/metadata/{schema}.yaml`
 FactoryBot.define do
   trait :base_metadata do
-    bibliographic_citation { [] }
-    contributor { [] }
-    creator { [] }
-    description { [] }
-    identifier { [] }
-    keyword { [] }
-    language { [] }
-    location { [] }
-    note { [] }
-    physical_medium { [] }
-    publisher { [] }
-    related_resource { [] }
+    bibliographic_citation { ['Lastname, First. Title of piece.'] }
+    contributor { ['Contributor, First-Name', 'Person, Another'] }
+    creator { ['Creator, Anne'] }
+    description { ['An account of the resource'] }
+    identifier { ['hdl:10385/abc123'] }
+    keyword { ['photo'] }
+    language { ['en'] }
+    location { ['http://sws.geonames.org/5188140/'] }
+    note { ['Some staff-side information'] }
+    physical_medium { ['Photograph'] }
+    publisher { ['Lafayette College'] }
+    related_resource { ['http://another-resource.com'] }
     resource_type { ['Other'] }
-    rights_holder { [] }
-    rights_statement { ['http://rightsstatements.org/vocab/NKC/1.0/'] }
-    source { [] }
+    rights_holder { ['Holder, R.'] }
+    rights_statement { ['http://creativecommons.org/publicdomain/mark/1.0/'] }
+    source { ['Lafayette College'] }
     source_identifier { [] }
-    subject { [] }
-    subtitle { [] }
-    title_alternative { [] }
+    subject { ['http://id.worldcat.org/fast/1061714'] }
+    subtitle { ['An object to view'] }
+    title { ['A Fabulous Work'] }
+    title_alternative { ['An alternative title for the work.'] }
   end
 
   trait :core_metadata do
@@ -35,37 +36,37 @@ FactoryBot.define do
   end
 
   trait :image_metadata do
-    date { [] }
-    date_associated { [] }
-    date_scope_note { [] }
-    donor { [] }
-    inscription { [] }
-    original_item_extent { [] }
-    repository_location { [] }
-    requested_by { [] }
-    research_assistance { [] }
-    subject_ocm { [] }
+    date { ['2024-11'] }
+    date_associated { ['2024'] }
+    date_scope_note { ['Printed information on the backs of postcards that provides information relevent to dating the postcard itself (not the image).'] }
+    donor { ['Alumnus, Anne Esteemed'] }
+    inscription { ['hey look over here'] }
+    original_item_extent { ['24 x 19.5 cm.'] }
+    repository_location { ['On that one shelf in the back'] }
+    requested_by { ['Requester, Jennifer Q.'] }
+    research_assistance { ['Student, Ashley'] }
+    subject_ocm { ['000 VALUE'] }
   end
 
   trait :institutional_metadata do
-    academic_department { [] }
-    division { [] }
-    organization { [] }
+    academic_department { ['Art'] }
+    division { ['Humanities'] }
+    organization { ['Lafayette College'] }
   end
 
   trait :publication_metadata do
-    abstract { [] }
+    abstract { ['A short description of the thing'] }
     date_issued { [Time.zone.now.strftime('%Y-%m-%d')] }
-    date_available { [] }
-    editor { [] }
-    license { [] }
+    date_available { ['2024-11-08'] }
+    editor { ['Sweeney, Mary'] }
+    license { ['This is some licensing text'] }
   end
 
   trait :student_work_metadata do
-    abstract { [] }
-    access_note { [] }
-    advisor { [] }
-    date { [] }
-    date_available { [] }
+    abstract { ['A short description of the thing'] }
+    access_note { ['Here is how to access the thing'] }
+    advisor { ['Smartfellow, Jane'] }
+    date { ['2024-11-08'] }
+    date_available { ['2024-11-08'] }
   end
 end
--- spec/indexers/student_work_resource_indexer_spec.rb
+++ spec/indexers/student_work_resource_indexer_spec.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 RSpec.describe StudentWorkResourceIndexer, valkyrization: true do
   it_behaves_like 'a BaseResourceIndexer'
+
+  describe 'student_work_metadata' do
+    it_behaves_like 'it indexes', :abstract, to: ['abstract_tesim']
+    it_behaves_like 'it indexes', :access_note, to: ['access_note_tesim']
+    it_behaves_like 'it indexes', :advisor, to: ['advisor_ssim']
+    it_behaves_like 'it indexes', :date, to: ['date_ssim']
+    it_behaves_like 'it indexes', :date_available, to: ['date_available_ssim']
+  end
 end
