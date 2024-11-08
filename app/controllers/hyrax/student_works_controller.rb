628cf11e253d4106f610e70050495232e42108c0
Anna Malantonio <malantoa@lafayette.edu>
2024-11-08 17:27:35 -0500

    set controllers back to old Hyrax models

--- app/controllers/hyrax/student_works_controller.rb
+++ app/controllers/hyrax/student_works_controller.rb
@@ -3,8 +3,7 @@ module Hyrax
   class StudentWorksController < ApplicationController
     include Spot::WorksControllerBehavior
 
-    # self.curation_concern_type = ::StudentWork
-    self.curation_concern_type = StudentWorkResource
+    self.curation_concern_type = ::StudentWork
     self.show_presenter = Hyrax::StudentWorkPresenter
 
     # Modifying the search_builder_class to our subclass which allows
