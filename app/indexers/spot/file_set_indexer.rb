89ffdc916a52a2a0404a245d6b39631444985c38
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2024-11-13 15:53:57 -0500

    transcript name flag

--- app/indexers/spot/file_set_indexer.rb
+++ app/indexers/spot/file_set_indexer.rb
@@ -6,6 +6,7 @@ module Spot
     def generate_solr_document
       super.reject { |k, _v| k == 'all_text_timv' }.tap do |solr_document|
         solr_document['original_filenames_ssim'] = object.files.map(&:original_name)
+        solr_document['transcript_name_ssi'] = object.transcript.nil? ? nil : object.transcript.original_name
       end
     end
   end
