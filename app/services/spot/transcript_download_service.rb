b76ebdeb9fad8be9db53797e8e12051237ed29db
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2024-11-13 16:47:23 -0500

    lint

--- app/services/spot/transcript_download_service.rb
+++ app/services/spot/transcript_download_service.rb
@@ -21,11 +21,6 @@ module Spot
       path = "/tmp/" + @transcript_name
       s3_client.get_object(key: @transcript_name, bucket: s3_source, response_target: path)
       Spot::FileSetTranscriptAttachmentService.attach(path: path, file_set: @file_set)
-      #remove_transcript(path)
-    end
-
-    def remove_transcript(path)
-      FileUtils.rm_f(path) if File.exist?(path)
     end
 
     private
