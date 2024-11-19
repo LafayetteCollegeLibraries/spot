a0710ffd41914362e75ff031083cd8e89d4832fd
Anna Malantonio <malantoa@lafayette.edu>
2024-11-19 13:55:51 -0500

    add support for standard_/local_ identifiers in form

--- spec/forms/student_work_resource_form_spec.rb
+++ spec/forms/student_work_resource_form_spec.rb
@@ -2,4 +2,6 @@
 RSpec.describe StudentWorkResourceForm, valkyrization: true do
   subject(:form) { described_class.new(resource) }
   let(:resource) { build(:student_work_resource) }
+
+  it_behaves_like 'it supports local/standard identifiers'
 end
