0c464aa752c010f0f209df8e8313b0612217d1b9
Anna Malantonio <malantoa@lafayette.edu>
2024-11-20 15:02:11 -0500

    listener specs

---
+++ spec/services/spot/listeners/parent_collection_membership_listener_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+RSpec.describe Spot::Listeners::ParentCollectionMembershipListener, valkyrization: true do
+  let(:listener) { described_class.new }
+  let(:user) { build(:admin_user) }
+
+  # @todo should these be factories?
+  let(:parent_collection) { Hyrax::PcdmCollection.new(id: 'parent', title: ['Parent Collection']) }
+  let(:child_collection) { Hyrax::PcdmCollection.new(id: 'child', title: ['Child Collection'], member_of_collection_ids: ['parent']) }
+  let(:resource) { build(:publication_resource, **metadata) }
+  let(:metadata) { { member_of_collection_ids: ['child'] } }
+
+  before do
+    allow(Hyrax.query_service)
+      .to receive(:find_by_alternate_identifier)
+      .with(alternate_identifier: 'parent')
+      .and_return(parent_collection)
+
+    allow(Hyrax.query_service)
+      .to receive(:find_by_alternate_identifier)
+      .with(alternate_identifier: 'child')
+      .and_return(child_collection)
+
+    allow(Hyrax.persister).to receive(:save)
+  end
+
+  describe '#on_object_metadata_updated' do
+    it 'it adds the parent collection id to resource#member_of_collection_ids' do
+      expect { listener.on_object_metadata_updated(object: resource, user: user) }
+        .to change { resource.member_of_collection_ids }
+        .from(['child'])
+        .to(['child', 'parent'])
+
+      expect(Hyrax.persister).to have_received(:save).with(resource: resource)
+    end
+
+    context 'when the resource does not belong to a collection' do
+      let(:metadata) { {} }
+
+      it 'does nothing' do
+        expect { listener.on_object_metadata_updated(object: resource, user: user) }
+          .not_to change { resource.member_of_collection_ids }
+      end
+    end
+  end
+end
