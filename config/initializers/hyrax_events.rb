8f55889d099f25f53534b97165fd1a0cb7c0ae6a
Anna Malantonio <malantoa@lafayette.edu>
2024-11-21 07:13:57 -0500

    typos

--- config/initializers/hyrax_events.rb
+++ config/initializers/hyrax_events.rb
@@ -6,16 +6,18 @@
 # @see https://www.rubydoc.info/github/samvera/hyrax/Hyrax/Publisher
 # @see https://github.com/samvera/hyrax/blob/hyrax-v3.5.0/lib/hyrax/publisher.rb
 module Spot
-  class ApplicationListener
-    # Mint Handles for records when they are deposited
-    def on_object_deposited(event)
-      MintHandleJob.perform_later(event[:object])
+  module Listeners
+    class ApplicationListener
+      # Mint Handles for records when they are deposited
+      def on_object_deposited(event)
+        MintHandleJob.perform_later(event[:object])
+      end
     end
   end
 end
 
 Rails.application.config.to_prepare do
-  Hyrax.publisher.subscribe(Spot::ApplicationListener.new)
-  Hyrax.publisher.subscribe(Spot::ParentCollectionMembershipListener.new)
-  Hyrax.publisher.subscribe(Spot::SolrSuggestDictionaryListener.new)
+  Hyrax.publisher.subscribe(Spot::Listeners::ApplicationListener.new)
+  Hyrax.publisher.subscribe(Spot::Listeners::ParentCollectionMembershipListener.new)
+  Hyrax.publisher.subscribe(Spot::Listeners::SolrSuggestDictionariesListener.new)
 end
