51a03fdeb00b21fefa4208fa5a396ad9951d904a
Anna Malantonio <malantoa@lafayette.edu>
2024-11-22 08:28:56 -0500

    more test work

--- spec/forms/image_resource_form_spec.rb
+++ spec/forms/image_resource_form_spec.rb
@@ -3,28 +3,52 @@ RSpec.describe ImageResourceForm, valkyrization: true do
   subject(:form) { described_class.new(resource) }
   let(:resource) { build(:image_resource) }
 
-  describe '#title' do
-    let(:field) { :title }
-    it_behaves_like 'a language-tagged resource field'
+  it_behaves_like 'it supports local/standard identifiers'
+  it_behaves_like 'it includes Hyrax::FormFields', schema: :core_metadata
+  it_behaves_like 'it includes Hyrax::FormFields', schema: :base_metadata
+  it_behaves_like 'it includes Hyrax::FormFields', schema: :image_metadata
+
+  describe 'language-tagged resource fields' do
+    describe '#title' do
+      let(:field) { :title }
+      it_behaves_like 'a language-tagged resource field'
+    end
+
+    describe '#title_alternative' do
+      let(:field) { :title_alternative }
+      it_behaves_like 'a language-tagged resource field'
+    end
+
+    describe '#subtitle' do
+      let(:field) { :subtitle }
+      it_behaves_like 'a language-tagged resource field'
+    end
+
+    describe '#description' do
+      let(:field) { :description }
+      it_behaves_like 'a language-tagged resource field'
+    end
+
+    describe '#inscription' do
+      let(:field) { :inscription }
+      it_behaves_like 'a language-tagged resource field'
+    end
   end
 
-  describe '#title_alternative' do
-    let(:field) { :title_alternative }
-    it_behaves_like 'a language-tagged resource field'
-  end
+  describe 'nested attribute fields' do
+    describe '#subject' do
+      let(:field) { :subject }
+      it_behaves_like 'a nested attribute field'
+    end
 
-  describe '#subtitle' do
-    let(:field) { :subtitle }
-    it_behaves_like 'a language-tagged resource field'
-  end
+    describe '#language' do
+      let(:field) { :language }
+      it_behaves_like 'a nested attribute field'
+    end
 
-  describe '#description' do
-    let(:field) { :description }
-    it_behaves_like 'a language-tagged resource field'
-  end
-
-  describe '#inscription' do
-    let(:field) { :inscription }
-    it_behaves_like 'a language-tagged resource field'
+    describe '#subject_ocm' do
+      let(:field) { :subject_ocm }
+      it_behaves_like 'a nested attribute field'
+    end
   end
 end
--- spec/forms/student_work_resource_form_spec.rb
+++ spec/forms/student_work_resource_form_spec.rb
@@ -4,4 +4,35 @@ RSpec.describe StudentWorkResourceForm, valkyrization: true do
   let(:resource) { build(:student_work_resource) }
 
   it_behaves_like 'it supports local/standard identifiers'
+  it_behaves_like 'it includes Hyrax::FormFields', schema: :core_metadata
+  it_behaves_like 'it includes Hyrax::FormFields', schema: :base_metadata
+  it_behaves_like 'it includes Hyrax::FormFields', schema: :student_work_metadata
+  it_behaves_like 'it includes Hyrax::FormFields', schema: :institutional_metadata
+
+  describe 'nested attribute fields' do
+    describe '#subject' do
+      let(:field) { :subject }
+      it_behaves_like 'a nested attribute field'
+    end
+
+    describe '#language' do
+      let(:field) { :language }
+      it_behaves_like 'a nested attribute field'
+    end
+
+    describe '#academic_department' do
+      let(:field) { :academic_department }
+      it_behaves_like 'a nested attribute field'
+    end
+
+    describe '#advisor' do
+      let(:field) { :advisor }
+      it_behaves_like 'a nested attribute field'
+    end
+
+    describe '#division' do
+      let(:field) { :division }
+      it_behaves_like 'a nested attribute field'
+    end
+  end
 end
