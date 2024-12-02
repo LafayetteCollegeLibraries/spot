8f70e44c2e9a5b5749dc241429aa9c88cf1ce7d8
Anna Malantonio <malantoa@lafayette.edu>
2024-12-02 09:41:14 -0500

    comments

--- app/services/spot/simple_schema_loader.rb
+++ app/services/spot/simple_schema_loader.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 module Spot
+  # Using our own SchemaLoader for the time being to bypass an issue with the Hyrax loader
+  # where 'type: uri' and 'multiple: true' fields were passing the Dry::Struct interface default
+  # value of +Dry::Types::Unknown+ through, as it responds true to #present?, which was resulting
+  # in new resources having default URI values of 'Unknown'
+  #
+  # @example
+  #   # config/metadata/cool_metadata.yml
+  #   attributes:
+  #     subject:
+  #       type: uri
+  #       multiple: true
+  #
+  #   # app/models/cool_resource.rb
+  #   class CoolResource < Hyrax::Resource
+  #     include Hyrax::Schema(:cool_metadata)
+  #   end
+  #
+  #   CoolResource.new.subject
+  #   => [#<RDF::URI:0x9743c URI:Undefined>]
+  #
   class SimpleSchemaLoader < Hyrax::SimpleSchemaLoader
     class AttributeDefinition
       def type
