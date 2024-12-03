03abf400e4a74dad7dd9c3bb70e7563ae38a37eb
Anna Malantonio <malantoa@lafayette.edu>
2024-12-03 15:25:52 -0500

    fix SimpleSchemaLoader

--- app/services/spot/simple_schema_loader.rb
+++ app/services/spot/simple_schema_loader.rb
@@ -20,8 +20,16 @@ module Spot
   #   CoolResource.new.subject
   #   => [#<RDF::URI:0x9743c URI:Undefined>]
   #
+  #   # app/models/better_resource.rb
+  #   class BetterResource < Hyrax::Resource
+  #     include Hyrax::Schema(:cool_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
+  #   end
+  #
+  #   BetterResource.new.subject
+  #   => []
+  #
   class SimpleSchemaLoader < Hyrax::SimpleSchemaLoader
-    class AttributeDefinition
+    class AttributeDefinition < Hyrax::SimpleSchemaLoader::AttributeDefinition
       def type
         collection_type = if config['multiple']
                             Valkyrie::Types::Array.constructor { |v| Array(v).select { |x| x.present? && x != Dry::Types::Undefined } }
@@ -30,6 +38,16 @@ module Spot
                           end
         collection_type.of(type_for(config['type']))
       end
+    end
+
+    private
+
+    # Map the definitions to use our overloaded AttributeDefinition class.
+    #
+    # @param [#to_s] schema_name
+    # @return [Enumerable<Spot::SimpleSchemaLoader::AttributeDefinition]
+    def definitions(schema_name)
+      super(schema_name).map { |prev| AttributeDefinition.new(prev.name, prev.config) }
     end
   end
 end
