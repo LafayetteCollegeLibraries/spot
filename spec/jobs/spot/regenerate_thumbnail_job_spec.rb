b0d74941f9f6dfe320994a4dab41efc942b94a05
Anna Malantonio <malantoa@lafayette.edu>
2024-12-06 16:50:44 -0500

    fix regenerate_thumbnail_job_spec

--- spec/jobs/spot/regenerate_thumbnail_job_spec.rb
+++ spec/jobs/spot/regenerate_thumbnail_job_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
-RSpec.describe Spot::RegenerateThumbnailJob do
-  let(:work) { instance_double(Publication, id: 'pub123abc', thumbnail_id: thumbnail_id, update_index: true) }
-  let(:file_set) { instance_double(FileSet, id: 'fst123abc', update_index: true, reload: true) }
+RSpec.describe Spot::RegenerateThumbnailJob, valkyrization: true do
+  let(:work) { instance_double(Publication, id: 'pub123abc', thumbnail_id: thumbnail_id) }
+  let(:file_set) { instance_double(FileSet, id: 'fst123abc') }
   let(:thumbnail_id) { file_set.id }
   let(:thumbnail_service_double) { instance_double(Spot::Derivatives::ThumbnailService) }
   let(:thumbnail_path) { '/path/to/thumbnail.jpg' }
@@ -14,10 +14,11 @@ RSpec.describe Spot::RegenerateThumbnailJob do
 
     allow(Spot::Derivatives::ThumbnailService)
       .to receive(:new)
-      .with(file_set)
       .and_return(thumbnail_service_double)
 
-    allow(FileSet).to receive(:find).with(file_set.id).and_return(file_set)
+    allow(Hyrax.query_service).to receive(:find_by_alternate_identifier).with(alternate_identifier: thumbnail_id).and_return(file_set)
+    allow(Hyrax.persister).to receive(:save)
+
     allow(thumbnail_service_double).to receive(:create_derivatives).with(thumbnail_path)
   end
 
@@ -36,9 +37,7 @@ RSpec.describe Spot::RegenerateThumbnailJob do
       described_class.perform_now(work)
 
       expect(thumbnail_service_double).to have_received(:create_derivatives).with(thumbnail_path)
-      expect(file_set).to have_received(:reload)
-      expect(file_set).to have_received(:update_index)
-      expect(work).to have_received(:update_index)
+      expect(Hyrax.persister).to have_received(:save).exactly(2).times
     end
   end
 end
