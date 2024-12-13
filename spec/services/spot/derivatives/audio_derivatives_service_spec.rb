612eb0ab51f730049e0e78a7ea69fb34d48503c7
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2024-12-13 10:21:37 -0500

    have_recieved

--- spec/services/spot/derivatives/audio_derivatives_service_spec.rb
+++ spec/services/spot/derivatives/audio_derivatives_service_spec.rb
@@ -256,8 +256,8 @@ RSpec.describe Spot::Derivatives::AudioDerivativeService, derivatives: true do
       end
 
       it 'should call rename' do
-        # expect(service).to receive(:rename_premade_derivative).with('example_derivative-480.mp3', 0)
-        expect(service).to receive(:rename_premade_derivative).with('example_derivative-1080.mp3', 1)
+        expect(service).to have_received(:rename_premade_derivative).with('example_derivative-480.mp3', 0)
+        expect(service).to have_received(:rename_premade_derivative).with('example_derivative-1080.mp3', 1)
       end
 
       it { is_expected.to eq(true) }
