e6f750c22a4a3b9f8fb8f2db5bb37ccc152d0af2
Anna Malantonio <malantoa@lafayette.edu>
2025-01-15 13:39:02 -0500

    fix rights_statement indexing issue?

--- app/indexers/concerns/indexes_rights_statements_and_labels.rb
+++ app/indexers/concerns/indexes_rights_statements_and_labels.rb
@@ -2,16 +2,18 @@
 module IndexesRightsStatementsAndLabels
   def to_solr
     super.tap do |document|
-      document['rights_statement_ssim'] ||= []
-      document['rights_statement_label_ssim'] ||= []
-      document['rights_statement_shortcode_ssim'] ||= []
+      document['rights_statement_ssim'] = []
+      document['rights_statement_label_ssim'] = []
+      document['rights_statement_shortcode_ssim'] = []
 
-      resource.rights_statement.each do |original_uri|
-        value = original_uri.is_a?(ActiveTriples::Resource) ? original_uri.id : original_uri
+      Array.wrap(resource.rights_statement).each do |original_uri|
+        value = original_uri.to_s
+        label = rights_service.label(value) { value }
+        shortcode = rights_service.shortcode(value) { nil }
 
         document['rights_statement_ssim'] << value
-        document['rights_statement_label_ssim'] << rights_service.label(value) { value }
-        document['rights_statement_shortcode_ssim'] << rights_service.shortcode(value) { nil }
+        document['rights_statement_label_ssim'] << label
+        document['rights_statement_shortcode_ssim'] << shortcode
       end
     end
   end
