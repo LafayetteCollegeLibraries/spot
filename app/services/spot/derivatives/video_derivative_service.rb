aed9f53d79186456a66ad59306473f3125cf121d
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2025-01-24 14:24:09 -0500

    try logger

--- app/services/spot/derivatives/video_derivative_service.rb
+++ app/services/spot/derivatives/video_derivative_service.rb
@@ -41,12 +41,12 @@ module Spot
         base_file = filename.to_s.split('/')[-1].split('.')[0]
         project_name = base_file.split('_')[0]
         prefix = project_name + "/" + base_file + "_derivative"
-        Rails.logger.warn { 'Derivative Prefix: ' + prefix }
+        logger.warn { 'Derivative Prefix: ' + prefix }
         object_list = s3_client.list_objects(bucket: s3_source, prefix: prefix).to_h[:contents]
 
         return false if object_list.nil?
 
-        Rails.logger.warn { 'Derivative list size: ' + object_list.length.to_s }
+        logger.warn { 'Derivative list size: ' + object_list.length.to_s }
 
         premade_derivatives = []
         object_list.each do |object|
@@ -82,15 +82,15 @@ module Spot
         base_file = filename.to_s.split('/')[-1].split('.')[0]
         project_name = base_file.split('_')[0]
         transcript_name = project_name + "/" + base_file + "_caption.vtt"
-        Rails.logger.warn { 'Transcript Name: ' + transcript_name }
+        logger.warn { 'Transcript Name: ' + transcript_name }
         begin
           s3_client.head_object(bucket: s3_source, key: transcript_name)
         rescue Aws::S3::Errors::NotFound
-          Rails.logger.warn('Transcript not found.')
+          logger.warn('Transcript not found.')
         else
           # enqueue job
           Spot::TranscriptJob.perform_later(file_set: file_set, transcript_name: transcript_name)
-          Rails.logger.warn { 'Transcript job enqueued.' }
+          logger.warn { 'Transcript job enqueued.' }
         end
       end
 
