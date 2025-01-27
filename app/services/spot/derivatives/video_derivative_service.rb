510ebf11c3f603917d6c697811b152e62795c287
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2025-01-27 09:05:32 -0500

    sidekiq logger

--- app/services/spot/derivatives/video_derivative_service.rb
+++ app/services/spot/derivatives/video_derivative_service.rb
@@ -41,12 +41,12 @@ module Spot
         base_file = filename.to_s.split('/')[-1].split('.')[0]
         project_name = base_file.split('_')[0]
         prefix = project_name + "/" + base_file + "_derivative"
-        logger.warn { 'Derivative Prefix: ' + prefix }
+        Sidekiq::Logging.logger.warn { 'Derivative Prefix: ' + prefix }
         object_list = s3_client.list_objects(bucket: s3_source, prefix: prefix).to_h[:contents]
 
         return false if object_list.nil?
 
-        logger.warn { 'Derivative list size: ' + object_list.length.to_s }
+        Sidekiq::Logging.logger.warn { 'Derivative list size: ' + object_list.length.to_s }
 
         premade_derivatives = []
         object_list.each do |object|
@@ -82,15 +82,15 @@ module Spot
         base_file = filename.to_s.split('/')[-1].split('.')[0]
         project_name = base_file.split('_')[0]
         transcript_name = project_name + "/" + base_file + "_caption.vtt"
-        logger.warn { 'Transcript Name: ' + transcript_name }
+        Sidekiq::Logging.logger.warn { 'Transcript Name: ' + transcript_name }
         begin
           s3_client.head_object(bucket: s3_source, key: transcript_name)
         rescue Aws::S3::Errors::NotFound
-          logger.warn('Transcript not found.')
+          Sidekiq::Logging.logger.warn('Transcript not found.')
         else
           # enqueue job
           Spot::TranscriptJob.perform_later(file_set: file_set, transcript_name: transcript_name)
-          logger.warn { 'Transcript job enqueued.' }
+          Sidekiq::Logging.logger.warn { 'Transcript job enqueued.' }
         end
       end
 
@@ -126,7 +126,7 @@ module Spot
       # only run service if bucket is defined and file includes video mime types
       def valid?
         if s3_bucket.blank?
-          Rails.logger.warn('Skipping audio derivative generation because the AWS_AUDIO_VISUAL_BUCKET environment variable is not defined.')
+          Sidekiq::Logging.logger.warn('Skipping audio derivative generation because the AWS_AUDIO_VISUAL_BUCKET environment variable is not defined.')
           return false
         end
 
