56191161dbe7b4489c5af5b89af34b37018d4d08
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2025-02-20 09:22:19 -0500

    the rest of the files

---
+++ app/forms/audio_visual_resource_form.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+# Form to edit AudioVisualResource objects
+class AudioVisualResourceForm < ::Hyrax::Forms::ResourceForm(AudioVisualResource)
+  include Hyrax::FormFields(:base_metadata)
+  include Hyrax::FormFields(:audio_visual_metadata)
+
+  include Spot::LanguageTaggedFormFields(:title, :title_alternative, :subtitle, :description, :inscription)
+  include Spot::NestedAttributeFormFields(:language)
+  include Spot::IdentifierFormFields
+
+  validates_with Spot::EdtfDateValidator, fields: [:date]
+end
