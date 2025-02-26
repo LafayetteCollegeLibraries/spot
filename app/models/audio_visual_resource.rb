42121c1fc24d33375ad13fc5ee1043edd38e67ac
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2025-02-26 10:50:28 -0500

    attribute
    broken

--- app/models/audio_visual_resource.rb
+++ app/models/audio_visual_resource.rb
@@ -2,4 +2,6 @@
 class AudioVisualResource < ::Hyrax::Work
   include Hyrax::Schema(:base_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
   include Hyrax::Schema(:audio_visual_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
+
+  attribute :stored_derivatives, Valkyrie::Types::String
 end
