a66bcdae085869af32c9d58c202eee171c05db50
Anna Malantonio <malantoa@lafayette.edu>
2025-02-27 10:05:46 -0500

    bugfix to load Hyrax::Forms::ResourceForm

--- app/services/spot/work_form_service.rb
+++ app/services/spot/work_form_service.rb
@@ -4,6 +4,9 @@ module Spot
     def self.form_class(curation_concern)
       super
     rescue NameError
+      # need to touch this to ensure that the class is loaded before we inherit from it
+      'Hyrax::Forms::ResourceForm'.constantize
+
       Object.const_get("#{curation_concern.model_name.name}Form")
     end
   end
