b951254193d6648e1ab6f9a4915a14dac48449b1
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2025-03-19 10:35:17 -0400

    factories and indexer test

---
+++ spec/factories/audio_visual_resource.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+FactoryBot.define do
+  factory :audio_visual_resource_with_required_fields_only, parent: :base_resource, class: 'AudioVisualResource' do
+    date { [Time.zone.now.strftime('%Y-%m-%d')] }
+    resource_type { ['Other'] }
+    rights_statement { ['http://creativecommons.org/publicdomain/mark/1.0/'] }
+  end
+
+  factory :audio_visual_resource,
+          parent: :audio_visual_resource_with_required_fields_only,
+          traits: [:base_metadata, :audio_visual_metadata]
+end
