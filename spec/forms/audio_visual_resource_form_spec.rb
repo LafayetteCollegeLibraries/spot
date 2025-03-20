7867476e5abd93ccc643bd89586a0319f04fee98
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2025-03-20 10:34:07 -0400

    form primary terms test

--- spec/forms/audio_visual_resource_form_spec.rb
+++ spec/forms/audio_visual_resource_form_spec.rb
@@ -43,4 +43,39 @@ RSpec.describe AudioVisualResourceForm, valkyrization: true do
       it_behaves_like 'a controlled vocabulary field'
     end
   end
+
+  describe '#primary_terms' do
+    subject(:primary_terms) { described_class.primary_terms }
+
+    describe 'includes fields' do
+      it { is_expected.to include :title }
+      it { is_expected.to include :date }
+      it { is_expected.to include :resource_type }
+      it { is_expected.to include :rights_statement }
+      it { is_expected.to include :rights_holder }
+      it { is_expected.to include :subtitle }
+      it { is_expected.to include :title_alternative }
+      it { is_expected.to include :date_associated }
+      it { is_expected.to include :creator }
+      it { is_expected.to include :contributor }
+      it { is_expected.to include :publisher }
+      it { is_expected.to include :source }
+      it { is_expected.to include :standard_identifier }
+      it { is_expected.to include :local_identifier }
+      it { is_expected.to include :description }
+      it { is_expected.to include :inscription }
+      it { is_expected.to include :subject }
+      it { is_expected.to include :keyword }
+      it { is_expected.to include :language }
+      it { is_expected.to include :physical_medium }
+      it { is_expected.to include :original_item_extent }
+      it { is_expected.to include :location }
+      it { is_expected.to include :repository_location }
+      it { is_expected.to include :note }
+      it { is_expected.to include :related_resource }
+      it { is_expected.to include :research_assistance }
+      it { is_expected.to include :provenance }
+      it { is_expected.to include :barcode }
+    end
+  end
 end
