625ecb849a62fe9685503398979f194199ca4679
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2025-03-21 15:02:24 -0400

    persister

--- spec/services/spot/derivatives/video_derivatives_service_spec.rb
+++ spec/services/spot/derivatives/video_derivatives_service_spec.rb
@@ -36,7 +36,11 @@ RSpec.describe Spot::Derivatives::VideoDerivativeService, derivatives: true do
     allow(Hyrax.query_service)
       .to receive(:find_by_alternate_identifier)
       .with(alternate_identifier: "1234")
-      .and_return(:_file_set)
+      .and_return(file_set)
+
+    allow(Hyrax.persister)
+      .to receive(:save)
+      .with(resource: file_set)
 
     allow(Hyrax::DerivativePath)
       .to receive(:derivative_path_for_reference)
