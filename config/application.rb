fc0844836ab3f30f5268200a840b3a5c73b0135f
Anna Malantonio <malantoa@lafayette.edu>
2025-07-24 08:44:31 -0400

    some comments

--- config/application.rb
+++ config/application.rb
@@ -4,14 +4,18 @@ require_relative 'boot'
 require 'rails/all'
 require 'sprockets/es6'
 require 'rack-cas/session_store/active_record'
-require 'deprecation'
 
+# Some gems in the Samvera stack use the 'deprecation' gem instead of
+# ActiveSupport::Deprecation calls, so we need to also set _that_ gem's
+# default behavior before requiring the stack's dependencies.
+#
+# @note dry-types uses its own Deprecation class but doesn't provide an API for silencing
 if ActiveModel::Type::Boolean.new.cast(ENV.fetch('SPOT_IGNORE_DEPRECATIONS', false))
+  require 'deprecation'
+
   ActiveSupport::Deprecation.silence do
     Deprecation.default_deprecation_behavior = :silence
 
-    # Require the gems listed in Gemfile, including any gems
-    # you've limited to :test, :development, or :production.
     Bundler.require(*Rails.groups)
   end
 else
