b899723c88e033caad65e9b6a2bf5717cc7825f2
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2025-09-25 11:41:49 -0400

    clean up and documentation

--- app/factories/concerns/spot/bulkrax_object_factory_find_patch.rb
+++ app/factories/concerns/spot/bulkrax_object_factory_find_patch.rb
@@ -1,73 +1,46 @@
 # frozen_string_literal: true
 module Spot
-  # Bulkrax will display a link to an imported work's record but the ObjectFactory
-  # appears to be searching for a work's 'source_identifier' as the object's ID.
-  # This modifies the search to query Solr for the work's source_identifier and
-  # then return the object if it's found.
+  # We made this patch because there are two places that the original ‘find’
+  # method in Bulkrax was failing to perform. First, Bulkrax will display a link
+  # to an imported work's record but the ObjectFactory appears to be searching
+  # for a work's 'source_identifier' as the object's ID. The second is that the ‘find’
+  # Method is what is used to match and object with a collection at ingest, also
+  # using 'source_identifier' as the object's ID. We believe that both of these
+  # problems arise from our setup being on Hyrax 3.6.0 and unvalkyrized, and
+  # that we can retire this patch once larger upgrades are made. This patch adds
+  # a new method, ‘find_by_source_identifier’, which is added to the front of the
+  # queue of methods to try in ‘find’. Notably, ‘find_by_source_identifier’ does not
+  # succeed when called during ingest to make the link, but for some reason truly
+  # unknown to us, if this method is called before ‘search_by_identifier’, that
+  # method then succeeds where it would typically fail. The order of those methods
+  # being switched does not produce the same result, both methods fail in that case.
+  # However, once the CreateRelationshipsJob stage is reached,
+  # ‘find_by_source_identifier’ always succeeds where ‘search_by_identifier’ always
+  # fails. Truly incomprehensible. Nonetheless, this order seems to fulfill all desired
+  # functions.
   #
   # @see config/initializers/spot_overrides.rb
   module BulkraxObjectFactoryFindPatch
     extend ActiveSupport::Concern
 
-    # For some reason, just overwriting :find_by_identifier wasn't working,
-    # so we'll write over the :find method to try finding by source_identifier
-    # first and then otherwise fall back to Bulkrax behavior.
+    # We have to overwrite this behavior to add 'find_by_source_identifier'
+    # to the queue of methods to try.
     #
     # @see https://github.com/samvera/bulkrax/blob/v9.1.0/app/factories/bulkrax/object_factory_interface.rb#L317-L325
     def find
-      find_by_source_identifier || find_by_id|| search_by_identifier || nil
+      find_by_source_identifier || find_by_id || search_by_identifier || nil
     end
 
+    # This method modifies the search to query Solr for the work's
+    # source_identifier and then returns the object if it's found.
     def find_by_source_identifier
-      # Hyrax.logger.warn("find_by_source_identifier")
       return unless attributes.key?('source_identifier') && attributes['source_identifier'].present?
-      # Hyrax.logger.warn("continue")
       identifier = Array.wrap(attributes['source_identifier']).first
-      # Hyrax.logger.warn(identifier)
       id_doc = Hyrax::SolrService.get("source_identifier_ssim:#{identifier}", fl: ['id', 'source_identifier_ssim'], defType: 'lucene')
-      # Hyrax.logger.warn(id_doc.to_s)
       id_doc = id_doc.try(:[], 'response').try(:[], 'docs')&.first
-      # Hyrax.logger.warn(id_doc.to_s)
       return nil if id_doc.nil? || id_doc.try(:[], 'id').nil?
 
       ActiveFedora::Base.find(id_doc['id'])
     end
-
-    # def find_by_id
-    #   Hyrax.logger.warn("find_by_id")
-    #   return false if attributes[:id].blank?
-    #   Hyrax.logger.warn("continue")
-    #   # Rails / Ruby upgrade, we moved from :exists? to :exist?  However we want to continue (for a
-    #   # bit) to support older versions.
-    #   method_name = klass.respond_to?(:exist?) ? :exist? : :exists?
-    #   Hyrax.logger.warn("method name - " + method_name.to_s)
-    #   klass.find(attributes[:id]) if klass.send(method_name, attributes[:id])
-    # rescue Valkyrie::Persistence::ObjectNotFoundError
-    #   Hyrax.logger.warn("valkyrie error")
-    #   false
-    # end
-
-    # def find_by_id_alt
-    #   Hyrax.logger.warn("find_by_id_alt")
-    #   return unless attributes.key?('id') && attributes['id'].present?
-    #   Hyrax.logger.warn("continue")
-    #   ActiveFedora::Base.find(attributes['id'])
-    # end
-
-    # def search_by_identifier
-    #   Hyrax.logger.warn("find_by_source_identifier")
-    #   return false if source_identifier_value.blank?
-    #   Hyrax.logger.warn("continue")
-    #   Hyrax.logger.warn("klass - " + klass.to_s)
-    #   Hyrax.logger.warn("work_identifier_search_field - " + work_identifier_search_field.to_s)
-    #   Hyrax.logger.warn("source_identifier_value - " + source_identifier_value.to_s)
-    #   Hyrax.logger.warn("work_identifier - " + work_identifier.to_s)
-    #   self.class.search_by_property(
-    #     klass: klass,
-    #     search_field: work_identifier_search_field,
-    #     value: source_identifier_value,
-    #     name_field: work_identifier
-    #   )
-    # end
   end
 end
