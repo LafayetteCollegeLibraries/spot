a664dbdc4bd09847ffdde9065724f8dc6f9b604d
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2025-09-26 13:29:35 -0400

    messy find test

---
+++ spec/factories/concerns/spot/bulkrax_object_factory_find_patch_spec.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+RSpec.describe BulkraxObjectFactoryFindPatch do
+  describe '#find' do
+    subject {find}
+    let(:mock_work) { instance_double(Hyrax::Work) }
+      
+    context "'find_by_source_identifier' returns the object"
+      before do 
+        allow(Bulkrax::ObjectFactory).to receive(:find_by_source_identifier).and_return(mock_work)
+      end
+
+      expect(Bulkrax::ObjectFactory).to receive(:find_by_source_identifier)
+      expect(Bulkrax::ObjectFactory).to_not receive(:find_by_id)
+      expect(Bulkrax::ObjectFactory).to_not receive(:search_by_identifier)
+
+      it {is_expected.to eq mock_work}
+    end
+
+    context "'find_by_id' returns the object"
+      before do 
+        allow(Bulkrax::ObjectFactory).to receive(:find_by_source_identifier).and_return(nil)
+        allow(Bulkrax::ObjectFactory).to receive(:find_by_id).and_return(mock_work)
+      end
+
+      expect(Bulkrax::ObjectFactory).to receive(:find_by_source_identifier)
+      expect(Bulkrax::ObjectFactory).to receive(:find_by_id)
+      expect(Bulkrax::ObjectFactory).to_not receive(:search_by_identifier)
+
+      it {is_expected.to eq mock_work}
+    end
+
+    context "'search_by_identifier' returns the object"
+      before do 
+        allow(Bulkrax::ObjectFactory).to receive(:find_by_source_identifier).and_return(nil)
+        allow(Bulkrax::ObjectFactory).to receive(:find_by_id).and_return(nil)
+        allow(Bulkrax::ObjectFactory).to receive(:search_by_identifier).and_return(mock_work)
+      end
+
+      expect(Bulkrax::ObjectFactory).to receive(:find_by_source_identifier)
+      expect(Bulkrax::ObjectFactory).to receive(:find_by_id)
+      expect(Bulkrax::ObjectFactory).to receive(:search_by_identifier)
+
+      it {is_expected.to eq mock_work}
+    end
+
+    context "all methods return nil"
+      before do 
+        allow(Bulkrax::ObjectFactory).to receive(:find_by_source_identifier).and_return(nil)
+        allow(Bulkrax::ObjectFactory).to receive(:find_by_id).and_return(nil)
+        allow(Bulkrax::ObjectFactory).to receive(:search_by_identifier).and_return(nil)
+      end
+
+      expect(Bulkrax::ObjectFactory).to receive(:find_by_source_identifier)
+      expect(Bulkrax::ObjectFactory).to receive(:find_by_id)
+      expect(Bulkrax::ObjectFactory).to receive(:search_by_identifier)
+
+      it {is_expected.to eq nil}
+    end
+  end
+end
