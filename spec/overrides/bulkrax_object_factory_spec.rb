9976ff9cc2cb63d0d5762fe0cc376a051b060496
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2025-09-29 13:41:41 -0400

    yet again

--- spec/overrides/bulkrax_object_factory_spec.rb
+++ spec/overrides/bulkrax_object_factory_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe Bulkrax::ObjectFactory do
 
     context "'find_by_source_identifier' returns the object" do
       before do
-        allow(Spot::BulkraxObjectFactoryFindPatch).to receive(:find_by_source_identifier).and_return(mock_work)
+        allow(Bulkrax::ObjectFactory).to receive(:find_by_source_identifier).and_return(mock_work)
       end
 
       it "runs the first method only and returns the object" do
-        expect(Spot::BulkraxObjectFactoryFindPatch).to receive(:find_by_source_identifier)
+        expect(Bulkrax::ObjectFactory).to receive(:find_by_source_identifier)
         expect(Bulkrax::ObjectFactory).to_not receive(:find_by_id)
         expect(Bulkrax::ObjectFactory).to_not receive(:search_by_identifier)
         is_expected.to eq mock_work
@@ -19,12 +19,12 @@ RSpec.describe Bulkrax::ObjectFactory do
 
     context "'find_by_id' returns the object" do
       before do
-        allow(Spot::BulkraxObjectFactoryFindPatch).to receive(:find_by_source_identifier).and_return(nil)
+        allow(Bulkrax::ObjectFactory).to receive(:find_by_source_identifier).and_return(nil)
         allow(Bulkrax::ObjectFactory).to receive(:find_by_id).and_return(mock_work)
       end
 
       it "runs the first two methods only and returns the object" do
-        expect(Spot::BulkraxObjectFactoryFindPatch).to receive(:find_by_source_identifier)
+        expect(Bulkrax::ObjectFactory).to receive(:find_by_source_identifier)
         expect(Bulkrax::ObjectFactory).to receive(:find_by_id)
         expect(Bulkrax::ObjectFactory).to_not receive(:search_by_identifier)
         is_expected.to eq mock_work
@@ -33,13 +33,13 @@ RSpec.describe Bulkrax::ObjectFactory do
 
     context "'search_by_identifier' returns the object" do
       before do
-        allow(Spot::BulkraxObjectFactoryFindPatch).to receive(:find_by_source_identifier).and_return(nil)
+        allow(Bulkrax::ObjectFactory).to receive(:find_by_source_identifier).and_return(nil)
         allow(Bulkrax::ObjectFactory).to receive(:find_by_id).and_return(nil)
         allow(Bulkrax::ObjectFactory).to receive(:search_by_identifier).and_return(mock_work)
       end
 
       it "runs all methods and returns the object" do
-        expect(Spot::BulkraxObjectFactoryFindPatch).to receive(:find_by_source_identifier)
+        expect(Bulkrax::ObjectFactory).to receive(:find_by_source_identifier)
         expect(Bulkrax::ObjectFactory).to receive(:find_by_id)
         expect(Bulkrax::ObjectFactory).to receive(:search_by_identifier)
         is_expected.to eq mock_work
@@ -48,13 +48,13 @@ RSpec.describe Bulkrax::ObjectFactory do
 
     context "all methods return nil" do
       before do
-        allow(Spot::BulkraxObjectFactoryFindPatch).to receive(:find_by_source_identifier).and_return(nil)
+        allow(Bulkrax::ObjectFactory).to receive(:find_by_source_identifier).and_return(nil)
         allow(Bulkrax::ObjectFactory).to receive(:find_by_id).and_return(nil)
         allow(Bulkrax::ObjectFactory).to receive(:search_by_identifier).and_return(nil)
       end
 
       it "runs all methods and returns nil" do
-        expect(Spot::BulkraxObjectFactoryFindPatch).to receive(:find_by_source_identifier)
+        expect(Bulkrax::ObjectFactory).to receive(:find_by_source_identifier)
         expect(Bulkrax::ObjectFactory).to receive(:find_by_id)
         expect(Bulkrax::ObjectFactory).to receive(:search_by_identifier)
         is_expected.to eq nil
