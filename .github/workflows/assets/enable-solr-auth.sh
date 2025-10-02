c033e633d0da2072af3f642f3a021391ee88a564
Anna Malantonio <malantoa@lafayette.edu>
2025-10-02 16:23:32 -0400

    redo auth script

--- .github/workflows/assets/enable-solr-auth.sh
+++ .github/workflows/assets/enable-solr-auth.sh
@@ -1,6 +1,15 @@
 #! /bin/bash
 #
-# boot up Solr + set up authentication for test user
-/opt/solr/bin/solr start
-/opt/solr/bin/solr auth enable -type basicAuth -credentials solr:SolrRocks -z $ZK_HOST
-/opt/solr/bin/solr stop
+# enable basic authentication by uploading a security config to zookeeper
+/opt/solr/server/scripts/cloud-scripts/zkcli.sh \
+  -zkhost $ZK_HOST \
+  -cmd put /security.json '{"authentication":{
+   "class":"solr.BasicAuthPlugin",
+   "credentials":{"solr":"IV0EHq1OnNrj6gvRCwvFwTrZ1+z1oBbnQdiVC3otuq0= Ndd7LKvVBAaZIF0QAVi1ekCfAJXr1GGfLtRUXhgrF8c="}
+},
+"authorization":{
+   "class":"solr.RuleBasedAuthorizationPlugin",
+   "permissions":[{"name":"all",
+      "role":"admin"}],
+   "user-role":{"solr":"admin"}
+}}'
