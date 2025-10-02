d289c51031c788998f374ba86e8a26af5eb59eee
Anna Malantonio <malantoa@lafayette.edu>
2025-10-02 16:44:50 -0400

    cmd again?

--- .github/workflows/assets/enable-solr-auth.sh
+++ .github/workflows/assets/enable-solr-auth.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 #
 # enable basic authentication by uploading a security config to zookeeper
-/opt/solr/server/scripts/cloud-scripts/zkcli.sh \
-  -zkhost $ZK_HOST \
-  -cmd put /security.json '{"authentication":{
+/opt/solr/server/scripts/cloud-scripts/zkcli.sh -zkhost $ZK_HOST -cmd put /security.json '{"authentication":{
    "class":"solr.BasicAuthPlugin",
    "credentials":{"solr":"IV0EHq1OnNrj6gvRCwvFwTrZ1+z1oBbnQdiVC3otuq0= Ndd7LKvVBAaZIF0QAVi1ekCfAJXr1GGfLtRUXhgrF8c="}
 },"authorization":{
