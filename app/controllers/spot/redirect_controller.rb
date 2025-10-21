da518d433ae41f789dbfc6bc9f3cd26f918e6e50
Anna Malantonio <malantoa@lafayette.edu>
2025-10-21 16:38:51 -0400

    controller specs pass

--- app/controllers/spot/redirect_controller.rb
+++ app/controllers/spot/redirect_controller.rb
@@ -28,9 +28,8 @@ module Spot
     private
 
     def document_params
-      # The only fields we'll need to generate a URL (or url_helper params) is are id and has_model_ssim.
-      result, _documents = repository.search(q: "{!terms f=identifier_ssim}url:#{http_uri}", fl: ['id', 'has_model_ssim'], defType: 'lucene')
-      document = result.response['docs']&.first
+      service = Hyrax::SolrQueryService.new(query: ["{!terms f=identifier_ssim}url:#{http_uri}"])
+      document = service.query_result(fl: ['id', 'has_model_ssim'], defType: 'lucene')['response']['docs'].first
 
       raise Blacklight::Exceptions::RecordNotFound if document.nil?
       redirect_params_for(solr_document: document)
