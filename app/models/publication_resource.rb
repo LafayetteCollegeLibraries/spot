4237fe03eebc6b337cc46591b5d38de90061ce86
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2025-10-22 11:15:50 -0400

    copy over some valkyrie work to use query service

--- app/models/publication_resource.rb
+++ app/models/publication_resource.rb
@@ -3,4 +3,6 @@ class PublicationResource < ::Hyrax::Work
   include Hyrax::Schema(:base_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
   include Hyrax::Schema(:institutional_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
   include Hyrax::Schema(:publication_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
+
+  include Spot::BaseResourceBehavior
 end
