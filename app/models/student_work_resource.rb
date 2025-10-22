ed70f59766497c14a41432a7a3e0d954877418bb
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2025-10-22 11:15:50 -0400

    copy over some valkyrie work to use query service

---
+++ app/models/student_work_resource.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+class StudentWorkResource < ::Hyrax::Work
+  include Hyrax::Schema(:base_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
+  include Hyrax::Schema(:institutional_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
+  include Hyrax::Schema(:student_work_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
+end
