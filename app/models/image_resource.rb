54dbf97f38b9a02414f31af1e25592ee9cd43fbe
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2025-10-27 15:10:33 -0400

    remove valkyrization

--- app/models/image_resource.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-class ImageResource < ::Hyrax::Work
-  include Hyrax::Schema(:base_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
-  include Hyrax::Schema(:image_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
-end
