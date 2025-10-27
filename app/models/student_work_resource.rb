03f1de0a2f7f15521032804f553b9aa9d7539062
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2025-10-27 15:10:33 -0400

    remove valkyrization

--- app/models/student_work_resource.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-class StudentWorkResource < ::Hyrax::Work
-  include Hyrax::Schema(:base_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
-  include Hyrax::Schema(:institutional_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
-  include Hyrax::Schema(:student_work_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
-end
