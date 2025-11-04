4c2cde5d3cc3221d60302bfdf5a5479c1a161003
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2025-11-04 14:58:00 -0500

    fix controlled vocab models

--- spec/models/spot/controlled_vocabularies/base_spec.rb
+++ spec/models/spot/controlled_vocabularies/base_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 RSpec.describe Spot::ControlledVocabularies::Base do
   let(:resource) { described_class.new(uri) }
-  let(:uri) { RDF::URI('http://id.loc.gov/authorities/subjects/sh85062079') }
+  let(:uri) { RDF::URI('http://id.loc.gov/authorities/subjects/sh85062079').to_s }
   let(:label_en) { RDF::Literal('Horror in art', language: :en) }
   let(:label_de) { RDF::Literal('Schrecken <Motiv>', language: :de) }
   let(:labels) { [label_en, label_de] }
@@ -34,8 +34,6 @@ RSpec.describe Spot::ControlledVocabularies::Base do
         # 3 tries, minus the first try
         expect(resource).to have_received(:sleep).exactly(2).times
       end
-
-      it_behaves_like 'it logs a warning'
     end
 
     context 'when a label has not been cached but others exist' do
