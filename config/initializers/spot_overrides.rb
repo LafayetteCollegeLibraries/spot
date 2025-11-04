27e9b7bf29c9aefbec25658bda2704078b6f24ee
Anna Malantonio <malantoa@lafayette.edu>
2025-11-04 16:10:42 -0500

    wip test work

--- config/initializers/spot_overrides.rb
+++ config/initializers/spot_overrides.rb
@@ -43,43 +43,67 @@ Rails.application.reloader.to_prepare do
   # "translation missing" message being provided as a fall-back label. this should
   # prevent that error from appearing by replacing the +translate+ calls with a symbolized
   # I18n key (see also 0717dee, + catalog_controller.rb)
-  [Hyrax::My::CollectionsController, Hyrax::Dashboard::CollectionsController].each do |klass|
-    klass.class_eval do
-      def self.update_facet_labels!
-        blacklight_config.facet_fields['visibility_ssi'].label = :'hyrax.dashboard.my.heading.visibility'
-        blacklight_config.facet_fields[Hyrax.config.collection_type_index_field].label = :'hyrax.dashboard.my.heading.collection_type'
-        blacklight_config.facet_fields['has_model_ssim'].label = :'hyrax.dashboard.my.heading.collection_type'
+  module Spot
+    module CollectionsControllerFacetDecorator
+      extend ActiveSupport::Concern
+
+      class_methods do
+        def update_facet_labels!
+          blacklight_config.facet_fields['visibility_ssi'].label = :'hyrax.dashboard.my.heading.visibility'
+          blacklight_config.facet_fields[Hyrax.config.collection_type_index_field].label = :'hyrax.dashboard.my.heading.collection_type'
+          blacklight_config.facet_fields['has_model_ssim'].label = :'hyrax.dashboard.my.heading.collection_type'
+        end
       end
-      update_facet_labels!
+
+      included do
+        update_facet_labels!
+      end
+    end
+  end
+
+  Hyrax::My::CollectionsController.prepend(Spot::CollectionsControllerFacetDecorator)
+  Hyrax::Dashboard::CollectionsController.prepend(Spot::CollectionsControllerFacetDecorator)
+
+  module Spot
+    module WorksControllerFacetDecorator
+      extend ActiveSupport::Concern
+
+      class_methods do
+        def update_facet_labels!
+          blacklight_config.facet_fields['visibility_ssi'].label = :'hyrax.dashboard.my.heading.visibility'
+        end
+      end
+
+      included do
+        update_facet_labels!
+      end
     end
   end
 
   # same as previous: updating facet labels for dashboard works controller
-  [Hyrax::My::WorksController, Hyrax::Dashboard::WorksController].each do |klass|
-    klass.class_eval do
-      def self.update_facet_labels!
-        blacklight_config.facet_fields['visibility_ssi'].label = :'hyrax.dashboard.my.heading.visibility'
-      end
-      update_facet_labels!
-    end
-  end
+  Hyrax::My::WorksController.prepend(Spot::WorksControllerFacetDecorator)
+  Hyrax::Dashboard::WorksController.prepend(Spot::WorksControllerFacetDecorator)
 
   # We're using an older version of the FITSServlet tool (1.1.3 as of 2019-12-03,
   # anything higher throws an exception that I can't nail down) that predates
   # a change to set the response encoding to UTF-8. So we need to do this as
   # early as possible within the Characterization tool.
-  require 'hydra-file_characterization'
+  module Spot
+    module FitsServletCharacterizerDecorator
+      extend ActiveSupport::Concern
 
-  Hydra::FileCharacterization::Characterizers::FitsServlet.class_eval do
-    # Wrap the datafile= param in quotes to handle filenames with spaces
-    def command
-      %(curl -k -F datafile=@"#{filename}" #{ENV['FITS_SERVLET_URL']}/examine)
-    end
+      # Wrap the datafile= param in quotes to handle filenames with spaces
+      def command
+        %(curl -k -F datafile=@"#{filename}" #{ENV['FITS_SERVLET_URL']}/examine)
+      end
 
-    def output
-      super.encode('UTF-8', invalid: :replace)
+      def output
+        super.encode('UTF-8', invalid: :replace)
+      end
     end
   end
+
+  Hydra::FileCharacterization::Characterizers::FitsServlet.prepend(Spot::FitsServletCharacterizerDecorator)
 
   # Add our SolrSuggestActor to the front of the default actor-stack. This will
   # trigger a build of all of the Solr suggestion dictionaries at the end of
@@ -99,25 +123,40 @@ Rails.application.reloader.to_prepare do
   #   # => false
   #
   # @see https://github.com/samvera/hydra-head/blob/v10.7.0/hydra-access-controls/app/models/hydra/access_controls/embargo.rb#L13-L15
-  Hydra::AccessControls::Embargo.class_eval do
-    def active?
-      embargo_release_date.present? && DateTime.current < embargo_release_date
+  module Spot
+    module EmbargoActiveDecorator
+      extend ActiveSupport::Concern
+
+      def active?
+        embargo_release_date.present? && DateTime.current < embargo_release_date
+      end
+    end
+
+    module LeaseActiveDecorator
+      extend ActiveSupport::Concern
+
+      def active?
+        lease_expiration_date.present? && DateTime.current < lease_expiration_date
+      end
     end
   end
 
-  Hydra::AccessControls::Lease.class_eval do
-    def active?
-      lease_expiration_date.present? && DateTime.current < lease_expiration_date
-    end
-  end
+  Hydra::AccessControls::Embargo.prepend(Spot::EmbargoActiveDecorator)
+  Hydra::AccessControls::Lease.prepend(Spot::LeaseActiveDecorator)
 
   # Updating how SimpleForm generates labels so that we can use the same locales
   # for the form as those for the metadata display.
-  SimpleForm::Inputs::Base.class_eval do
-    protected def raw_label_text
-      options[:label] || I18n.t("blacklight.search.fields.#{attribute_name}", default: label_translation)
+  module Spot
+    module SimpleFormBaseInputDecorator
+      extend ActiveSupport::Concern
+
+      protected def raw_label_text
+        options[:label] || I18n.t("blacklight.search.fields.#{attribute_name}", default: label_translation)
+      end
     end
   end
+
+  SimpleForm::Inputs::Base.prepend(Spot::SimpleFormBaseInputDecorator)
 
   # Adding label support for metadata-only records
   Hyrax::PermissionBadge.class_eval do
@@ -145,12 +184,18 @@ Rails.application.reloader.to_prepare do
   # wonky one-liner seems to preserve user-selected sorting. ¯\_(ツ)_/¯
   #
   # @see https://github.com/samvera/hyrax/blob/main/app/search_builders/hyrax/collection_search_builder.rb#L36-L42
-  Hyrax::CollectionMemberSearchBuilder.class_eval do
-    def add_sorting_to_solr(solr_parameters)
-      return if solr_parameters[:q]
-      solr_parameters[:sort] ||= (sort || "title_sort_si asc")
+  module Spot
+    module CollectionMemberSearchBuilderDecorator
+      extend ActiveSupport::Concern
+
+      def add_sorting_to_solr(solr_parameters)
+        return if solr_parameters[:q]
+        solr_parameters[:sort] ||= (sort || "title_sort_si asc")
+      end
     end
   end
+
+  Hyrax::CollectionMemberSearchBuilder.prepend(Spot::CollectionMemberSearchBuilderDecorator)
 
   # Override to fix Hyrax bug where calling Hyrax::AdminSetCreateService.find_or_create_default_admin_set
   # will try to load an AdminSet's entire set of members when called.
@@ -173,78 +218,113 @@ Rails.application.reloader.to_prepare do
   #
   # @see https://github.com/biola/rack-cas/blob/v0.16.1/lib/rack/cas.rb#L96-L102
   # rubocop:disable Style/IfUnlessModifier
-  require 'rack/cas'
-  Rack::CAS.class_eval do
-    def store_session(request, user, ticket, extra_attrs = {})
-      if RackCAS.config.extra_attributes_filter?
-        extra_attrs.select! { |key, _val| RackCAS.config.extra_attributes_filter.map(&:to_s).include?(key.to_s) }
-      end
+  module Spot
+    module RackCasEntitlementDecorator
+      extend ActiveSupport::Concern
 
-      if extra_attrs['eduPersonEntitlement'].present?
-        extra_attrs['eduPersonEntitlement'] = Array.wrap(extra_attrs['eduPersonEntitlement']).select do |val|
-          URI.parse(val).host == Spot::CasUserRolesService.entitlement_host
+      def store_session(request, user, ticket, extra_attrs = {})
+        if RackCAS.config.extra_attributes_filter?
+          extra_attrs.select! { |key, _val| RackCAS.config.extra_attributes_filter.map(&:to_s).include?(key.to_s) }
         end
-      end
 
-      request.session['cas'] = { 'user' => user, 'ticket' => ticket, 'extra_attributes' => extra_attrs }
+        if extra_attrs['eduPersonEntitlement'].present?
+          extra_attrs['eduPersonEntitlement'] = Array.wrap(extra_attrs['eduPersonEntitlement']).select do |val|
+            URI.parse(val).host == Spot::CasUserRolesService.entitlement_host
+          end
+        end
+
+        request.session['cas'] = { 'user' => user, 'ticket' => ticket, 'extra_attributes' => extra_attrs }
+      end
     end
   end
 
+  require 'rack/cas' unless Object.const_defined?('Rack::RAS')
+
+  Rack::CAS.prepend(Spot::RackCasEntitlementDecorator)
+
   # Modifying how Questiong Authority returns AssignFAST results by
   # converting fst ids into URLs
-  require 'qa/authorities/assign_fast'
-  Qa::Authorities::AssignFast::GenericAuthority.class_eval do
-    private
+  module Spot
+    module QaAssignFastGenericAuthorityDecorator
+      extend ActiveSupport::Concern
 
-    def parse_authority_response(raw_response)
-      raw_response['response']['docs'].map do |doc|
-        index = Qa::Authorities::AssignFast.index_for_authority(subauthority)
-        term = doc[index].first
-        term += " (USE #{doc['auth']})" if doc['type'] == 'alt'
-        fast_id = Array.wrap(doc['idroot']).first
+      private
 
-        {
-          fast_id: fast_id,
-          id: "http://id.worldcat.org/fast/#{fast_id.gsub(/^fst/, '')}",
-          label: term,
-          type: doc['type'],
-          value: doc['auth']
-        }
+      def parse_authority_response(raw_response)
+        raw_response['response']['docs'].map do |doc|
+          index = Qa::Authorities::AssignFast.index_for_authority(subauthority)
+          term = doc[index].first
+          term += " (USE #{doc['auth']})" if doc['type'] == 'alt'
+          fast_id = Array.wrap(doc['idroot']).first
+
+          {
+            fast_id: fast_id,
+            id: "http://id.worldcat.org/fast/#{fast_id.gsub(/^fst/, '')}",
+            label: term,
+            type: doc['type'],
+            value: doc['auth']
+          }
+        end
+      end
+    end
+  end
+
+  Qa::Authorities::AssignFast::GenericAuthority.prepend(Spot::QaAssignFastGenericAuthorityDecorator)
+
+  module Spot
+    module QaAssignFastSubauthorityDecorator
+      extend ActiveSupport::Concern
+
+      def index_for_authority(authority)
+        return authority if authority == 'idroot'
+
+        Qa::Authorities::AssignFastSubauthority::SUBAUTHORITIES[authority]
+      end
+
+      def subauthorities
+        Qa::Authorities::AssignFastSubauthority::SUBAUTHORITIES.keys + ['idroot']
       end
     end
   end
 
   # In order for us to search assignFAST by FAST IDs, we need to
   # add the 'idroot' searchIndex as a valid subauthority for AssignFast
-  Qa::Authorities::AssignFastSubauthority.module_eval do
-    def index_for_authority(authority)
-      return authority if authority == 'idroot'
-
-      Qa::Authorities::AssignFastSubauthority::SUBAUTHORITIES[authority]
-    end
-
-    def subauthorities
-      Qa::Authorities::AssignFastSubauthority::SUBAUTHORITIES.keys + ['idroot']
-    end
-  end
+  Qa::Authorities::AssignFastSubauthority.prepend(Spot::QaAssignFastSubauthorityDecorator)
 
   # Modifying the Video Runner for Hydra to use a customized Processor
   # which backports changes from 3.8.0
   #
   # @see https://github.com/samvera/hydra-derivatives/blob/v3.8.0/lib/hydra/derivatives/runners/video_derivatives.rb
-  Hydra::Derivatives::VideoDerivatives.class_eval do
-    def self.processor_class
-      Spot::VideoProcessor
+
+  module Spot
+    module VideoProcessorClassDecorator
+      extend ActiveSupport::Concern
+
+      class_methods do
+        def processor_class
+          Spot::VideoProcessor
+        end
+      end
     end
   end
+
+  Hydra::Derivatives::VideoDerivatives.prepend(Spot::VideoProcessorClassDecorator)
 
   # Add original file names and the transcript flag to presenter for file sets
   #
   # @see https://github.com/samvera/hyrax/blob/e4f8a06aaf1c9ec378f87764da59f73a8adf06d7/app/presenters/hyrax/file_set_presenter.rb
-  Hyrax::FileSetPresenter.class_eval do
-    delegate :original_filenames, :transcript_name,
-             :stored_derivatives, to: :solr_document
+  module Spot
+    module FileSetPresenterDecorator
+      extend ActiveSupport::Concern
+
+      included do
+        delegate :original_filenames, :transcript_name,
+                 :stored_derivatives, to: :solr_document
+      end
+    end
   end
+
+  Hyrax::FileSetPresenter.prepend(Spot::FileSetPresenterDecorator)
 
   # Add support for downloading file_set transcripts
   Hyrax::DownloadsController.prepend(Spot::DownloadsControllerBehavior)
@@ -253,30 +333,35 @@ Rails.application.reloader.to_prepare do
   #
   # @see https://github.com/samvera/bulkrax/blob/v5.5.1/app/parsers/bulkrax/csv_parser.rb#L258
   #
-  Bulkrax::ImporterJob.class_eval do
-    # checks the file sizes of the download files to match the original files
-    def all_files_completed?(importer)
-      cloud_files = importer.parser_fields['cloud_file_paths']
-      original_files = importer.parser_fields['original_file_paths']
-      return true unless cloud_files.present? && original_files.present?
+  module Spot
+    module BulkraxImporterJobDecorator
+      extend ActiveSupport::Concern
+      # checks the file sizes of the download files to match the original files
+      def all_files_completed?(importer)
+        cloud_files = importer.parser_fields['cloud_file_paths']
+        original_files = importer.parser_fields['original_file_paths']
+        return true unless cloud_files.present? && original_files.present?
 
-      imported_file_sizes = cloud_files.map { |_, v| get_file_size_from_s3(v['url']) }
-      original_file_sizes = original_files.map { |imported_file| File.size(imported_file) }
+        imported_file_sizes = cloud_files.map { |_, v| get_file_size_from_s3(v['url']) }
+        original_file_sizes = original_files.map { |imported_file| File.size(imported_file) }
 
-      original_file_sizes == imported_file_sizes
-    end
+        original_file_sizes == imported_file_sizes
+      end
 
-    # s3 file size fetch
-    # @todo should we add handling for other types of cloud files?
-    def get_file_size_from_s3(url)
-      uri_parsed = ::Addressable::URI.parse(url)
-      return unless uri_parsed.scheme == 's3'
+      # s3 file size fetch
+      # @todo should we add handling for other types of cloud files?
+      def get_file_size_from_s3(url)
+        uri_parsed = ::Addressable::URI.parse(url)
+        return unless uri_parsed.scheme == 's3'
 
-      client = Aws::S3::Client.new
-      resp = client.head_object(bucket: uri_parsed.host, key: uri_parsed.path[1..-1])
-      resp.content_length
+        client = Aws::S3::Client.new
+        resp = client.head_object(bucket: uri_parsed.host, key: uri_parsed.path[1..-1])
+        resp.content_length
+      end
     end
   end
+
+  Bulkrax::ImporterJob.prepend(Spot::BulkraxImporterJobDecorator)
 
   # Bulkrax's parser replaces any whitespace character with a space, which kills paragraph breaks
   # and newlines. Our patch modifies the base #result method to only replace tabs with
@@ -290,35 +375,44 @@ Rails.application.reloader.to_prepare do
   # The unauthorized status breaks the laf only thumbnail.
   #
   # @see https://github.com/samvera/hyrax/blob/hyrax-v3.6.0/app/controllers/hyrax/downloads_controller.rb#L52-L63
-  Hyrax::DownloadsController.class_eval do
-    # Customize the :read ability in your Ability class, or override this method.
-    # Hydra::Ability#download_permissions can't be used in this case because it assumes
-    # that files are in a LDP basic container, and thus, included in the asset's uri.
-    def authorize_download!
-      authorize! :download, params[asset_param_key]
-      # Deny access if the work containing this file is restricted by a workflow
-      return unless workflow_restriction?(file_set_parent(params[asset_param_key]), ability: current_ability)
-      raise Hyrax::WorkflowAuthorizationException
-    rescue CanCan::AccessDenied, Hyrax::WorkflowAuthorizationException
-      unauthorized_image = Rails.root.join("app", "assets", "images", "unauthorized.png")
-      send_file unauthorized_image
+  module Spot
+    module HyraxDownloadsControllerDecorator
+      extend ActiveSupport::Concern
+      # Customize the :read ability in your Ability class, or override this method.
+      # Hydra::Ability#download_permissions can't be used in this case because it assumes
+      # that files are in a LDP basic container, and thus, included in the asset's uri.
+      def authorize_download!
+        authorize! :download, params[asset_param_key]
+        # Deny access if the work containing this file is restricted by a workflow
+        return unless workflow_restriction?(file_set_parent(params[asset_param_key]), ability: current_ability)
+        raise Hyrax::WorkflowAuthorizationException
+      rescue CanCan::AccessDenied, Hyrax::WorkflowAuthorizationException
+        unauthorized_image = Rails.root.join("app", "assets", "images", "unauthorized.png")
+        send_file unauthorized_image
+      end
     end
   end
+
+  Hyrax::DownloadsController.prepend(Spot::HyraxDownloadsControllerDecorator)
 
   # Encountering an issue where Hyrax::PersistDirectlyContainedOutputFileService.retrieve_file_set requires
   # Hyrax::UploadedFile#file_set_uri to be an URI but querying for that URI throws an error (ActiveFedora
   # is appending the base root to the full uri, resulting in errors like:
   #     Ldp::BadRequest: Path contains empty element! /dev/ht/tp/:/http://fedora:8080/rest/dev/2v/23/vt/36/2v23vt362")
-  Hyrax::UploadedFile.class_eval do
-    def add_file_set!(file_set)
-      uri = case file_set
-            when ActiveFedora::Base
-              file_set.uri
-            when Hyrax::Resource
-              file_set.id.is_a?(URI::HTTP) ? file_set.id : Hyrax::Base.id_to_uri(file_set.id.to_s)
-            end
+  module Spot
+    module HyraxUploadedFileDecorator
+      def add_file_set!(file_set)
+        uri = case file_set
+              when ActiveFedora::Base
+                file_set.uri
+              when Hyrax::Resource
+                file_set.id.is_a?(URI::HTTP) ? file_set.id : Hyrax::Base.id_to_uri(file_set.id.to_s)
+              end
 
-      update!(file_set_uri: uri) if uri.present?
+        update!(file_set_uri: uri) if uri.present?
+      end
     end
   end
+
+  Hyrax::UploadedFile.prepend(Spot::HyraxUploadedFileDecorator)
 end
