9b460e5ed02d32779ebbe03de5bae54d2e9bce65
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2025-11-05 11:09:07 -0500

    rubo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 Rails.application.configure do
   config.hosts << "ldr-future.lafayette.edu"
-  
+
   config.public_file_server.enabled = true
 
   # Settings specified here will take precedence over those in config/application.rb.
