6c7f621d0d2a246ff3770ba2aa7cdd0ace5347bb
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2025-11-17 10:14:30 -0500

    specs

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe ApplicationController do
   describe '#default_url_options' do
     subject(:options) { described_class.new.default_url_options }
 
-    it { is_expected.not_to include :locale }
+    it { is_expected.to include :locale }
   end
 
   describe '#render_bookmarks_control?' do
