bdd79ca7b417636b5099c6c7a8fb6c21b833cca4
Anna Malantonio <malantoa@lafayette.edu>
2025-11-17 12:47:49 -0500

    views catchup

--- app/presenters/spot/permission_badge.rb
+++ app/presenters/spot/permission_badge.rb
@@ -9,13 +9,5 @@ module Spot
       open: "badge-success",
       restricted: "badge-danger"
     }.freeze
-
-    def badge_class
-      dom_label_class
-    end
-
-    def visibility_label
-      text
-    end
   end
 end
--- spec/presenters/spot/permission_badge_spec.rb
+++ spec/presenters/spot/permission_badge_spec.rb
@@ -3,12 +3,4 @@ RSpec.describe Spot::PermissionBadge do
   it 'renders a span badge' do
     expect(described_class.new('metadata').render.to_s).to eq '<span class="badge badge-info">Metadata Only</span>'
   end
-
-  it 'returns the correct badge_class' do
-    expect(described_class.new('metadata').badge_class.to_s).to eq 'badge-info'
-  end
-
-  it 'returns the correct visibility_label' do
-    expect(described_class.new('metadata').visibility_label.to_s).to eq 'Metadata Only'
-  end
 end
