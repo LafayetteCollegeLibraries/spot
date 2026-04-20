ce383459f145c8cb3f6da3affe10d46e9b02d28a
Anna Malantonio <malantoa@lafayette.edu>
2026-04-20 09:35:07 -0400

    add spec for permission badge view-component

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,11 +43,6 @@ require 'view_component/test_helpers'
 require 'hyrax/specs/shared_specs/factories/strategies/valkyrie_resource'
 FactoryBot.register_strategy(:valkyrie_create, ValkyrieCreateStrategy)
 
-# FactoryBot setup borrowed from Hyrax
-# @see https://github.com/samvera/hyrax/blob/hyrax-v3.6.0/spec/spec_helper.rb#L83-L88
-require 'hyrax/specs/shared_specs/factories/strategies/valkyrie_resource'
-FactoryBot.register_strategy(:valkyrie_create, ValkyrieCreateStrategy)
-
 Capybara.register_driver :selenium_firefox_headless do |app|
   browser_options = ::Selenium::WebDriver::Firefox::Options.new
   browser_options.binary = ENV['FIREFOX_BINARY_PATH'] if ENV['FIREFOX_BINARY_PATH'].present?
