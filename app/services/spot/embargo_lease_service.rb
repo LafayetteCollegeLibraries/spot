35e5edd00843b2142ef6171da9669dcfb6d428d1
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2026-04-28 11:35:46 -0400

    no cov

--- app/services/spot/embargo_lease_service.rb
+++ app/services/spot/embargo_lease_service.rb
@@ -88,59 +88,6 @@ module Spot
         end
       end
 
-      def copy_visibility_to_members!(resource:)
-        Hyrax.query_service.find_members(resource: resource).each do |member|
-          Hyrax::AccessControlList.copy_permissions(source: resource, target: member)
-        end
-      end
-
-      private
-
-      # Embargos and Leases behave very similarly and are managed through similar interfaces
-      # in Hyrax, so we'll do the bulk of that work here.
-      #
-      # @param [String] id
-      # @option [:embargo, :lease] :type
-      # @option [true/false] :regenerate_thumbnails
-      # @return [void]
-      def release_and_save_for_id(id, type:, regenerate_thumbnails: false)
-        resource = release_and_save_acl_for(type: type, resource: Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: id))
-        return if resource.nil?
-
-        copy_visibility_to_members!(resource: resource)
-
-        RegenerateThumbnailJob.perform_later(resource) if regenerate_thumbnails == true
-      end
-
-      # calling #release! on the managers will raise a +NotReleaseableError+ if
-      # the embargo/lease isn't ready to be deactivated. Since the resource's
-      # visibility hasn't changed, we won't bother to resave the object or enqueue
-      # a thumbnail regeneration job.
-      #
-      # @todo how can we test the `nil` return value for coverage?
-      def release_and_save_acl_for(type:, resource:)
-        manager_class = manager_class_for(type)
-        return if manager_class.nil?
-
-        manager_class.new(resource: resource).release!
-        resource.permission_manager.acl.save
-        resource
-      rescue Hyrax::EmbargoManager::NotReleasableError, Hyrax::LeaseManager::NotReleasableError
-        # :nocov:
-        # This is covered via other tests
-        nil
-        # :nocov:
-      end
-
-      def manager_class_for(type)
-        case type
-        when :embargo
-          Hyrax::EmbargoManager
-        when :lease
-          Hyrax::LeaseManager
-        end
-      end
-
       # :nocov:
       # return to this post valkyrization
       def copy_visibility_to_members!(resource:)
