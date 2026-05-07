b01c7ef679a74682858dda30e0161d4dae521ce5
Anna Malantonio <malantoa@lafayette.edu>
2026-05-07 15:08:19 -0400

    initial model pass (needs controlled vocab/identifier support)

--- config/initializers/wings.rb
+++ config/initializers/wings.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 #
-# Copied from Dassie example in Hyrax—register our models with Wings so they convert correctly
+# Set up cribbed from the Dassie example app within Hyrax, which itself is adapted from Hyku.
 #
-# @todo Revisit this when Valkyrizing. This might need to be moved to config/initializers/valkyrie.rb
+# @note I have VALKYRIE_TRANSITION=true defined in .env.local but I'm unsure if it's neccessary?
 # @see https://github.com/samvera/hyrax/blob/hyrax-v5.2.0/.dassie/config/initializers/wings.rb
 Rails.application.config.after_initialize do
+  # active_fedora models we're migrating
+  [Publication, Image, StudentWork, AudioVisual].each do |work_type|
+    Wings::ModelRegistry.register("#{work_type}Resource".constantize, work_type)
+
+    # from dassie:
+    #   "we register itself so we can pre-translate the class in Freyja instead of having to translate in each query_service"
+    Wings::ModelRegistry.register(work_type, work_type)
+  end
+
   Wings::ModelRegistry.register(Collection, Collection)
   Wings::ModelRegistry.register(AdminSet, AdminSet)
   Wings::ModelRegistry.register(FileSet, FileSet)
@@ -12,8 +21,80 @@ Rails.application.config.after_initialize do
   Wings::ModelRegistry.register(Hydra::PCDM::File, Hydra::PCDM::File)
   Wings::ModelRegistry.register(Hyrax::FileMetadata, Hydra::PCDM::File)
 
-  Wings::ModelRegistry.register(Publication, PublicationResource)
-  Wings::ModelRegistry.register(Image, ImageResource)
-  Wings::ModelRegistry.register(StudentWork, StudentWorkResource)
-  Wings::ModelRegistry.register(AudioVisual, AudioVisualResource)
+  Valkyrie::MetadataAdapter.register(Freyja::MetadataAdapter.new, :freyja)
+
+  Valkyrie::StorageAdapter.register(
+    Valkyrie::Storage::VersionedDisk.new(
+      base_path: Rails.root.join('storage', 'files'),
+      file_mover: FileUtils.method(:cp)
+    ),
+    :disk
+  )
+
+  Hyrax.config.query_index_from_valkyrie = true
+  Hyrax.config.index_adapter = :solr_index
+
+  Valkyrie.config.metadata_adapter = :freyja
+  Valkyrie.config.storage_adapter  = :disk
+  Valkyrie.config.indexing_adapter = :solr_index
+
+  # load all the sql based custom queries
+  [
+    Hyrax::CustomQueries::Navigators::CollectionMembers,
+    Hyrax::CustomQueries::Navigators::ChildCollectionsNavigator,
+    Hyrax::CustomQueries::Navigators::ParentCollectionsNavigator,
+    Hyrax::CustomQueries::Navigators::ChildFileSetsNavigator,
+    Hyrax::CustomQueries::Navigators::ChildWorksNavigator,
+    Hyrax::CustomQueries::Navigators::FindFiles,
+    Hyrax::CustomQueries::FindAccessControl,
+    Hyrax::CustomQueries::FindCollectionsByType,
+    Hyrax::CustomQueries::FindFileMetadata,
+    Hyrax::CustomQueries::FindIdsByModel,
+    Hyrax::CustomQueries::FindManyByAlternateIds,
+    Hyrax::CustomQueries::FindModelsByAccess,
+    Hyrax::CustomQueries::FindCountBy,
+    Hyrax::CustomQueries::FindByDateRange,
+    # Hyrax::CustomQueries::FindBySourceIdentifier  # from bulkrax
+  ].each do |handler|
+    Hyrax.query_service.services[0].custom_queries.register_query_handler(handler)
+  end
+end
+
+Rails.application.config.to_prepare do
+  # AdminSetResource.class_eval do
+  #   attribute :internal_resource, Valkyrie::Types::Any.default("AdminSet"), internal: true
+  # end
+
+  # CollectionResource.class_eval do
+  #   attribute :internal_resource, Valkyrie::Types::Any.default("Collection"), internal: true
+  # end
+
+  Valkyrie.config.resource_class_resolver = lambda do |resource_klass_name|
+    # TODO: Can we use some kind of lookup.
+    klass_name = resource_klass_name.gsub(/^Wings\((.+)\)$/, '\1')
+    klass_name = klass_name.gsub(/Resource$/, '')
+    if %w[
+      GenericWork
+    ].include?(klass_name)
+      "#{klass_name}Resource".constantize
+    elsif 'Collection' == klass_name
+      CollectionResource
+    elsif 'AdminSet' == klass_name
+      AdminSetResource
+      # Without this mapping, we'll see cases of Postgres Valkyrie adapter attempting to write to
+      # Fedora.  Yeah!
+    elsif 'Hydra::AccessControl' == klass_name
+      Hyrax::AccessControl
+    elsif 'FileSet' == klass_name
+      Hyrax::FileSet
+    elsif 'Hydra::AccessControls::Embargo' == klass_name
+      Hyrax::Embargo
+    elsif 'Hydra::AccessControls::Lease' == klass_name
+      Hyrax::Lease
+    elsif 'Hydra::PCDM::File' == klass_name
+      Hyrax::FileMetadata
+    else
+      klass_name.constantize
+    end
+  end
 end
