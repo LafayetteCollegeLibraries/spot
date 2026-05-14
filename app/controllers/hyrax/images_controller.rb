c482a57b25f5e7055aad2d589cee81d53f0d232b
Anna Malantonio <malantoa@lafayette.edu>
2026-05-14 17:03:16 -0400

    can navigate to work forms

--- app/controllers/hyrax/images_controller.rb
+++ app/controllers/hyrax/images_controller.rb
@@ -3,7 +3,10 @@ module Hyrax
   class ImagesController < ApplicationController
     include ::Spot::WorksControllerBehavior
 
-    self.curation_concern_type = ::Image
+    # self.curation_concern_type = ::Image
+    self.curation_concern_type = ::PublicationResource
+    self.work_form_service = Hyrax::FormFactory.new
+
     self.show_presenter = Hyrax::ImagePresenter
   end
 end
--- app/models/student_work_resource.rb
+++ app/models/student_work_resource.rb
@@ -3,4 +3,6 @@ class StudentWorkResource < ::Hyrax::Work
   include Hyrax::Schema(:base_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
   include Hyrax::Schema(:institutional_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
   include Hyrax::Schema(:student_work_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
+
+  Hyrax::ValkyrieLazyMigration.migrating(self, from: ::StudentWork)
 end
