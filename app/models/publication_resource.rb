742d28638ddfef70f34a3ab171e59618685966e2
Anna Malantonio <malantoa@lafayette.edu>
2026-05-18 10:42:00 -0400

    wip first pass @ forms

--- app/models/publication_resource.rb
+++ app/models/publication_resource.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
-class PublicationResource < ::Hyrax::Work
-  include Hyrax::Schema(:base_metadata) #, schema_loader: Spot::SimpleSchemaLoader.new)
-  include Hyrax::Schema(:institutional_metadata) #, schema_loader: Spot::SimpleSchemaLoader.new)
-  include Hyrax::Schema(:publication_metadata) #, schema_loader: Spot::SimpleSchemaLoader.new)
+class PublicationResource < BaseResource
+  include Hyrax::Schema(:institutional_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
+  include Hyrax::Schema(:publication_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
 
   Hyrax::ValkyrieLazyMigration.migrating(self, from: ::Publication)
 end
