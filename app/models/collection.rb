844ccedb918e43941a5e76e31c67362a7ed2d2f3
Anna Malantonio <malantoa@lafayette.edu>
2026-05-18 10:42:00 -0400

    wip first pass @ forms

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -38,7 +38,7 @@ class Collection < ActiveFedora::Base
   end
 
   property :location, predicate: ::RDF::Vocab::DC.spatial,
-                      class_name: Spot::ControlledVocabularies::Location do |index|
+                      class_name: Spot::ControlledVocabularies::GeonamesLocation do |index|
     index.as :symbol
   end
 
