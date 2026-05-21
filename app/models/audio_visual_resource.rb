bc0f10dfa3399a097104266c954595c5e57d96cb
Anna Malantonio <malantoa@lafayette.edu>
2026-05-21 16:08:06 -0400

    wip valkyrie postgres writes work?

--- app/models/audio_visual_resource.rb
+++ app/models/audio_visual_resource.rb
@@ -3,6 +3,4 @@ class AudioVisualResource < BaseResource
   include Hyrax::Schema(:audio_visual_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
 
   attribute :stored_derivatives, Valkyrie::Types::String
-
-  Hyrax::ValkyrieLazyMigration.migrating(self, from: ::AudioVisual)
 end
--- app/models/publication_resource.rb
+++ app/models/publication_resource.rb
@@ -3,5 +3,15 @@ class PublicationResource < BaseResource
   include Hyrax::Schema(:institutional_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
   include Hyrax::Schema(:publication_metadata, schema_loader: Spot::SimpleSchemaLoader.new)
 
-  Hyrax::ValkyrieLazyMigration.migrating(self, from: ::Publication)
+  def identifier
+    attributes[:identifier].map { |v| v.is_a?(String) ? Spot::Identifier.from_string(v) : v }
+  end
+
+  def local_identifier
+    identifier.select(&:local?)
+  end
+
+  def standard_identifier
+    identifier.select(&:standard?)
+  end
 end
