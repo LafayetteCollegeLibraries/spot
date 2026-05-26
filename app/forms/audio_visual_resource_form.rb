06616420ad90e1b2c11cc6dca04564bee46e3e98
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2026-05-26 15:31:41 -0400

    preliminary forms

--- app/forms/audio_visual_resource_form.rb
+++ app/forms/audio_visual_resource_form.rb
@@ -4,4 +4,40 @@ class AudioVisualResourceForm < Hyrax::Forms::ResourceForm(AudioVisualResource)
   include Hyrax::FormFields(:core_metadata)
   include Hyrax::FormFields(:base_metadata)
   include Hyrax::FormFields(:audio_visual_metadata)
+
+  def primary_terms
+    [
+      # required_fields first
+      :title,
+      :date,
+      :resource_type,
+      :rights_statement,
+
+      # non-required fields
+      :rights_holder,
+      :subtitle,
+      :title_alternative,
+      :date_associated,
+      :creator,
+      :contributor,
+      :publisher,
+      :source,
+      :standard_identifier,
+      :local_identifier,
+      :description,
+      :inscription,
+      :subject,
+      :keyword,
+      :language,
+      :physical_medium,
+      :original_item_extent,
+      :location,
+      :repository_location,
+      :note,
+      :related_resource,
+      :research_assistance,
+      :provenance,
+      :barcode
+    ]
+  end
 end
