9efcf5f4168a9dbc7a32d408a98fdb46856142cd
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2026-05-26 15:31:41 -0400

    preliminary forms

--- app/forms/image_resource_form.rb
+++ app/forms/image_resource_form.rb
@@ -3,4 +3,40 @@ class ImageResourceForm < Hyrax::Forms::ResourceForm(ImageResource)
   include Hyrax::FormFields(:core_metadata)
   include Hyrax::FormFields(:base_metadata)
   include Hyrax::FormFields(:image_metadata)
+
+  def primary_terms
+    [
+      :title,
+      :date,
+      :resource_type,
+      :rights_statement,
+
+      # non-required fields
+      :title_alternative,
+      :subtitle,
+      :date_associated,
+      :date_scope_note,
+      :rights_holder,
+      :description,
+      :inscription,
+      :creator,
+      :contributor,
+      :publisher,
+      :keyword,
+      :subject,
+      :location,
+      :language,
+      :source,
+      :physical_medium,
+      :original_item_extent,
+      :repository_location,
+      :requested_by,
+      :research_assistance,
+      :donor,
+      :related_resource,
+      :local_identifier,
+      :subject_ocm,
+      :note
+    ]
+  end
 end
--- app/forms/student_work_resource_form.rb
+++ app/forms/student_work_resource_form.rb
@@ -4,4 +4,35 @@ class StudentWorkResourceForm < Hyrax::Forms::ResourceForm(StudentWorkResource)
   include Hyrax::FormFields(:base_metadata)
   include Hyrax::FormFields(:Student_work_metadata)
   include Hyrax::FormFields(:institutional_metadata)
+
+  def primary_terms
+    [
+      :title,
+      :creator,
+      :advisor,
+      :academic_department,
+      :description,
+      :date,
+      :date_available,
+      :resource_type,
+      :rights_statement,
+      :rights_holder
+    ]
+  end
+
+  def secondary_terms
+    [
+      :division,
+      :abstract,
+      :language,
+      :related_resource,
+      :organization,
+      :subject,
+      :keyword,
+      :bibliographic_citation,
+      :standard_identifier,
+      :access_note,
+      :note
+    ]
+  end
 end
