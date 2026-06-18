99bd677a9a4e81c7e5399e5b540b9eac2cf4fe3b
Anna Malantonio <malantoa@lafayette.edu>
2026-06-18 18:01:18 -0400

    we'll want to valkyrie off to add af objects for dev

--- app/controllers/hyrax/audio_visuals_controller.rb
+++ app/controllers/hyrax/audio_visuals_controller.rb
@@ -3,8 +3,7 @@ module Hyrax
   class AudioVisualsController < ApplicationController
     include ::Spot::WorksControllerBehavior
 
-    # self.curation_concern_type = ::AudioVisual
-    self.curation_concern_type = ::AudioVisualResource
+    self.curation_concern_type = Hyrax.config.use_valkyrie? ? AudioVisualResource : AudioVisual
     self.work_form_service = Hyrax::FormFactory.new
 
     self.show_presenter = Hyrax::AudioVisualPresenter
