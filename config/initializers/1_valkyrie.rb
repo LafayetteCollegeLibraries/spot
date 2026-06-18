f8629f793c0aefd8afa5fdefe380a79dc2b798ea
Anna Malantonio <malantoa@lafayette.edu>
2026-06-18 17:59:17 -0400

    move valkyrie config to its own initializer

---
+++ config/initializers/1_valkyrie.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+#
+# Configuration for Valkyrie
+Rails.application.config.after_initialize do
+  # We're using the "Freyja" metadata adapter, included with Hyrax, as a way to migrate off of our
+  # Fedora 4 instance and onto PostgreSQL (assets stored in S3): Freyja writes to Postgres and tries
+  # reading from Fedora before falling over to Postgres. This requires Hyrax's "Wings" adapter
+  # (specifically the ModelRegistry) to translate ActiveFedora models to Valkyrie ones.
+  # That configuration has its own file.
+  #
+  # @see config/initializers/wings.rb
+  Valkyrie::MetadataAdapter.register(Freyja::MetadataAdapter.new, :freyja)
+  Valkyrie.config.metadata_adapter = :freyja
+
+  # We're still writing to Solr for search and browsing. The indexing adapter
+  # is registered in a Hyrax initializer.
+  #
+  # @see https://github.com/samvera/hyrax/blob/hyrax-v5.2.0/config/initializers/indexing_adapter_initializer.rb
+  Valkyrie.config.indexing_adapter = :solr_index
+
+  # Set up Valkyrie storage adapters for the LDR's object store as well as
+  # IIIF and A/V derivatives.
+  #
+  # @see app/services/spot/derivatives/image_derivative_service.rb (:s3_iiif)
+  # @see https://github.com/samvera-labs/valkyrie-shrine/
+  aws_opts = { force_path_style: !Rails.env.production? }
+
+  Shrine.storages = {
+    s3_iiif: Valkyrie::Shrine::Storage::S3.new(bucket: ENV.fetch('AWS_IIIF_ASSET_BUCKET') { 'iiif-derivatives' }, **aws_opts),
+    s3_av: Valkyrie::Shrine::Storage::S3.new(bucket: ENV.fetch('AWS_AV_ASSET_BUCKET') { 'av-derivatives' }, **aws_opts),
+    s3_object_store: Valkyrie::Shrine::Storage::S3.new(bucket: ENV.fetch('AWS_OBJECT_STORE_BUCKET') { 'ldr-object-store' }, **aws_opts)
+  }
+
+  # @note We need to use a custom PathGenerator for the valkyrie-shrine adapter, as the default one
+  #       appends a uuid to the path to prevent overwrites, but as these are access derivatives, we're not
+  #       particularly concerned about that.
+  #
+  # @see app/services/spot/s3_path.rb
+  Valkyrie::StorageAdapter.register(
+    Valkyrie::Storage::Shrine.new(Shrine.storages[:s3_iiif], nil, Spot::S3Path::IiifPathGenerator),
+    :iiif_source_s3
+  )
+
+  Valkyrie::StorageAdapter.register(
+    Valkyrie::Storage::Shrine.new(Shrine.storages[:s3_av], nil, Spot::S3Path::AvPathGenerator),
+    :av_source_s3
+  )
+
+  if ENV['AWS_OBJECT_STORE_BUCKET'].present?
+    Valkyrie::StorageAdapter.register(
+      Valkyrie::Storage::VersionedShrine.new(Shrine.storages[:s3_object_store]),
+      :versioned_object_store_s3
+    )
+    Valkyrie.config.storage_adapter = :versioned_object_store_s3
+    Rails.logger.info("Storing object assets in s3://#{ENV['AWS_OBJECT_STORE_BUCKET']}")
+  else
+    base_path = Rails.root.join('storage', 'files')
+    Valkyrie::StorageAdapter.register(
+      Valkyrie::Storage::VersionedDisk.new(
+        base_path: base_path,
+        file_mover: FileUtils.method(:cp)
+      ),
+      :disk
+    )
+    Valkyrie.config.storage_adapter = :disk
+    Rails.logger.info("Storing object assets on disk at #{base_path}")
+  end
+end
