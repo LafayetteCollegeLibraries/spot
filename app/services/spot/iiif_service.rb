d0220c05b93b533531d778a5ade1b07eead6a8bf
Anna Malantonio <malantoa@lafayette.edu>
2026-06-18 18:00:21 -0400

    ensure object store bucket is created

--- app/services/spot/iiif_service.rb
+++ app/services/spot/iiif_service.rb
@@ -103,10 +103,14 @@ module Spot
 
     private
 
-    # file_ids look like "<file_set.id>/files/<file_id>(/<md5 digest of version_id>)"
+    # file_ids look like "<file_set.id>/files/<file_id>(/<md5 digest of version_id>)".
+    # When we were on ActiveFedora, we used the file_set.id portion to determine the
+    # filename in S3. When using Valkyrie's storage adapters, the practice is to use
+    # the FileMetadata id (nee: file_id) to name the object.
     #
     # @return [String]
     # @see https://github.com/samvera/hyrax/blob/hyrax-v5.2.0/app/models/hyrax/file_set.rb#L76-L83
+    # @see app/services/spot/s3_path.rb
     def asset_id
       @asset_id ||=
         if Hyrax.config.use_valkyrie?
