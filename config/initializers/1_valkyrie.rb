c7749cad7514258b8fb27977feaa40cf0c043eec
Anna Malantonio <malantoa@lafayette.edu>
2026-06-22 14:09:22 -0400

    ensure the shrine storages have different prefixes

--- config/initializers/1_valkyrie.rb
+++ config/initializers/1_valkyrie.rb
@@ -23,32 +23,40 @@ Rails.application.config.after_initialize do
   #
   # @see app/services/spot/derivatives/image_derivative_service.rb (:s3_iiif)
   # @see https://github.com/samvera-labs/valkyrie-shrine/
+  # @note minio in development requires use to use path style s3 urls rather than hostnamed
   aws_opts = { force_path_style: !Rails.env.production? }
 
   Shrine.storages = {
+    s3_object_store: Valkyrie::Shrine::Storage::S3.new(bucket: ENV.fetch('AWS_OBJECT_STORE_BUCKET') { 'ldr-object-store' }, **aws_opts),
     s3_iiif: Valkyrie::Shrine::Storage::S3.new(bucket: ENV.fetch('AWS_IIIF_ASSET_BUCKET') { 'iiif-derivatives' }, **aws_opts),
-    s3_av: Valkyrie::Shrine::Storage::S3.new(bucket: ENV.fetch('AWS_AV_ASSET_BUCKET') { 'av-derivatives' }, **aws_opts),
-    s3_object_store: Valkyrie::Shrine::Storage::S3.new(bucket: ENV.fetch('AWS_OBJECT_STORE_BUCKET') { 'ldr-object-store' }, **aws_opts)
+    s3_av: Valkyrie::Shrine::Storage::S3.new(bucket: ENV.fetch('AWS_AV_ASSET_BUCKET') { 'av-derivatives' }, **aws_opts)
   }
 
+  # As we're using multiple buckets for different purposes (IIIF derivatives vs AV derivatives vs Object Store)
+  # we'll want to utilize the `identifier_prefix` to store the intended bucket as part of the file's remote uri.
+  #
+  # @example prefixed remote uri
+  #
+  #
+  #
   # @note We need to use a custom PathGenerator for the valkyrie-shrine adapter, as the default one
   #       appends a uuid to the path to prevent overwrites, but as these are access derivatives, we're not
   #       particularly concerned about that.
   #
   # @see app/services/spot/s3_path.rb
   Valkyrie::StorageAdapter.register(
-    Valkyrie::Storage::Shrine.new(Shrine.storages[:s3_iiif], nil, Spot::S3Path::IiifPathGenerator),
+    Valkyrie::Storage::Shrine.new(Shrine.storages[:s3_iiif], nil, Spot::S3Path::IiifPathGenerator, identifier_prefix: 'iiif'),
     :iiif_source_s3
   )
 
   Valkyrie::StorageAdapter.register(
-    Valkyrie::Storage::Shrine.new(Shrine.storages[:s3_av], nil, Spot::S3Path::AvPathGenerator),
+    Valkyrie::Storage::Shrine.new(Shrine.storages[:s3_av], nil, Spot::S3Path::AvPathGenerator, identifier_prefix: 'av'),
     :av_source_s3
   )
 
   if ENV['AWS_OBJECT_STORE_BUCKET'].present?
     Valkyrie::StorageAdapter.register(
-      Valkyrie::Storage::VersionedShrine.new(Shrine.storages[:s3_object_store]),
+      Valkyrie::Storage::VersionedShrine.new(Shrine.storages[:s3_object_store], identifier_prefix: 'obj'),
       :versioned_object_store_s3
     )
     Valkyrie.config.storage_adapter = :versioned_object_store_s3
