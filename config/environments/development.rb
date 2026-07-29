f300e1bad3f60de6ecafcfdff6034870dcf9ffd4
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2026-07-29 16:05:03 -0400

    initial upgrade working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Rails' default is to not eager_load in development for speed's sake (I think?)
   # but we were encountering a lot of Missing Constant errors in our Sidekiq logs
   # and eager_loding classes seemed to help fix that.
-  config.eager_load = true
+  config.eager_load = false
 
   # Show full error reports.
   config.consider_all_requests_local = true
