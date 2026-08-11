6e54da879ce84fcaa0889694673b4fd24ae9f4ef
Anna Malantonio <malantoa@lafayette.edu>
2026-08-11 14:12:53 -0400

    configure gallery document_component

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -24,11 +24,8 @@ class CatalogController < ApplicationController
     config.advanced_search[:query_parser] ||= 'dismax'
     config.advanced_search[:form_solr_parameters] ||= {}
 
+    config.view.gallery.document_component = Blacklight::Gallery::DocumentComponent
     config.view.gallery.partials = [:index_header, :index]
-
-    config.view.gallery(document_component: Blacklight::Gallery::DocumentComponent)
-    config.view.masonry(document_component: Blacklight::Gallery::DocumentComponent)
-    config.view.slideshow(document_component: Blacklight::Gallery::SlideshowComponent)
 
     config.show.tile_source_field = :content_metadata_image_iiif_info_ssm
     config.show.partials.insert(1, :openseadragon)
