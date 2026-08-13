b31eff35693d5afd5024025d7b542f922d185326
Jennifer Wellnitz <jennifer.wellnitz22@gmail.com>
2026-08-13 16:39:49 -0400

    clean up derivatives tests

--- spec/services/spot/derivatives/av_valkyrie_derivatives_service_spec.rb
+++ spec/services/spot/derivatives/av_valkyrie_derivatives_service_spec.rb
@@ -2,7 +2,7 @@
 RSpec.describe Spot::Derivatives::AvValkyrieDerivativeService, derivatives: true do
   context "when given an audio file" do
     let(:valid_file_metadata) do
-      FactoryBot.valkyrie_create(:hyrax_file_metadata, :audio_file, file_set_id: SecureRandom.uuid)
+      FactoryBot.valkyrie_create(:hyrax_file_metadata, :audio_file, file_set_id: 'abcd1234')
     end
 
     let(:file_metadata) { valid_file_metadata }
@@ -14,7 +14,6 @@ RSpec.describe Spot::Derivatives::AvValkyrieDerivativeService, derivatives: true
     it_behaves_like "a Spot::DerivativeService"
 
     before do
-      allow(file_metadata).to receive(:file_set_id).and_return('abcd1234')
       allow(Hyrax.query_service).to receive(:find_by).with(id: 'abcd1234').and_return(file_set)
     end
 
@@ -37,11 +36,25 @@ RSpec.describe Spot::Derivatives::AvValkyrieDerivativeService, derivatives: true
         )
       end
     end
+
+    describe "#cleanup_derivatives" do
+      before do
+        allow(Hyrax::DerivativePath).to receive(:derivatives_for_reference)
+          .with(file_set)
+          .and_return(["/spot/tmp/derivatives/ab/cd/12/34-mp3.mp3"])
+        allow(FileUtils).to receive(:rm_f)
+        service.cleanup_derivatives
+      end
+
+      it 'removes derivative files' do
+        expect(FileUtils).to have_received(:rm_f).with("/spot/tmp/derivatives/ab/cd/12/34-mp3.mp3")
+      end
+    end
   end
 
   context "when given an video file" do
     let(:valid_file_metadata) do
-      FactoryBot.valkyrie_create(:hyrax_file_metadata, :video_file, file_set_id: SecureRandom.uuid)
+      FactoryBot.valkyrie_create(:hyrax_file_metadata, :video_file, file_set_id: 'abcd1234')
     end
 
     let(:file_metadata) { valid_file_metadata }
@@ -53,7 +66,6 @@ RSpec.describe Spot::Derivatives::AvValkyrieDerivativeService, derivatives: true
     it_behaves_like "a Spot::DerivativeService"
 
     before do
-      allow(file_metadata).to receive(:file_set_id).and_return('abcd1234')
       allow(Hyrax.query_service).to receive(:find_by).with(id: 'abcd1234').and_return(file_set)
     end
 
@@ -65,12 +77,14 @@ RSpec.describe Spot::Derivatives::AvValkyrieDerivativeService, derivatives: true
       let(:stream) { double(width: 100, height: 200) }
 
       before do
+        allow(service).to receive(:create_thumbnail_from)
         allow(Ffprober::Parser).to receive(:from_file).with(filename).and_return(mock_ffprobe)
         allow(Hydra::Derivatives::VideoDerivatives).to receive(:create)
         service.create_derivatives(filename)
       end
 
       it 'creates derivative files' do
+        expect(service).to have_received(:create_thumbnail_from).with(filename)
         expect(Hydra::Derivatives::VideoDerivatives).to have_received(:create).with(
           filename,
           outputs: contain_exactly(
@@ -80,7 +94,6 @@ RSpec.describe Spot::Derivatives::AvValkyrieDerivativeService, derivatives: true
                             url: 'file:///spot/tmp/derivatives/ab/cd/12/34-webm.webm',
                             size: '240x480',
                             mime_type: 'video/webm',
-                            input_options: "-ss 1",
                             video: "-g 30 -b:v 2500k",
                             audio: "-b:a 256k -ar 44100"
                           ),
@@ -90,12 +103,26 @@ RSpec.describe Spot::Derivatives::AvValkyrieDerivativeService, derivatives: true
                             url: 'file:///spot/tmp/derivatives/ab/cd/12/34-mp4.mp4',
                             size: '544x1080',
                             mime_type: 'video/mp4',
-                            input_options: "-ss 1",
                             video: "-g 30 -b:v 8000k",
                             audio: "-b:a 256k -ar 44100"
                           )
           )
         )
+      end
+    end
+
+    describe "#cleanup_derivatives" do
+      before do
+        allow(Hyrax::DerivativePath).to receive(:derivatives_for_reference)
+          .with(file_set)
+          .and_return(["/spot/tmp/derivatives/ab/cd/12/34-webm.webm", "/spot/tmp/derivatives/ab/cd/12/34-mp4.mp4"])
+        allow(FileUtils).to receive(:rm_f)
+        service.cleanup_derivatives
+      end
+
+      it 'removes derivative files' do
+        expect(FileUtils).to have_received(:rm_f).with("/spot/tmp/derivatives/ab/cd/12/34-webm.webm")
+        expect(FileUtils).to have_received(:rm_f).with("/spot/tmp/derivatives/ab/cd/12/34-mp4.mp4")
       end
     end
   end
